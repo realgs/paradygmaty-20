7659f76fd7b00067aa38bb7791150a573aad6d44
Łukasz Łosieczka <lukasz.losieczka@gmail.com>
2020-10-07 18:12:18 +0200

    Update BubbleSort

--- BubbleSort.java
+++ BubbleSort.java
@@ -1,7 +1,7 @@
 import java.util.ArrayList;
 
 public class BubbleSort {
-    public static void sort(ArrayList<Integer> list) {
+    public void sort(ArrayList<Integer> list) {
         for (int i = 0; i < list.size(); i++) {
             for (int j = 0; j < list.size()-1 - i; j++) {
                 int prev = list.get(j);
