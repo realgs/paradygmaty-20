94cb62ea05959aaf143674d4940539b9f2ebecf6
Bismarck1862 <bartosz.walkowiak3@gmail.com>
2020-10-07 11:54:19 +0200

    Kod Sortowania Java

---
+++ SortingMethods/src/SortingMethods/NaturalComparator.java
@@ -0,0 +1,10 @@
+package SortingMethods;
+
+import java.util.Comparator;
+
+public class NaturalComparator<T extends Comparable<? super T>> implements Comparator<T> {
+	@Override
+	public int compare(T o1, T o2) {
+		return o1.compareTo(o2);
+	}
+}
