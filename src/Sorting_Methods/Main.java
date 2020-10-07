6c5b9a1c958117c90b63e4a027685873af90a257
Zosia-Malyszok <soniamalish17@gmail.com>
2020-10-07 13:48:01 +0200

    Tests for selectionSort method are added

--- src/Sorting_Methods/Main.java
+++ src/Sorting_Methods/Main.java
@@ -2,29 +2,39 @@ package Sorting_Methods;
 
 import java.util.Random;
 
-import static Sorting_Methods.Sorting.bubbleSort;
-import static Sorting_Methods.Sorting.printArray;
+import static Sorting_Methods.Sorting.*;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("Test of first array");
-        int[] FirstTestArray = {35, 25, 15, 85,};
-        int[] firstSortedArray = bubbleSort(FirstTestArray);
-        printArray(firstSortedArray);
+        int[] firstTestArray = {35, 25, 15, 85,};
+        System.out.println("Bubble sort");
+        int[] firstSortedArrayBubble = bubbleSort(firstTestArray);
+        printArray(firstSortedArrayBubble);
+        System.out.println("Selection sort");
+        int[] firstSortedArraySelection = selectionSort(firstTestArray);
+        printArray(firstSortedArraySelection);
 
         System.out.println("Test of second array");
         int[] secondTestArray = {12, 67, -90, -1000, 0, 135, -80};
-        int[] secondSortedArray = bubbleSort(secondTestArray);
-        printArray(secondSortedArray);
+        System.out.println("Bubble sort");
+        int[] secondSortedArrayBubble = bubbleSort(secondTestArray);
+        printArray(secondSortedArrayBubble);
+        System.out.println("Selection sort");
+        int[] secondSortedArraySelection = selectionSort(secondTestArray);
+        printArray(secondSortedArraySelection);
 
-        System.out.println("Test of random array");
-        int[] randomlyGeneratedArray = new int[1000];
+        System.out.println("Test of random generated array");
+        int[] randomGeneratedArray = new int[1000];
         Random r = new Random();
-        for (int i = 0; i < randomlyGeneratedArray.length; i++) {
-            randomlyGeneratedArray[i] = r.nextInt((500 + 500) + 1) - 500;
+        for (int i = 0; i < randomGeneratedArray.length; i++) {
+            randomGeneratedArray[i] = r.nextInt((500 + 500) + 1) - 500;
         }
-        int[] thirdSortedArray = bubbleSort(randomlyGeneratedArray);
-        printArray(thirdSortedArray);
-
+        System.out.println("Bubble sort");
+        int[] thirdSortedArrayBubble = bubbleSort(randomGeneratedArray);
+        printArray(thirdSortedArrayBubble);
+        System.out.println("Selection sort");
+        int[] thirdSortedArraySelection = selectionSort(randomGeneratedArray);
+        printArray(thirdSortedArraySelection);
     }
 }
