5982230a728daf67896bf558cdff4a3eee3b1f26
Kacper Kochanski <kacper1134@o2.pl>
2020-10-08 18:10:47 +0200

    Added insertSort method

--- Main.java
+++ Main.java
@@ -2,7 +2,21 @@ public class Main
 {
     public static void main(String[] args)
     {
+        
+    }
 
+    public static void insertSort(int [] tab)
+    {
+        for(int i = 1;i<tab.length;i++)
+        {
+            int index_Act = i;
+
+            while(index_Act > 0 && tab[index_Act] < tab[index_Act - 1])
+            {
+                swap(tab,index_Act,index_Act - 1);
+                index_Act--;
+            }
+        }
     }
 
     public static void swap(int [] tab,int firstIndex,int secondIndex)
