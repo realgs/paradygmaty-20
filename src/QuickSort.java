869b0d309ec61a78d575b677be207e938605635d
Milena_Mazur <milenamazur00@gmail.com>
2020-10-08 12:25:26 +0200

    tests added and quicksort imp adjusted to pass the test

--- src/QuickSort.java
+++ src/QuickSort.java
@@ -1,19 +1,13 @@
 import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.Random;
 
-public class QuickSort<T extends Comparable<T>> {
-    private Comparator<T> comparator;
+public class QuickSort {
 
-    protected QuickSort(Comparator<T> comparator) {
-        this.comparator = comparator;
-    }
-
-    public void sort(ArrayList<T> list) {
+    public static <T extends Comparable<T>> void sort(ArrayList<T> list) {
         quickSort(list, 0, list.size() - 1);
     }
 
-    private void quickSort(ArrayList<T> list, int start, int end) {
+    private static <T extends Comparable<T>> void quickSort(ArrayList<T> list, int start, int end) {
         if (start < end) {
             int q = partition(list, start, end);
             quickSort(list, start, q - 1);
@@ -21,13 +15,13 @@ public class QuickSort<T extends Comparable<T>> {
         }
     }
 
-    private int partition(ArrayList<T> list, int start, int end) {
+    private static <T extends Comparable<T>> int partition(ArrayList<T> list, int start, int end) {
         int r = new Random().nextInt(end - start) + start;
         swap(list, r, end);
         T x = list.get(end);
         int i = start - 1;
         for (int j = start; j < end; j++) {
-            if (comparator.compare(list.get(j), x) <= 0) {
+            if (x.compareTo(list.get(j)) > 0) {
                 swap(list, ++i, j);
             }
         }
@@ -36,7 +30,7 @@ public class QuickSort<T extends Comparable<T>> {
     }
 
 
-    private void swap(ArrayList<T> list, int left, int right) {
+    private static <T> void swap(ArrayList<T> list, int left, int right) {
         T temp = list.get(left);
         list.set(left, list.get(right));
         list.set(right, temp);
---
+++ src/SortsTest.java
@@ -0,0 +1,103 @@
+import org.junit.jupiter.api.Test;
+
+import java.util.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class SortsTest {
+    private Random random = new Random();
+
+    @Test
+    public void randomInteger_Test() {
+        ArrayList<Integer> list = new ArrayList<>();
+        for (int i = 0; i < 1000; i++) {
+            list.add(random.nextInt());
+        }
+        ArrayList<Integer> list1 = new ArrayList<>(list);
+        QuickSort.sort(list);
+        list1.sort(Integer::compareTo);
+
+        assertTrue(ifSorted(list));
+        assertTrue(ifSorted(list1));
+    }
+
+    @Test
+    public void randomDouble_Test() {
+        ArrayList<Double> list = new ArrayList<>();
+        for (int i = 0; i < 1000; i++) {
+            list.add(random.nextDouble());
+        }
+        ArrayList<Double> list1 = new ArrayList<>(list);
+        QuickSort.sort(list);
+        list1.sort(Double::compareTo);
+
+        assertTrue(ifSorted(list));
+        assertTrue(ifSorted(list1));
+    }
+
+    @Test
+    public void randomFloat_Test() {
+        ArrayList<Float> list = new ArrayList<>();
+        for (int i = 0; i < 5; i++) {
+            list.add(random.nextFloat());
+        }
+        ArrayList<Float> list1 = new ArrayList<>(list);
+        QuickSort.sort(list);
+        list1.sort(Float::compareTo);
+
+        assertTrue(ifSorted(list));
+        assertTrue(ifSorted(list1));
+    }
+
+    @Test
+    public void randomByte_Test() {
+        ArrayList<Byte> list = new ArrayList<>();
+        for (int i = 0; i < 1000; i++) {
+            list.add(((Integer) random.nextInt()).byteValue());
+        }
+        ArrayList<Byte> list1 = new ArrayList<>(list);
+        QuickSort.sort(list);
+        list1.sort(Byte::compareTo);
+
+        assertTrue(ifSorted(list));
+        assertTrue(ifSorted(list1));
+    }
+
+    @Test
+    public void theSameValue_Test() {
+        ArrayList<Integer> list = new ArrayList<>();
+        for (int i = 0; i < 1000; i++) {
+            list.add(1);
+        }
+        ArrayList<Integer> list1 = new ArrayList<>(list);
+        QuickSort.sort(list);
+        list1.sort(Integer::compareTo);
+
+        assertTrue(ifSorted(list));
+        assertTrue(ifSorted(list1));
+    }
+
+    @Test
+    public void negativeValues_Test() {
+        ArrayList<Integer> list = new ArrayList<>();
+        for (int i = 0; i < 1000; i++) {
+            list.add(random.nextInt(1000) * -1);
+        }
+        ArrayList<Integer> list1 = new ArrayList<>(list);
+        QuickSort.sort(list);
+        list1.sort(Integer::compareTo);
+
+        assertTrue(ifSorted(list));
+        assertTrue(ifSorted(list1));
+    }
+
+
+    public <T extends Comparable<T>> boolean ifSorted(ArrayList<T> list) {
+        for (int i = 1; i < list.size(); i++) {
+            if (list.get(i - 1).compareTo(list.get(i)) > 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
