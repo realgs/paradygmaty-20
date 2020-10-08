1dfc805d68b46304c167825b1e42c3132e3aa4b6
Kacper Kochanski <kacper1134@o2.pl>
2020-10-08 18:43:08 +0200

    Added tests and equalArrays method

--- Main.java
+++ Main.java
@@ -1,10 +1,46 @@
+import java.util.Arrays;
+import java.util.Random;
 public class Main
 {
     public static void main(String[] args)
     {
-        
-    }
+        if(bigRandomTest())
+        {
+            System.out.println("bigRandomTest passed");
+        }
+        else
+        {
+            System.out.println("bigRandomTest not passed");
+        }
 
+        if(bigReverseOrderedTest())
+        {
+            System.out.println("bigReverseOrderedTest passed");
+        }
+        else
+        {
+            System.out.println("bigReverseOrderedTest not passed");
+        }
+
+        if(zeroElementTest())
+        {
+            System.out.println("zeroElementTest passed");
+        }
+        else
+        {
+            System.out.println("zeroElementTest not passed");
+        }
+
+        if(bigOrderedTest())
+        {
+            System.out.println("bigOrderedTest passed");
+        }
+        else
+        {
+            System.out.println("bigOrderedTest not passed");
+        }
+    }
+    
     public static void insertSort(int [] tab)
     {
         for(int i = 1;i<tab.length;i++)
@@ -56,5 +92,98 @@ public class Main
         }
 
         System.out.println();
+    }
+
+    public static boolean bigRandomTest()
+    {
+        int [] bubbleArray = new int[30000];
+        int [] insertArray = new int[30000];
+        int [] bulitinArray = new int[30000];
+
+        Random random = new Random();
+
+        for(int i = 0;i<30000;i++)
+        {
+            int element = random.nextInt(10000000);
+
+            bubbleArray[i] = element;
+            insertArray[i] = element;
+            bulitinArray[i] = element;
+        }
+
+        insertSort(insertArray);
+        bubbleSort(bubbleArray);
+        Arrays.sort(bulitinArray);
+
+        return equalArrays(insertArray,bubbleArray) && equalArrays(bubbleArray,bulitinArray);
+    }
+
+    public static boolean bigReverseOrderedTest()
+    {
+        int [] bubbleArray = new int[30000];
+        int [] insertArray = new int[30000];
+        int [] bulitinArray = new int[30000];
+
+        for(int i = 0;i<30000;i++)
+        {
+            int element = 30000 - i;
+
+            bubbleArray[i] = element;
+            insertArray[i] = element;
+            bulitinArray[i] = element;
+        }
+
+        insertSort(insertArray);
+        bubbleSort(bubbleArray);
+        Arrays.sort(bulitinArray);
+
+        return equalArrays(insertArray,bubbleArray) && equalArrays(bubbleArray,bulitinArray);
+    }
+
+    public static boolean zeroElementTest()
+    {
+        int [] bubbleArray = new int[0];
+        int [] insertArray = new int[0];
+        int [] bulitinArray = new int[0];
+
+        insertSort(insertArray);
+        bubbleSort(bubbleArray);
+        Arrays.sort(bulitinArray);
+
+        return equalArrays(insertArray,bubbleArray) && equalArrays(bubbleArray,bulitinArray);
+    }
+
+    public static boolean bigOrderedTest()
+    {
+        int [] bubbleArray = new int[30000];
+        int [] insertArray = new int[30000];
+        int [] bulitinArray = new int[30000];
+
+        for(int i = 0;i<30000;i++)
+        {
+            int element = i;
+
+            bubbleArray[i] = element;
+            insertArray[i] = element;
+            bulitinArray[i] = element;
+        }
+
+        insertSort(insertArray);
+        bubbleSort(bubbleArray);
+        Arrays.sort(bulitinArray);
+
+        return equalArrays(insertArray,bubbleArray) && equalArrays(bubbleArray,bulitinArray);
+    }
+
+    public static boolean equalArrays(int [] firstArray,int [] secondArray)
+    {
+        for(int i = 0;i<firstArray.length;i++)
+        {
+            if(firstArray[i] != secondArray[i])
+            {
+                return false;
+            }
+        }
+        return true;
     }
 }
