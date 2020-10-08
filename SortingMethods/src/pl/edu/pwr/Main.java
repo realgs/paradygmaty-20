6f2adc6f902da9edf9faf01055ea605eae7b8df9
Nazar Pechevystyi <nazarpechka@gmail.com>
2020-10-08 09:55:22 +0200

    Add tests

--- SortingMethods/src/pl/edu/pwr/Main.java
+++ SortingMethods/src/pl/edu/pwr/Main.java
@@ -1,13 +1,43 @@
 package pl.edu.pwr;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Main {
 
     public static void main(String[] args) {
-        SelectionSort sort = new SelectionSort();
-        int[] arr = {-14, 28, 13, 4, 27, -122, 1, 53};
-        sort.sort(arr);
-        System.out.println(Arrays.toString(arr));
+        MergeSort mSort = new MergeSort();
+        SelectionSort sSort = new SelectionSort();
+
+
+        int[] intArr = {-14, 28, 13, 4, 27, -122, 1, 53, 8, 3, 14, 13},
+                intArrCopy = new int[intArr.length];
+        int[] bigIntArr = {4323243, -2315555, 1236997, -1324578, 69994312},
+                bigIntArrCopy = new int[bigIntArr.length];
+        float[] floatArr = {1.1f, 5.28f, -3.42f, 1356.55f};
+
+
+        testSort(mSort, sSort, intArr, intArrCopy);
+        testSort(mSort, sSort, bigIntArr, bigIntArrCopy);
+
+        mSort.sort(null);
+        sSort.sort(null);
+
+//        mSort.sort(floatArr);
+//        sSort.sort(floatArr);
+    }
+
+    private static void testSort(MergeSort mSort, SelectionSort sSort, int[] arr, int[] arrCopy) {
+        System.arraycopy(arr, 0, arrCopy, 0, arr.length);
+        mSort.sort(arrCopy);
+        System.out.println(Arrays.toString(arrCopy) + " - merge sort");
+
+        System.arraycopy(arr, 0, arrCopy, 0, arr.length);
+        sSort.sort(arrCopy);
+        System.out.println(Arrays.toString(arrCopy) + " - selection sort");
+
+        System.arraycopy(arr, 0, arrCopy, 0, arr.length);
+        Arrays.sort(arrCopy);
+        System.out.println(Arrays.toString(arrCopy) + " - Arrays.sort()\n");
     }
 }
