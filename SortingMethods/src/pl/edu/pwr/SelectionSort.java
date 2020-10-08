3430dae27839b7d5ff44b53f7248cfe4a32de756
Nazar Pechevystyi <nazarpechka@gmail.com>
2020-10-08 09:54:44 +0200

    Add null array check

--- SortingMethods/src/pl/edu/pwr/SelectionSort.java
+++ SortingMethods/src/pl/edu/pwr/SelectionSort.java
@@ -2,20 +2,24 @@ package pl.edu.pwr;
 
 public class SelectionSort {
     public void sort(int[] array) {
-        int minInd, tmp, n = array.length;
+        if(array == null) return;
+
+        int minIdx, n = array.length;
+        int tmp;
 
         for(int i = 0; i < n - 1; ++i) {
-            minInd = i;
+            minIdx = i;
             for(int j = i + 1; j < n; ++j) {
-                if(array[j] < array[minInd]) {
-                    minInd = j;
+                if(array[j] <= array[minIdx]) {
+                    minIdx = j;
                 }
             }
 
             tmp = array[i];
-            array[i] = array[minInd];
-            array[minInd] = tmp;
+            array[i] = array[minIdx];
+            array[minIdx] = tmp;
 
         }
     }
+
 }
