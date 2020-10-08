b128bfda16ebd148e27d16d980780ccf9b65b847
Kacper Kochanski <kacper1134@o2.pl>
2020-10-08 18:06:19 +0200

    Added swap and displayArray methods

--- Main.java
+++ Main.java
@@ -2,6 +2,23 @@ public class Main
 {
     public static void main(String[] args)
     {
-        
+
+    }
+
+    public static void swap(int [] tab,int firstIndex,int secondIndex)
+    {
+        int tmp = tab[firstIndex];
+        tab[firstIndex] = tab[secondIndex];
+        tab[secondIndex] = tmp;
+    }
+
+    public static void displayArray(int [] tab)
+    {
+        for(int i = 0;i<tab.length;i++)
+        {
+            System.out.print(tab[i] + " ");
+        }
+
+        System.out.println();
     }
 }
