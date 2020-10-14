de8d07e5629d36ff268af2b50c86acaefc063856
Klaudia Blazyczek <blazyczek.klaudia@gmail.com>
2020-10-14 18:35:28 +0200

    Improve excercise 4

--- Lista2/src/Zadanie4.scala
+++ Lista2/src/Zadanie4.scala
@@ -1,6 +1,8 @@
 object Zadanie4 extends App{
   def power(base: Double, exponent: Int): Double =
-    if(exponent==0) 1
+    if(base==0 && exponent<=0) throw new Exception("Incorrect base and exponent")
+    else if(base==0) 0
+    else if(exponent==0) 1
     else if(exponent>0) base*power(base,exponent-1)
     else (1/base)*power(base, exponent+1)
 
@@ -9,4 +11,6 @@ object Zadanie4 extends App{
   println(power(2.5,4)==39.0625)
   println(power(-2, 5)==(-32))
   println(power(4, -2)==0.0625)
+  println(power(0,5)==0)
+  //println(power(0,-1)) // =>> throws Exception ("Incorrect base and exponent")
 }
