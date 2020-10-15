336f1cfcec50419784b8b92866e0b74b7310b922
Witold Brzozowski <brzozowskyy@gmail.com>
2020-10-15 19:24:16 +0200

    Change type to BigDecimal

--- L02/src/zad04.scala
+++ L02/src/zad04.scala
@@ -1,15 +1,15 @@
 object zad04 {
-  def toPower(base: Double, exponent: Int): Double = {
+  def toPower(base: BigDecimal, exponent: Int): BigDecimal = {
     validate(base, exponent)
     if (exponent < 0) 1 / calcPower(base, -exponent)
     else calcPower(base, exponent)
   }
 
-  private def validate(base: Double, exponent: Int): Unit = {
+  private def validate(base: BigDecimal, exponent: Int): Unit = {
     if (base == 0 && exponent <= 0) throw new Exception("Invalid input")
   }
 
-  private def calcPower(base: Double, nonNegativeExponent: Int): Double = {
+  private def calcPower(base: BigDecimal, nonNegativeExponent: Int): BigDecimal = {
     if (nonNegativeExponent == 0) 1
     else base * calcPower(base, nonNegativeExponent - 1)
   }
@@ -18,6 +18,7 @@ object zad04 {
     assert(toPower(2, 10) == 1024)
     assert(toPower(1, -1) == 1)
     assert(toPower(-2, -3) == -0.125)
+    assert(toPower(-0.2, 3) == -0.008)
     Utils.assertThatExceptionIsThrown(() => toPower(0, 0))
     Utils.assertThatExceptionIsThrown(() => toPower(0, -5))
   }
