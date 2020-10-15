b4eb26d8fb84ba9e38f14fbabdc85a5286b4e00b
Klaudia Blazyczek <blazyczek.klaudia@gmail.com>
2020-10-15 08:03:59 +0200

    Improve excercise 3

--- Lista2/src/Zadanie3.scala
+++ Lista2/src/Zadanie3.scala
@@ -1,13 +1,14 @@
+//rozwizanie zakłada, że ktoś może zamienić domknięcia przedziałów. Program sprawdza: [X,Y] oraz [Y,X]
 object Zadanie3 extends App{
   @scala.annotation.tailrec
   def intervalChecker(list: List[Double], X: Double, Y: Double): Boolean =
     if(list==Nil) true
-    else if((list.head>=X) && (list.head<=Y)) intervalChecker(list.tail, X, Y)
+    else if(((list.head>=X) && (list.head<=Y))  || ((list.head>=Y) && (list.head<=X))) intervalChecker(list.tail, X, Y)
     else false
 
   println(intervalChecker(List(1.4, 2, 3.5, 2.2), 0, 4)==true)
   println(intervalChecker(List(1.6, -2, 4.5, 2.2), 0, 5)==false)
-  println(intervalChecker(List(1.6, 13, 4.5, 4.2), 0, 10)==false)
+  println(intervalChecker(List(1.6, 13, 4.5, 4.2), 10, 0)==false)
   println(intervalChecker(List(), 0, 3.3)==true)
   println(intervalChecker(List(1.1,-2.2), -2.2, 1.1)==true)
 }
