8ee541583eadc44c9de1cf5ce8985679075e8559
piXel2K <rafal.piwnik@gmail.com>
2020-10-15 19:06:40 +0200

    Added tasks 2-4 with tests

--- List2/src/main/scala/Functions.scala
+++ List2/src/main/scala/Functions.scala
@@ -12,4 +12,58 @@ object Functions {
     else cumulativeProductRec(numbers)
   }
 
+  // Zadanie 2
+  private def concatenateStringsRec(words: List[String], separator: String, endOfLineCharacter: Char): String = {
+    if (words == Nil) ""
+    else if (words.length == 1) words.head + endOfLineCharacter.toString
+    else words.head + separator + concatenateStringsRec(words.tail, separator, endOfLineCharacter)
+  }
+
+  def concatenateStrings(words: List[String], separator: String, endOfLineCharacter: Char): String = {
+    if (words == null || separator == null) {
+      throw new IllegalArgumentException("Can't concatenate null values");
+    } else concatenateStringsRec(words, separator, endOfLineCharacter)
+  }
+
+  // Zadanie 3
+  private def areInRec(numbers: List[Double], lowerBound: Double, upperBound: Double): Boolean = {
+    if (numbers == Nil) true
+    else {
+      if (lowerBound <= numbers.head && numbers.head <= upperBound) {
+        areInRec(numbers.tail, lowerBound, upperBound)
+      } else false
+    }
+  }
+
+  def areIn(numbers: List[Double], lowerBound: Double, upperBound: Double): Boolean = {
+    if (numbers == null || numbers == Nil) return false
+
+    if (lowerBound > upperBound) areInRec(numbers, upperBound, lowerBound)
+    else areInRec(numbers, lowerBound, upperBound)
+  }
+
+  // Zadanie 4
+  def exponent(base: Double, exponent: Int): Double = {
+    @tailrec
+    def auxExponent(exponent: Int, result: Double, power: Double): Double = {
+      if (exponent <= 0) result
+      else {
+        if (exponent % 2 == 1) auxExponent(exponent >> 1, result * power, power * power)
+        else auxExponent(exponent >> 1, result, power * power)
+      }
+    }
+
+    if (exponent >= 0) {
+      auxExponent(exponent, 1, base)
+    } else {
+      auxExponent(-exponent, 1, 1 / base)
+    }
+  }
+
+  def linearExponentDemo(base: Double, exponent: Int): Double = {
+    if (exponent == 0) 1
+    else {
+      base * linearExponentDemo(base, exponent - 1)
+    }
+  }
 }
--- List2/src/test/scala/FunctionsTest.scala
+++ List2/src/test/scala/FunctionsTest.scala
@@ -25,4 +25,85 @@ class FunctionsTest extends FunSuite {
   test("cumulativeProduct.bigIntToDoubleCoercion") {
     assert(Functions.cumulativeProduct(List(1 << 30, 1 << 30)) === (1 << 30).toDouble * (1 << 30).toDouble)
   }
+
+
+  // Zadanie 2
+  test("concatenateStrings.null") {
+    assertThrows[IllegalArgumentException] {
+      Functions.concatenateStrings(List("first", "second", "third"), null, 'c')
+    }
+  }
+
+  test("concatenateStrings.emptyList") {
+    assert(Functions.concatenateStrings(List(), ",", 'c') === "")
+  }
+
+  test("concatenateStrings.oneElementList") {
+    assert(Functions.concatenateStrings(List("AAA"), ",", 'c') === "AAAc")
+  }
+
+  test("concatenateStrings.simpleList") {
+    assert(
+      Functions.concatenateStrings(List("first", "second", "third"), ",", '#') ===
+        "first,second,third#"
+    )
+  }
+
+
+  // Zadanie 3
+  test("areIn.nullList") {
+    assert(!Functions.areIn(null, 10, 15))
+  }
+
+  test("areIn.simpleInt") {
+    assert(Functions.areIn(List(7, 39, 4, 24, 38, 26, 44, 12, 47), 1, 50))
+  }
+
+  test("areIn.simpleDouble") {
+    assert(Functions.areIn(List(0.333, 0.9999999999, 0.564, 0.0000001, 0.783), 0, 1))
+  }
+
+  test("areIn.negative") {
+    assert(Functions.areIn(List(-14.5909, 10, 7, -24, 19.31425, 9.9999), -25, 25))
+  }
+
+  test("areIn.oneValueNotInRange") {
+    assert(!Functions.areIn(List(-14.5909, 10, 7, -27, 19.31425, 9.9999), -25, 25))
+  }
+
+  test("areIn.noValueInRange") {
+    assert(!Functions.areIn(List(18, 27.34, 30, 32, 25, 11.99, -31, 29, 25.657, -14), 0, 10))
+  }
+
+  test("areIn.rangeBoundsNotInOrder") {
+    assert(Functions.areIn(List(9, 4, 6, 6, 5, 10, 1), 10, 0))
+  }
+
+
+  // Zadanie 4
+  test("exponent.zeroBase") {
+    assert(Functions.exponent(0, 15) === 0)
+  }
+
+  test("exponent.positiveBase") {
+    assert(Functions.exponent(9.5, 3) === 857.375)
+  }
+
+  test("exponent.negativeBase") {
+    assert(Math.abs(Functions.exponent(-3.14, 5) - -305.244776182) < 0.000001)
+  }
+
+  test("exponent.negativeExponent") {
+    assert(Functions.exponent(1.0 / 2, -9) === 512)
+  }
+
+  test("exponent.maxExponent") {
+    assert(Functions.exponent(1.0, Integer.MAX_VALUE) === 1)
+  }
+
+  test("linearExponentDemo.maxExponent") {
+    assertThrows[StackOverflowError] {
+      Functions.linearExponentDemo(1.0, Integer.MAX_VALUE)
+    }
+  }
 }
