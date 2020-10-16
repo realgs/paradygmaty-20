fc3f73b647b2f3a8ca573d7938c0103dcba13ad9
wietrzynska7 <wietrzynska7@gmail.com>
2020-10-16 22:27:07 +0200

    Add Main object

---
+++ list2/src/Main.scala
@@ -0,0 +1,10 @@
+object Main {
+
+  def main(args: Array[String]): Unit = {
+    Tests.multiplyingTest()
+    Tests.formattingTextTest()
+    Tests.powTest()
+    Tests.rangingNumberTest()
+  }
+
+}
