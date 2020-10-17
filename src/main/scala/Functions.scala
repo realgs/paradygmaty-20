59cc1ba88ebe7c4c8ffea0c876e2c403ed38334e
Nazar Pechevystyi <nazarpechka@gmail.com>
2020-10-17 14:44:59 +0200

    Better tests

--- src/main/scala/Functions.scala
+++ src/main/scala/Functions.scala
@@ -1,21 +1,23 @@
 object Functions {
 
-  def doubleProduct(list:List[Double]): Double =
-    if(list == Nil || list == null) 0
-    else if(list.length == 1) list.head
+  def doubleProduct(list: List[Double]): Double =
+    if (list == Nil || list == null) 0
+    else if (list.length == 1) list.head
     else list.head * doubleProduct(list.tail)
 
-  def combineTokens(words:List[String], ending:String, separator:String): String =
-    if(words.length == 1) words.head + ending
-    else words.head + separator + combineTokens(words.tail, ending, separator)
+  def combineTokens(words: List[String], separator: String, ending: Char): String =
+    if (words == Nil || words == null) ""
+    else if (words.length == 1) words.head + ending
+    else words.head + separator + combineTokens(words.tail, separator, ending)
 
-  def listInRange(numbers:List[Double], low:Double, high:Double): Boolean =
-    if(numbers == Nil) true
-    else if(numbers.head < low || numbers.head > high) false
+  def listInRange(numbers: List[Double], low: Double, high: Double): Boolean =
+    if (numbers == Nil || numbers == null) true
+    else if (numbers.head < low || numbers.head > high) false
     else listInRange(numbers.tail, low, high)
 
-  def power(base:Double, exponent:Int): Double =
-    if(exponent != 0) base * power(base, exponent - 1)
+  def power(base: Double, exponent: Int): Double =
+    if (exponent < 0) throw new IllegalArgumentException("This function doesn't support exponents < 0!")
+    else if (exponent != 0) base * power(base, exponent - 1)
     else 1
 
 }
--- src/test/scala/FunctionsTest.scala
+++ src/test/scala/FunctionsTest.scala
@@ -1,26 +1,40 @@
 import org.scalatest.funsuite.AnyFunSuite
 
 class FunctionsTest extends AnyFunSuite {
-    test("doubleProduct") {
-      assert(Functions.doubleProduct(List(5, 2, 9, 1, 2, 8, 5)) === 7200)
-      assert(Functions.doubleProduct(List(6.24, 3.78, 1235.9, 45.87)) - 1337175.6574176 < 0.0001)
-      assert(Functions.doubleProduct(List(-5.2, -6.7, -8.24)) - (-287.0816) < 0.0001)
-      assert(Functions.doubleProduct(List(1.0, -1.0, 0.0)) === 0.0)
-      assert(Functions.doubleProduct(Nil) === 0.0)
-      assert(Functions.doubleProduct(null) === 0.0)
+  test("doubleProduct") {
+    assert(Functions.doubleProduct(List(6.24, 3.78, 1235.9, 45.87)) === (6.24 * 3.78 * 1235.9 * 45.87))
+    assert(Functions.doubleProduct(List(-5.2, -6.7, -8.2)) === (-5.2 * -6.7 * -8.2))
+    assert(Functions.doubleProduct(List(23423468.43452, 5469894525.283443, 23445345.3656456)) ===
+      (23423468.43452 * 5469894525.283443 * 23445345.3656456))
+    assert(Functions.doubleProduct(List(1.0, -1.0, 0.0)) === 0.0)
+    assert(Functions.doubleProduct(Nil) === 0.0)
+    assert(Functions.doubleProduct(null) === 0.0)
+  }
 
-    }
+  test("combineTokens") {
+    assert(Functions.combineTokens(List("I", "love", "it"), " ", '!') === "I love it!")
+    assert(Functions.combineTokens(List("word1", "word2", "word3"), ", ", '.') === "word1, word2, word3.")
+    assert(Functions.combineTokens(Nil, ",", '!') === "")
+    assert(Functions.combineTokens(null, "", ' ') === "")
 
-    test("combineTokens") {
-      assert(Functions.combineTokens(List("I","love","it"), "!", " ") === "I love it!")
-    }
+  }
 
-    test("listInRange") {
-      assert(Functions.listInRange(List(1, 2, 3, 4), 0, 5) === true)
-    }
+  test("listInRange") {
+    assert(Functions.listInRange(List(1), 1, 1))
+    assert(Functions.listInRange(List(1, 2, 3, 4, 5), 1, 5))
+    assert(!Functions.listInRange(List(5.5, 82.3, 900.8, 23.4, -5.2), 0, 1000))
+    assert(Functions.listInRange(List(), 0, 1))
+    assert(Functions.listInRange(null, -5, 10))
+  }
 
-    test("power") {
-      assert(Functions.power(2, 5) === 32)
+  test("power") {
+    assert(Functions.power(256.128, 0) === 1)
+    assert(Functions.power(1234.567, 1) === 1234.567)
+    assert(Functions.power(12, 5) === math.pow(12, 5))
+    assert(Functions.power(-8, 6) === math.pow(-8, 6))
+    assertThrows[IllegalArgumentException] {
+      Functions.power(81, -2)
     }
+  }
 
 }
