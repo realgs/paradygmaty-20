f132b2e0859739671f8d2e1746347011a6e5e8df
Nazar Pechevystyi <nazarpechka@gmail.com>
2020-10-17 14:55:39 +0200

    Additional test cases

--- src/main/scala/Functions.scala
+++ src/main/scala/Functions.scala
@@ -10,13 +10,16 @@ object Functions {
     else if (words.length == 1) words.head + ending
     else words.head + separator + combineTokens(words.tail, separator, ending)
 
+  @scala.annotation.tailrec
   def listInRange(numbers: List[Double], low: Double, high: Double): Boolean =
     if (numbers == Nil || numbers == null) true
+    else if(low > high) throw new IllegalArgumentException("Incorrect range: low should be < than high")
     else if (numbers.head < low || numbers.head > high) false
     else listInRange(numbers.tail, low, high)
 
   def power(base: Double, exponent: Int): Double =
     if (exponent < 0) throw new IllegalArgumentException("This function doesn't support exponents < 0!")
+    else if(base == 0 && exponent == 0) throw new IllegalArgumentException("Undefined result for base = 0 and exponent = 0")
     else if (exponent != 0) base * power(base, exponent - 1)
     else 1
 
--- src/test/scala/FunctionsTest.scala
+++ src/test/scala/FunctionsTest.scala
@@ -25,13 +25,20 @@ class FunctionsTest extends AnyFunSuite {
     assert(!Functions.listInRange(List(5.5, 82.3, 900.8, 23.4, -5.2), 0, 1000))
     assert(Functions.listInRange(List(), 0, 1))
     assert(Functions.listInRange(null, -5, 10))
+    assertThrows[IllegalArgumentException] {
+      Functions.listInRange(List(1, 2, 3, 4), 4, 1)
+    }
   }
 
   test("power") {
     assert(Functions.power(256.128, 0) === 1)
     assert(Functions.power(1234.567, 1) === 1234.567)
+    assert(Functions.power(1, 1234) === 1)
     assert(Functions.power(12, 5) === math.pow(12, 5))
     assert(Functions.power(-8, 6) === math.pow(-8, 6))
+    assertThrows[IllegalArgumentException] {
+      Functions.power(0, 0)
+    }
     assertThrows[IllegalArgumentException] {
       Functions.power(81, -2)
     }
