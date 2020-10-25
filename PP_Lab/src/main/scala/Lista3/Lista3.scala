e981a91919d2691b9cfc51fea07c894c079d4c75
Klaudia Blazyczek <blazyczek.klaudia@gmail.com>
2020-10-25 18:58:48 +0100

    Implement exc. 4, improved exc. 5. Add tests

--- PP_Lab/src/main/scala/Lista3/Lista3.scala
+++ PP_Lab/src/main/scala/Lista3/Lista3.scala
@@ -2,7 +2,6 @@ package Lista3
 import scala.annotation.tailrec
 
 object Lista3 extends App{
-
   // zadanie 1
   private def isNegative(number: Double): Boolean =
     number<0
@@ -19,6 +18,7 @@ object Lista3 extends App{
     (innerFilterList(list, isNegative), innerFilterList(list, isNegativeAndOdd))
   }
 
+
   // zadanie 2
   def length[A](list: List[A]): Int = {
     @tailrec
@@ -27,6 +27,7 @@ object Lista3 extends App{
       else innerLength (innerList.tail, accum+1)
     innerLength(list, 0)
   }
+
 
   //zadanie 3
   def merge[A](leftList: List[A], rightList: List[A]): List[A] =
@@ -37,7 +38,92 @@ object Lista3 extends App{
       case(Nil, Nil) => Nil
     }
 
+
+  //zadanie 4
+  def appendList[A](leftList: List[A], rightList: List[A]): List[A] = {
+    (leftList, rightList) match {
+      case (_, Nil) => leftList
+      case (Nil, _) => rightList
+      case (head::tail, _) => head::appendList(tail, rightList)
+    }
+  }
+
+  def stringLength(string: String): Int = {
+    @scala.annotation.tailrec
+    def innerStringLength(innerString: String, accum: Int): Int =
+      if (innerString == "") accum
+      else innerStringLength(innerString.tail, accum+1)
+    innerStringLength(string, 0)
+  }
+
+  def sliceStringFromBegin(string: String, endIndex: Int): (String, String) = {
+    val stringSize = stringLength(string)
+    if(endIndex >= stringSize) (string,"")
+    else if (endIndex < 0) ("", string)
+    else {
+      @scala.annotation.tailrec
+      def innerSliceString(word: String, accum: String, end: Int): (String, String) =
+        if(end>=0) innerSliceString(word.tail, accum+word.head, end-1)
+        else (accum, word)
+      innerSliceString(string, "", endIndex)
+    }
+  }
+
+  def ifContainsKey(word: String, key: String): Boolean = {
+    val wordLength = stringLength(word)
+    val keyLength = stringLength(key)
+    if(wordLength < keyLength) false
+    else{
+      @scala.annotation.tailrec
+      def innerContains(leftString: String, rightString: String): Boolean = {
+        if(leftString=="") false
+        else if(leftString.equals(key)) true
+        else if(leftString!="" && rightString!="") innerContains(leftString.tail+rightString.head, rightString.tail)
+        else false
+      }
+      val tuple = sliceStringFromBegin(word, keyLength-1)
+      innerContains(tuple._1, tuple._2)
+    }
+  }
+
+  def doesWordMatchAnyKey(word: String, keyList: List[String]): Boolean = {
+    @scala.annotation.tailrec
+    def innerDoesWordMatchAnyKey(word: String, keyList: List[String]): Boolean =
+      keyList match{
+        case(h::t) => if(ifContainsKey(word, h)) true  else innerDoesWordMatchAnyKey(word, t)
+        case(Nil) => false
+      }
+    innerDoesWordMatchAnyKey(word, keyList)
+  }
+
+  //zwykla rekursja
+  def find(inputList: List[String], keyWordsList: List[String]): List[String] = {
+    def innerFind(inputList: List[String], keyWordsList: List[String]): List[String] =
+      (inputList, keyWordsList) match{
+        case(Nil, _) => List()
+        case(_, Nil) => List()
+        case(headI::tailI, headK::tailK) => if(doesWordMatchAnyKey(headI, keyWordsList)) headI::innerFind(tailI, keyWordsList)
+                                            else innerFind(tailI, keyWordsList)
+      }
+    innerFind(inputList, keyWordsList)
+  }
+
+  //rekursja ogonowa
+  def findTail(inputList: List[String], keyWordsList: List[String]): List[String] = {
+    @scala.annotation.tailrec
+    def innerFind(inputList: List[String], keyWordsList: List[String], accum: List[String]): List[String] =
+      (inputList, keyWordsList) match{
+        case(Nil, _) => accum
+        case(_, Nil) => List()
+        case(headI::tailI, headK::tailK) => if(doesWordMatchAnyKey(headI, keyWordsList)) innerFind(tailI, keyWordsList, appendList(accum, List(headI)))
+                                            else innerFind(tailI, keyWordsList, accum)
+      }
+    innerFind(inputList, keyWordsList, List())
+  }
+
+
   //zadanie 5
+  //rekursja zwykla
   def joinLists[A](firstList: List[A], secondList: List[A], thirdList: List[A]): List[A] = {
     (firstList, secondList, thirdList) match{
       case(headFL::tailFL, _, _) => headFL::joinLists(tailFL, secondList, thirdList)
@@ -45,5 +131,18 @@ object Lista3 extends App{
       case(Nil, Nil, headTL::tailTL) => headTL::joinLists(Nil, Nil, tailTL)
       case(Nil, Nil, Nil) => Nil
     }
+  }
+
+  //rekursja ogonowa
+  def joinListsTail[A](firstList: List[A], secondList: List[A], thirdList: List[A]): List[A] ={
+    @scala.annotation.tailrec
+    def innerJoinListsTail[A](fL: List[A], sL: List[A], tL: List[A], accum: List[A]): List[A] =
+      (fL, sL, tL) match{
+        case(headFL::tailFL, _, _) => innerJoinListsTail(tailFL, sL, tL, appendList(accum, List(headFL)))
+        case(Nil, headSL::tailSL, _)  => innerJoinListsTail(Nil, tailSL, tL, appendList(accum, List(headSL)))
+        case(Nil, Nil, headTL::tailTL) => innerJoinListsTail(Nil, Nil, tailTL, appendList(accum, List(headTL)))
+        case(Nil, Nil, Nil) => accum
+      }
+    innerJoinListsTail(firstList, secondList, thirdList, List())
   }
 }
--- PP_Lab/src/test/scala/Lista3/Lista3Test.scala
+++ PP_Lab/src/test/scala/Lista3/Lista3Test.scala
@@ -10,11 +10,13 @@ class Lista3Test extends FunSuite {
     assert(Lista3.filterList(List(1, 2, 3, 4, -5, 6, 7)) == (List(-5), List(-5)))
   }
 
+
   test("Lista 3 zadanie 2 test"){
     assert(Lista3.length(List(5,4,3,2)) == 4)
     assert(Lista3.length(List()) == 0)
     assert(Lista3.length(List(1)) == 1)
   }
+
 
   test("Lista 3 zadanie 3 test"){
     assert(Lista3.merge(List(5,4,3,2), List(1,2,3,4,5,6)) == List(5,1,4,2,3,3,2,4,5,6))
@@ -24,6 +26,40 @@ class Lista3Test extends FunSuite {
     assert(Lista3.merge(List('a', 'a', 'a', 'o', 'a'), List('l', 'm', 'k', 't')) == List('a', 'l', 'a', 'm', 'a', 'k', 'o', 't', 'a'))
   }
 
+
+  test("Lista 3 zadanie 4 test"){
+    assert(Lista3.stringLength("Ala ma kota") == 11)
+    assert(Lista3.stringLength("") == 0)
+    assert(Lista3.stringLength("  ") == 2)
+    assert(Lista3.stringLength("jakiswyraz") == 10)
+
+    assert(Lista3.sliceStringFromBegin("Ala ma kota", 5) == ("Ala ma", " kota"))
+    assert(Lista3.sliceStringFromBegin("Ala ma kota",0) == ("A", "la ma kota"))
+    assert(Lista3.sliceStringFromBegin("trolek", 3) == ("trol", "ek"))
+    assert(Lista3.sliceStringFromBegin("kot",  2) == ("kot", ""))
+
+    assert(Lista3.ifContainsKey("bufor", "for"))
+    assert(!Lista3.ifContainsKey("for", "bufor"))
+    assert(Lista3.ifContainsKey("ala ma kota", "ala ma kota"))
+    assert(Lista3.ifContainsKey("ala ma kota", "kota"))
+    assert(Lista3.ifContainsKey("ala ma kota", "ma kota"))
+    assert(Lista3.ifContainsKey("notak", "tak"))
+
+    assert(Lista3.doesWordMatchAnyKey("notak", List("rak", "opak", "tak", "ok")))
+    assert(Lista3.doesWordMatchAnyKey("alpaka", List("mak", "tak", "paka", "ok")))
+    assert(Lista3.doesWordMatchAnyKey("ok", List("rak", "opak", "tak", "ok")))
+    assert(!Lista3.doesWordMatchAnyKey("olej", List("rak", "opak", "tak", "ok")))
+
+    assert(Lista3.find(List("ala", "ma", "kota", "mama"), List("al", "ma", "kto", "amam")) == List("ala", "ma", "mama"))
+    assert(Lista3.find(List("index0169", "index0168202", "index0168211", "index0168210", "index0169222", "index0169224"), List("index0168")) == List("index0168202", "index0168211", "index0168210"))
+    assert(Lista3.find(List("alek", "ma", "kota", "mama"), List("ala", "amam")) == List())
+
+    assert(Lista3.findTail(List("ala", "ma", "kota", "mama"), List("al", "ma", "kto", "amam")) == List("ala", "ma", "mama"))
+    assert(Lista3.findTail(List("index0169", "index0168202", "index0168211", "index0168210", "index0169222", "index0169224"), List("index0168")) == List("index0168202", "index0168211", "index0168210"))
+    assert(Lista3.findTail(List("alek", "ma", "kota", "mama"), List("ala", "amam")) == List())
+  }
+
+
   test("Lista 3 zadanie 5 test"){
     assert(Lista3.joinLists(List(1,2,3), List(4,5,6), List(7,8,9)) == List(1,2,3,4,5,6,7,8,9))
     assert(Lista3.joinLists(List('a', 'l', 'a'), List('m', 'a'), List('k', 'o', 't', 'a')) == List('a', 'l', 'a', 'm', 'a', 'k', 'o', 't', 'a'))
@@ -31,5 +67,15 @@ class Lista3Test extends FunSuite {
     assert(Lista3.joinLists(List(), List("good", "job"), List()) == List("good", "job"))
     assert(Lista3.joinLists(List("good", "job"), List(), List()) == List("good", "job"))
     assert(Lista3.joinLists(List("good", "job"), List(), List("have", "fun")) == List("good", "job", "have", "fun"))
+
+    assert(Lista3.appendList(List(1,2,3), List(5,6))== List(1,2,3,5,6))
+    assert(Lista3.appendList(List('a', 'l', 'a', ' '), List('o', 'l', 'a'))== List('a', 'l', 'a', ' ', 'o', 'l', 'a'))
+
+    assert(Lista3.joinListsTail(List(1,2,3), List(4,5,6), List(7,8,9)) == List(1,2,3,4,5,6,7,8,9))
+    assert(Lista3.joinListsTail(List('a', 'l', 'a'), List('m', 'a'), List('k', 'o', 't', 'a')) == List('a', 'l', 'a', 'm', 'a', 'k', 'o', 't', 'a'))
+    assert(Lista3.joinListsTail(List(), List(), List("good", "job")) == List("good", "job"))
+    assert(Lista3.joinListsTail(List(), List("good", "job"), List()) == List("good", "job"))
+    assert(Lista3.joinListsTail(List("good", "job"), List(), List()) == List("good", "job"))
+    assert(Lista3.joinListsTail(List("good", "job"), List(), List("have", "fun")) == List("good", "job", "have", "fun"))
   }
 }
