bab511c3b8f3a2a63a2984409aefa67b8b57e157
Puchatek <kuba.chmielow@gmail.com>
2020-10-27 16:35:35 +0100

    add 4th tail rec version

--- L3.scala
+++ L3.scala
@@ -1,7 +1,4 @@
 //zadanie 1
-
-//zadanie 1
-
 def listDivision(list: List[Int]): (List[Int], List[Int]) = {
   def firstCrit(arg_list: List[Int]): List[Int] =
     if (arg_list == Nil) Nil
@@ -56,8 +53,6 @@ merge(List(), List(10, 20, 30, 40)) == List(10, 20 ,30 ,40)
 merge(List(4.5, 6.8), List(1.2, 20, 30, 40, 9.9)) == List(4.5, 1.2, 6.8, 20, 30, 40, 9.9)
 
     
-
-
 //zadanie 5
 def joinLists[T](list1: List[T], list2: List[T], list3: List[T]): List[T] = {
 
@@ -79,54 +74,161 @@ joinLists(List(2.5, 2.6), List(1.0), List()) == List(2.5, 2.6, 1.0)
 joinLists(List(2.5, 2.6), List(1.0), List(90)) == List(2.5, 2.6, 1.0, 90)
 
 
+//zadanie 5 - tail recursion
+import scala.annotation.tailrec
 
-def contains(elem: String, key: String) = {
-  def contains_inner(elem: String, key: String, fullKey: String): Boolean =  //current character: elem.head, key.head
-    if (elem.tail == "" && key.tail != "") false
-    else if (key.tail == "" && key.head != 0 && elem.head == key.head) true  //ostatni z key
-    else if (key.head == elem.head) contains_inner(elem.tail, key.tail, key)
-    else contains_inner(elem.tail, fullKey, fullKey)
-  contains_inner(elem, key, key)
+def joinListsTail[T](list1: List[T], list2: List[T], list3: List[T]): List[T] = {
+
+  // O(n), ale stos wywołań: 1, dzięki optymalizacji kompilatora
+  @tailrec
+  def innerJoinLists[T](list1: List[T], list2: List[T], acc: List[T] = Nil): List[T] =
+    list1 match{
+      case Nil => list2
+      case head :: tail => innerJoinLists(tail, head::list2)
+    }
+
+  // O(n)
+  @tailrec
+  def reverse(list: List[T], acc: List[T] = Nil): List[T] =
+    list match{
+      case Nil => acc
+      case head :: tail => reverse(tail, head::acc)
+    }
+
+  innerJoinLists(reverse(innerJoinLists(reverse(list1), list2)), list3)
 }
 
+joinListsTail(List(1), List(2), List(3)) == List(1, 2, 3)
+joinListsTail(List("Ala"), List("ma"), List("kota")) == List("Ala", "ma", "kota")
+joinListsTail(List(), List(), List("test")) == List("test")
+joinListsTail(List(), List(), List()) == List()
+joinListsTail(List(2.5, 2.6), List(1.0), List()) == List(2.5, 2.6, 1.0)
+joinListsTail(List(2.5, 2.6), List(1.0), List(90)) == List(2.5, 2.6, 1.0, 90)
+
+
+//zadanie 4
 def find[T](list: List[T], listOfKeys: List[T]) = {
+  def contains(elem: String, key: String) = {
+    def contains_inner(elem: String, key: String, fullKey: String): Boolean =
+      if (elem.tail == "" && key.tail != "") false
+      else if (key.tail == "" && key.head != 0 && elem.head == key.head) true
+      else if (key.head == elem.head) contains_inner(elem.tail, key.tail, key)
+      else contains_inner(elem.tail, fullKey, fullKey)
+    contains_inner(elem, key, key)
+  }
+    //  func contains() tests
+//  contains("s", "tra") == false
+//  contains("trata", "tra") == true
+//  contains("456", "567") == false
+//  contains("456", "56") == true
+//  contains("456", "4") == true
+//  contains("alicja", "alicja") == true
+//  contains("alicja", "lic") == true
+//  contains("alicja", "lic9") == false
+//  contains("12345", "239") == false
+//  contains("zamek", "ame") == true
+//  contains("zamek", "ameh") == false
+
   def findByOneKey[T](list2: List[T], key: T): List[String] =
     if (list2 == Nil) Nil
     else if (contains(list2.head.toString, key.toString)) list2.head.toString :: findByOneKey(list2.tail, key)
     else findByOneKey(list2.tail, key)
 
-  def findingByAllKeysLoop[T](list3: List[T], listOfKeys: List[T]): List[List[String]] =
+  def findingByAllKeysLoop[T](list3: List[T], listOfKeys: List[T]): List[String] =
     if (listOfKeys == Nil) Nil
-    else findByOneKey(list3, listOfKeys.head) :: findingByAllKeysLoop(list3, listOfKeys.tail)
+    else joinLists(findingByAllKeysLoop(list3, listOfKeys.tail), findByOneKey(list3, listOfKeys.head))
 
   def listContains(list: List[String], key: String): Boolean =
     if (list == Nil) false
     else list.head == key || listContains(list.tail, key)
 
-  def mergeAndDeleteRepeats(list4: List[List[String]]): List[String] = {
-    if (list4 == Nil) list5
-    else if (list5 == Nil) list4
-    else if (!listContains(list5, list4.head)) list4.head :: mergeAndDeleteRepeats(list4.tail, list5)
-    else mergeAndDeleteRepeats(list4.tail, list5)
+  def deleteRepeats(list: List[String], result: List[String] = Nil): List[String] =
+    list match{
+      case Nil => result
+      case head :: tail if listContains(result, head) => deleteRepeats(tail, result)
+      case head :: tail => deleteRepeats(tail, head :: result)
+    }
+
+  def joinLists[T](list1: List[T], list2: List[T]): List[T] = {
+    if (list1 == Nil) list2
+    else if (list2 == Nil) list1
+    else list1.head :: joinLists(list1.tail, list2)
   }
 
-  findingByAllKeysLoop(list, listOfKeys)
-
+  deleteRepeats(findingByAllKeysLoop(list, listOfKeys))
 }
 
-find(List("ala", "alicja", "alina"), List("ali", "al"))
-find(List("456", "4567", "4588"), List("56"))
+find(List("ala", "alicja", "alina","oskar"), List("a")) == List("oskar", "alina", "alicja", "ala")
+find(List(), List("a")) == List()
+find(List("student6745", "student6666", "student9967"), List()) == List()
+find(List("student6745", "student6666", "student9967"), List("67")) == List("student9967", "student6745")
+find(List("student6745", "student6666", "student9967"), List("67","student67")) == List("student9967", "student6745")
+find(List("student6745", "student6666", "student9967"), List("67","6")) == List("student9967", "student6666", "student6745")
+find(List("student6745", "student6666", "student9967"), List("65","66")) == List("student6666")
+find(List("student6745", "student6666", "student9967"), List("udent")) == List("student9967", "student6666", "student6745")
+find(List("student6745", "student6666", "student9967"), List("u")) == List("student9967", "student6666", "student6745")
+find(List("student6745", "student6666", "student9967"), List("s")) == List("student9967", "student6666", "student6745")
+find(List("456", "4567", "4588"), List("56")) == List("4567", "456")
+find(List("szafa", "szafka", "szafeczka", "kafka", "agrafka"), List("afka")) == List("agrafka", "kafka", "szafka")
+find(List("szafa", "szafka", "szafeczka", "kafka", "agrafka","szuflada"),List("afka", "szaf", "lada"))  == List("agrafka", "kafka", "szafeczka", "szafka", "szafa", "szuflada")
+find(List("szafa", "szafka", "szafeczka", "kafka", "agrafka", "szuflada"),List("flada", "samochód", "dom", "pies")) == List("szuflada")
 
 
+//zadanie 4 tail recursion
+def findTail[T](list: List[T], listOfKeys: List[T]) = {
+  def contains(elem: String, key: String) = {
+    def contains_inner(elem: String, key: String, fullKey: String): Boolean =
+      if (elem.tail == "" && key.tail != "") false
+      else if (key.tail == "" && key.head != 0 && elem.head == key.head) true
+      else if (key.head == elem.head) contains_inner(elem.tail, key.tail, key)
+      else contains_inner(elem.tail, fullKey, fullKey)
+    contains_inner(elem, key, key)
+  }
 
-contains("s", "tra") == false
-contains("trata", "tra") == true
-contains("456", "567") == false
-contains("456", "56") == true
-contains("456", "4") == true
-contains("alicja", "alicja") == true
-contains("alicja", "lic") == true
-contains("alicja", "lic9") == false
-contains("12345", "239") == false
-contains("zamek", "ame") == true
-contains("zamek", "ameh") == false
+  @tailrec
+  def findByOneKey[T](list2: List[T], key: T, acc: List[String] = Nil): List[String] =
+    if (list2 == Nil) acc
+    else if (contains(list2.head.toString, key.toString)) findByOneKey(list2.tail, key, list2.head.toString::acc)
+    else findByOneKey(list2.tail, key, acc)
+
+  def findingByAllKeysLoop[T](list3: List[T], listOfKeys: List[T]): List[String] =
+    if (listOfKeys == Nil) Nil
+    else joinLists(findingByAllKeysLoop(list3, listOfKeys.tail), findByOneKey(list3, listOfKeys.head))
+
+  @tailrec
+  def listContains(list: List[String], key: String, acc: Boolean = false): Boolean =
+    if (list == Nil) acc
+    else  listContains(list.tail, key, acc || list.head == key)
+
+  @tailrec
+  def deleteRepeats(list: List[String], result: List[String] = Nil): List[String] =
+    list match{
+      case Nil => result
+      case head :: tail if listContains(result, head) => deleteRepeats(tail, result)
+      case head :: tail => deleteRepeats(tail, head :: result)
+    }
+
+  @tailrec
+  def joinLists[T](list1: List[T], list2: List[T], acc: List[T] = Nil): List[T] =
+    list1 match{
+      case Nil => list2
+      case head :: tail => joinLists(tail, head::list2)
+    }
+
+  deleteRepeats(findingByAllKeysLoop(list, listOfKeys))
+}
+
+findTail(List("ala", "alicja", "alina","oskar"), List("a")) == List("ala", "alicja", "alina","oskar")
+findTail(List(), List("a")) == List()
+findTail(List("student6745", "student6666", "student9967"), List()) == List()
+findTail(List("student6745", "student6666", "student9967"), List("67")) == List("student6745", "student9967")
+findTail(List("student6745", "student6666", "student9967"), List("67","student67")) == List("student9967", "student6745")
+findTail(List("student6745", "student6666", "student9967"), List("67","6")) == List("student9967", "student6666", "student6745")
+findTail(List("student6745", "student6666", "student9967"), List("65","66")) == List("student6666")
+findTail(List("student6745", "student6666", "student9967"), List("udent")) == List("student6745", "student6666", "student9967")
+findTail(List("student6745", "student6666", "student9967"), List("u")) == List("student6745", "student6666", "student9967")
+findTail(List("student6745", "student6666", "student9967"), List("s")) == List("student6745", "student6666", "student9967")
+findTail(List("456", "4567", "4588"), List("56")) == List("456", "4567")
+findTail(List("szafa", "szafka", "szafeczka", "kafka", "agrafka"), List("afka")) == List("szafka", "kafka", "agrafka")
+findTail(List("szafa", "szafka", "szafeczka", "kafka", "agrafka", "szuflada"),List("afka", "szaf", "lada")) == List("kafka", "agrafka", "szuflada", "szafeczka", "szafka", "szafa")
+findTail(List("szafa", "szafka", "szafeczka", "kafka", "agrafka", "szuflada"),List("flada", "samochód", "dom", "pies")) == List("szuflada")
