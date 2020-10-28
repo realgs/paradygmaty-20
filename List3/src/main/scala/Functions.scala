640909ad8782dfea8fa80327aed1fc9f6333bdb8
piXel2K <rafal.piwnik@gmail.com>
2020-10-28 21:05:03 +0100

    Implemented custom reverse

--- List3/src/main/scala/Functions.scala
+++ List3/src/main/scala/Functions.scala
@@ -1,16 +1,29 @@
-object Functions {
-  import scala.annotation.tailrec
+import scala.annotation.tailrec
 
-  val isEven = (x: Int) => x % 2 == 1
+object Functions {
+  private val mod = (x: Int, n: Int) => (x % n + n) % n
+  private val isOdd = (x: Int) => mod(x, 2) == 1
+
+  // Write fold left here
+
+  private def reverse [A](xs: List[A]): List[A] = {
+    def auxReverse: (List[A], List[A]) => List[A] = (xs, acc) => {
+      xs match {
+        case Nil => acc
+        case h::t => auxReverse(t, h :: acc)
+      }
+    }
+    auxReverse(xs, List())
+  }
 
   val split: List[Int] => (List[Int], List[Int])= xs => {
     @tailrec
     def auxSplit(xs: List[Int], left: List[Int], right: List[Int]): (List[Int], List[Int]) = {
       xs match {
-        case Nil => (left.reverse, right.reverse)
+        case Nil => (reverse(left), reverse(right))
         case h :: t => {
           if (h < 0) {
-            if (h % 2 == -1) {
+            if (isOdd(h)) {
               auxSplit(t, h :: left, h :: right)
             } else {
               auxSplit(t, h :: left, right)
@@ -23,6 +36,4 @@ object Functions {
     }
     auxSplit(xs, List(), List())
   }
-
-
 }
