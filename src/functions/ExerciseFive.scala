f9f75a659d539290474a797c17ceab9cdc2f72e7
arko0x <arkadiusz88084@gmail.com>
2020-10-28 21:23:39 +0100

    add exercise five

---
+++ src/functions/ExerciseFive.scala
@@ -0,0 +1,29 @@
+package functions
+
+import scala.annotation.tailrec
+import utilities.UtilityFunctions.reverseList
+
+object ExerciseFive {
+  def joinLists[A](list1: List[A], list2: List[A], list3: List[A]): List[A] = {
+    (list1, list2, list3) match {
+      case (Nil, Nil, Nil) => Nil
+      case (h1 :: t1, _, _) => h1 :: joinLists(t1, list2, list3)
+      case (_, h2 :: t2, _) => h2 :: joinLists(list1, t2, list3)
+      case (_, _, h3 :: t3) => h3 :: joinLists(list1, list2, t3)
+    }
+  }
+
+  def joinListsWithTailRecursion[A](list1: List[A], list2: List[A], list3: List[A]): List[A] = {
+    @tailrec
+    def joinListsTail(accum: List[A], list1: List[A], list2: List[A], list3: List[A]): List[A] = {
+      (list1, list2, list3) match {
+        case (Nil, Nil, Nil) => accum
+        case (_, _, h3 :: t3) => joinListsTail(h3 :: accum, list1, list2, t3)
+        case (_, h2 :: t2, _) => joinListsTail(h2 :: accum, list1, t2, list3)
+        case (h1 :: t1, _, _) => joinListsTail(h1 :: accum, t1, list2, list3)
+      }
+    }
+    joinListsTail(Nil, reverseList(list1), reverseList(list2), reverseList(list3))
+  }
+
+}
