014ba469bbc8c86f141d99eed508e7cd6f7b4beb
piXel2K <rafal.piwnik@gmail.com>
2020-10-28 20:28:06 +0100

    Started work on task 1

--- List3/src/main/scala/Functions.scala
+++ List3/src/main/scala/Functions.scala
@@ -1,3 +1,28 @@
 object Functions {
+  import scala.annotation.tailrec
+
+  val isEven = (x: Int) => x % 2 == 1
+
+  val split: List[Int] => (List[Int], List[Int])= xs => {
+    @tailrec
+    def auxSplit(xs: List[Int], left: List[Int], right: List[Int]): (List[Int], List[Int]) = {
+      xs match {
+        case Nil => (left.reverse, right.reverse)
+        case h :: t => {
+          if (h < 0) {
+            if (h % 2 == -1) {
+              auxSplit(t, h :: left, h :: right)
+            } else {
+              auxSplit(t, h :: left, right)
+            }
+          } else {
+            auxSplit(t, left, right)
+          }
+        }
+      }
+    }
+    auxSplit(xs, List(), List())
+  }
+
 
 }
--- List3/src/test/scala/FunctionsTest.scala
+++ List3/src/test/scala/FunctionsTest.scala
@@ -1,5 +1,5 @@
 import org.scalatest.FunSuite
 
 class FunctionsTest extends FunSuite {
-
+  assert(Functions.split(List(-3, -6, 8, -9, 13)) === (List(-3, -6, -9),List(-3, -9)))
 }
