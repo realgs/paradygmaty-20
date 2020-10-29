6f098e5043cf7e08860b0dfa376c37415cdeac34
piXel2K <rafal.piwnik@gmail.com>
2020-10-29 11:57:13 +0100

    Rewritten split

--- List3/src/main/scala/Functions.scala
+++ List3/src/main/scala/Functions.scala
@@ -4,18 +4,29 @@ object Functions {
   private val mod = (x: Int, n: Int) => (x % n + n) % n
   private val isOdd = (x: Int) => mod(x, 2) == 1
 
-  def fold_left [A, B](xs: List[A], accu: B)(f: (A, B) => B): B = {
+  @tailrec
+  def fold_left[A, B](xs: List[A], accu: B)(f: (A, B) => B): B = {
     xs match {
       case Nil => accu
-      case h::t => fold_left(t, f(h, accu)) (f)
+      case h :: t => fold_left(t, f(h, accu))(f)
     }
   }
 
-  def reverse [A](xs: List[A]): List[A] = {
+  def reverse[A](xs: List[A]): List[A] = {
     fold_left(xs, List[A]())(_ :: _)
   }
 
-  val split: List[Int] => (List[Int], List[Int])= xs => {
+  val split: List[Int] => (List[Int], List[Int]) = xs => {
+    val (left, right) = fold_left(xs, (List[Int](), List[Int]()))((x, accu) => {
+      if (x < 0) {
+        if (isOdd(x)) (x :: accu._1, x :: accu._2)
+        else (x :: accu._1, accu._2)
+      } else accu
+    })
+    (reverse(left), reverse(right))
+  }
+
+  val splitOld: List[Int] => (List[Int], List[Int]) = xs => {
     @tailrec
     def auxSplit(xs: List[Int], left: List[Int], right: List[Int]): (List[Int], List[Int]) = {
       xs match {
@@ -33,6 +44,7 @@ object Functions {
         }
       }
     }
+
     auxSplit(xs, List(), List())
   }
 }
