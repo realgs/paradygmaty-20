ae5d094b9fdc6b9304360582240b2241ad277f06
Kic4k <lukaskickic@gmail.com>
2020-10-30 18:06:19 +0100

    task 2 test added

--- L3/l3.scala
+++ L3/l3.scala
@@ -1,5 +1,9 @@
-
-
+//2
+/*
+zlozonosc:
+pamieciowa:O(n), n-długosc podanej listy
+obliczeniowa:O(1),
+*/
 def listLenght[A](list:List[A]):Int = {
   def help(acc:Int,list:List[A]):Int = list match {
     case Nil => acc
@@ -7,3 +11,8 @@ def listLenght[A](list:List[A]):Int = {
   }
   help(0,list)
 }
+//2 test
+listLenght(List())
+listLenght(List(1))
+listLenght(List(1,2))
+listLenght(List('a','b','c'))
