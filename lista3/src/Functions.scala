03fffc93343b50f225916a870636ebeef5f84b8c
Milena Mazur <milenamazur00@gmail.com>
2020-10-30 20:37:44 +0100

    task 1 implemented

--- lista3/src/Functions.scala
+++ lista3/src/Functions.scala
@@ -1,42 +1,56 @@
 import scala.annotation.tailrec
 
 class Functions {
+  //zadanie 1
+  def split(list: List[Int]): (List[Int], List[Int]) = {
+    @tailrec
+    def splitIter(list: List[Int], negative: List[Int], negativeAndOdd: List[Int]): (List[Int], List[Int]) = {
+      if (list == Nil) (reverse(negative), reverse(negativeAndOdd))
+      else if (list.head < 0) {
+        if (list.head % 2 == -1) splitIter(list.tail, list.head :: negative, list.head :: negativeAndOdd)
+        else splitIter(list.tail, list.head :: negative, negativeAndOdd)
+      }
+      else splitIter(list.tail, negative, negativeAndOdd)
+    }
+    splitIter(list, Nil, Nil)
+  }
+
+  //zadanie 2
   def length[A](list: List[A]): Int = {
     @tailrec
-    def lengthHelper(list: List[A], length: Int): Int = {
+    def lengthIter (list: List[A], length: Int): Int = {
       if (list == Nil) length
-      else lengthHelper(list.tail, length + 1)
+      else lengthIter(list.tail, length + 1)
     }
-
-    lengthHelper(list, 0)
+    lengthIter(list, 0)
   }
 
+  //zadanie 3
   def merge[A](firstList: List[A], secondList: List[A]): List[A] = {
     @tailrec
-    def helper(firstList: List[A], secondList: List[A], returnList: List[A]): List[A] = {
+    def mergeIter(firstList: List[A], secondList: List[A], returnList: List[A]): List[A] = {
       (firstList, secondList) match {
         case (Nil, Nil) => reverse(returnList)
-        case (head :: tail, Nil) => helper(tail, secondList, head :: returnList)
-        case (Nil, head :: tail) => helper(firstList, tail, head :: returnList)
-        case (head :: tail, _) => helper(secondList, tail, head :: returnList)
+        case (head :: tail, Nil) => mergeIter(tail, secondList, head :: returnList)
+        case (Nil, head :: tail) => mergeIter(firstList, tail, head :: returnList)
+        case (head :: tail, _) => mergeIter(secondList, tail, head :: returnList)
       }
     }
-
-    helper(firstList, secondList, Nil)
+    mergeIter(firstList, secondList, Nil)
   }
 
+  //zadanie 5
   def joinLists_TailRec[A](firstList: List[A], secondList: List[A], thirdList: List[A]): List[A] = {
     @tailrec
-    def helper(firstList: List[A], secondList: List[A], thirdList: List[A], returnList: List[A]): List[A] = {
+    def joinLists_TailRecIter(firstList: List[A], secondList: List[A], thirdList: List[A], returnList: List[A]): List[A] = {
       (firstList, secondList, thirdList) match {
-        case (head :: tail, _, _) => helper(tail, secondList, thirdList, head :: returnList)
-        case (Nil, head :: tail, _) => helper(firstList, tail, thirdList, head :: returnList)
-        case (Nil, Nil, head :: tail) => helper(firstList, secondList, tail, head :: returnList)
+        case (head :: tail, _, _) => joinLists_TailRecIter(tail, secondList, thirdList, head :: returnList)
+        case (Nil, head :: tail, _) => joinLists_TailRecIter(firstList, tail, thirdList, head :: returnList)
+        case (Nil, Nil, head :: tail) => joinLists_TailRecIter(firstList, secondList, tail, head :: returnList)
         case (Nil, Nil, Nil) => reverse(returnList)
       }
     }
-
-    helper(firstList, secondList, thirdList, Nil)
+    joinLists_TailRecIter(firstList, secondList, thirdList, Nil)
   }
 
   def joinLists[A](firstList: List[A], secondList: List[A], thirdList: List[A]): List[A] = {
@@ -47,14 +61,13 @@ class Functions {
     }
   }
 
-
+  //own reverse function
   def reverse[A](list: List[A]): List[A] = {
     @tailrec
-    def helper(list: List[A], rev: List[A]): List[A] = {
+    def reverseIter(list: List[A], rev: List[A]): List[A] = {
       if (list == Nil) rev
-      else helper(list.tail, list.head :: rev)
+      else reverseIter(list.tail, list.head :: rev)
     }
-
-    helper(list, Nil)
+    reverseIter(list, Nil)
   }
 }
