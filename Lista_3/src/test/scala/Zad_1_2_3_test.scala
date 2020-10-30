3de39d319a1b61009525453f8f08435adfec296a
LeviSforza <3monika100@gmail.com>
2020-10-30 23:52:28 +0100

    Add some changes to zad 1, 2, 3, 5

--- Lista_3/src/test/scala/Zad_1_2_3_test.scala
+++ Lista_3/src/test/scala/Zad_1_2_3_test.scala
@@ -1,3 +1,41 @@
-class Zad_1_2_3_test {
+import org.scalatest.funsuite.AnyFunSuite
+
+class Zad_1_2_3_test extends AnyFunSuite{
+
+  val zad_test = new Zad_1_2_3
+
+  //zadanie 1
+  test("Test not empty lists; findNegativeNumbers() function") {
+    assert(zad_test.findNegativeNumbers(List(-3, -6, 8, -9, 13)) == (List(-3, -6, -9), List(-3, -9)))
+    assert(zad_test.findNegativeNumbers(List(0, 1, 2, 3)) == (Nil, Nil))
+    assert(zad_test.findNegativeNumbers(List(-4, -6, -10)) == (List(-4, -6, -10), Nil))
+    assert(zad_test.findNegativeNumbers(List(-3, -5, -7)) == (List(-3, -5, -7), List(-3, -5, -7)))
+  }
+
+  test("Test empty list; findNegativeNumbers() function") {
+    assert(zad_test.findNegativeNumbers(Nil) == (Nil, Nil))
+  }
+
+  //zadanie 2
+  test("Test not empty list; findLength() function") {
+    assert(zad_test.findLength(List(5, 4, 3, 2)) == 4)
+    assert(zad_test.findLength(List(true, false, false)) == 3)
+    assert(zad_test.findLength(List("Ala ma", " kota.")) == 2)
+  }
+
+  test("Test empty list; findLength() function") {
+    assert(zad_test.findLength(Nil) == 0)
+  }
+
+  //zadanie 3
+  test("Test not empty list; mixLists() function") {
+    assert(zad_test.mixLists(List(5, 4, 3, 2), List(1, 2, 3, 4, 5, 6)) == List(5, 1, 4, 2, 3, 3, 2, 4, 5, 6))
+    assert(zad_test.mixLists(List("Ala", "burego"), List("ma", "kota")) == List("Ala", "ma", "burego", "kota"))
+    assert(zad_test.mixLists(List(true, true, false), Nil) == List(true, true, false))
+  }
+
+  test("Test empty list; mixLists() function") {
+    assert(zad_test.mixLists(Nil, Nil) == Nil)
+  }
 
 }
