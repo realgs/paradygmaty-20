253d80c7a045bba5d4e4290bb5fa2f8d4f3a7321
TsundereMixErr <mlibura@interia.pl>
2020-10-30 14:51:09 +0100

    L3 added tail recursion

--- pp_lab/src/l3.scala
+++ pp_lab/src/l3.scala
@@ -23,57 +23,58 @@ object l3 {
   }
 
   //ZADANIE 3
-  def combine2[A](xs1: List[A], xs2: List[A]): List[A] =
-    if (xs1 == Nil)
-      if (xs2 == Nil) Nil
-      else xs2.head :: combine2(xs1, xs2.tail)
-    else xs1.head :: combine2(xs2, xs1.tail)
+  def combine2[A](xs1: List[A], xs2: List[A]): List[A] = {
+    @tailrec
+    def combine2Rec[A](xs1: List[A], xs2: List[A], res: List[A]): List[A] = {
+      (xs1, xs2) match {
+        case (Nil, Nil) => myReverse(res)
+        case (h1 :: t1, h2 :: t2) => combine2Rec(t1, t2, h2 :: h1 :: res)
+        case (Nil, h :: t) => combine2Rec(xs1, t, h :: res)
+        case (h :: t, Nil) => combine2Rec(t, xs2, h :: res)
+      }
+    }
+    combine2Rec(xs1, xs2, List())
+  }
+
 
   //ZADANIE 4
   def find(xs: List[String], x: String): List[String] = {
     if (xs == Nil) xs
-    else if (xs.head.contains(x)) xs.head :: find(xs.tail, x)
+    else if (myContains(xs.head, x)) xs.head :: find(xs.tail, x)
     else find(xs.tail, x)
   }
 
   def findTail(xs: List[String], x: String): List[String] = {
     @tailrec
-    def findTailRec(xs: List[String], x: String, curr: List[String]): List[String] = {
-      if (xs == Nil) curr
-      else if (xs.head.contains(x)) findTailRec(xs.tail, x, xs.head :: curr)
-      else findTailRec(xs.tail, x, curr)
+    def findTailRec(xs: List[String], x: String, res: List[String]): List[String] = {
+      if (xs == Nil) res
+      else if (myContains(xs.head, x)) findTailRec(xs.tail, x, xs.head :: res)
+      else findTailRec(xs.tail, x, res)
     }
     findTailRec(xs, x, List())
   }
+
   def findMultiple(xs: List[String], ys: List[String]): List[String] = {
     var flag = false
     if (xs == Nil) return Nil
-    else for (str <- ys) if (xs.head.contains(str)) flag = true
+    else for (str <- ys) if (myContains(xs.head, str)) flag = true
     if (flag) xs.head :: findMultiple(xs.tail, ys)
     else findMultiple(xs.tail, ys)
   }
 
-  /*def findMultipleTail(xs: List[String], ys: List[String]): List[String] = {
+  def findMultipleTail(xs: List[String], ys: List[String]): List[String] = {
     @tailrec
-    def findMultipleRec(xs: List[String], ys: List[String], result: List[String]): List[String] =
-      (xs, ys) match {
-        case (Nil, _) => myReverse(result)
-        case (_ :: tl, Nil) => findMultipleRec(tl, ys, result)
-        case (h1 :: t1, h2 :: t2) => if (contains(hd1, hd2)) innerFindManyTailRec(tl1, patterns, patterns, hd1 :: resultList)
-        else findMultipleRec(xs, t2, ys1, result)
+    def findMultipleRec(xs: List[String], ys: List[String], res: List[String], ys1: List[String]): List[String] =
+      (xs, ys1) match {
+        case (Nil, _) => myReverse(res)
+        case (_ :: tl, Nil) => findMultipleRec(tl, ys, res, ys)
+        case (h1 :: t1, h2 :: t2) => if (myContains(h1, h2)) findMultipleRec(t1, ys, h1 :: res, ys)
+        else findMultipleRec(xs, ys, res, t2)
       }
-    findMultipleRec(xs, ys, List())
-  }*/
+    findMultipleRec(xs, ys, List(), ys)
+  }
 
   //ZADANIE 5
-  /*def combine3[A](xs1: List[A], xs2: List[A], xs3: List[A]): List[A] =
-    if (xs1 == Nil)
-      if (xs2 == Nil)
-        if (xs3 == Nil) Nil
-        else xs3.head :: combine3(xs1, xs2, xs3.tail)
-      else xs2.head :: combine3(xs3, xs1, xs2.tail)
-    else xs1.head :: combine3(xs2, xs3, xs1.tail)(*/
-
   def combine3[A](xs1: List[A], xs2: List[A], xs3: List[A]): List[A] =
     if (xs1 == Nil)
       if (xs2 == Nil)
@@ -93,11 +94,27 @@ object l3 {
     myReverse(combine3Rec(xs1, xs2, xs3, List().reverse))
   }
 
+  //METODY POMOCNICZE
   def myReverse[A](xs: List[A]): List[A] = {
-    if (xs == Nil) Nil
-    else myReverse(xs.tail) ::: List(xs.head)
+    @tailrec
+    def myReverseRec[A](xs: List[A], res: List[A]): List[A] = {
+      if (xs == Nil) res
+      else myReverseRec(xs.tail, xs.head :: res)
+    }
+    myReverseRec(xs, List())
+  }
 
-  /*def myContains(x: String, y: String): Boolean =
-
-  }*/
+    def myContains(x: String, y: String): Boolean = {
+      @tailrec
+      def myContainsRec(x1: String, y1: String, x: String, y: String): Boolean = {
+        (x1, y1) match {
+          case ("", "") => true
+          case ("", _) => false
+          case (_, "") => true
+          case (_, _) => if (x1.head == y1.head) myContainsRec(x1.tail, y1.tail, x, y)
+          else myContainsRec(x.tail, y, x.tail, y)
+        }
+      }
+      myContainsRec(x, y, x, y)
+    }
 }
