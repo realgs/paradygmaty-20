b7e3dbb9f14180a82d4a53d8cdcba42fd74d28f7
Milena Mazur <milenamazur00@gmail.com>
2020-10-30 20:53:59 +0100

    task 4 implemented

--- lista3/src/Functions.scala
+++ lista3/src/Functions.scala
@@ -18,7 +18,7 @@ class Functions {
   //zadanie 2
   def length[A](list: List[A]): Int = {
     @tailrec
-    def lengthIter (list: List[A], length: Int): Int = {
+    def lengthIter(list: List[A], length: Int): Int = {
       if (list == Nil) length
       else lengthIter(list.tail, length + 1)
     }
@@ -37,6 +37,79 @@ class Functions {
       }
     }
     mergeIter(firstList, secondList, Nil)
+  }
+
+  //zadanie 4
+  @tailrec
+  private def matchStringToElementHelper(argument: String, element: String): Boolean = {
+    (argument, element) match {
+      case ("", "") => return true
+      case ("", _) => return false
+      case (_, "") => return true
+      case (_, _) =>
+        if (argument.head == element.head) matchStringToElementHelper(argument.tail, element.tail)
+        else matchStringToElementHelper(argument.tail, element)
+    }
+  }
+
+  def matchStringToElement(argument: String, element: String): Boolean = {
+    if (stringLength(argument) == 0 && stringLength(element) == 0) return true
+    else if (argument.length < element.length) return false;
+    else matchStringToElementHelper(argument, element)
+  }
+
+  def find_Tail(list: List[String], element: String): List[String] = {
+    @tailrec
+    def find_TailIter(list: List[String], element: String, returnList: List[String]): List[String] = {
+      if (list == Nil) reverse(returnList)
+      else if (matchStringToElement(list.head, element)) find_TailIter(list.tail, element, list.head :: returnList)
+      else find_TailIter(list.tail, element, returnList)
+    }
+    find_TailIter(list, element, Nil)
+  }
+
+  def findEach_Tail(list: List[String], elements: List[String]): List[String] = {
+    @tailrec
+    def findEach_Tail_Iter(list: List[String], elements: List[String], returnList: List[String]): List[String] = {
+      if (elements == Nil) returnList
+      else {
+        @tailrec
+        def addMatchesToReturnList(matches: List[String], returnList: List[String]): List[String] = {
+          if (matches == Nil) returnList
+          else if (!contains(returnList, matches.head)) addMatchesToReturnList(matches.tail, matches.head :: returnList)
+          else addMatchesToReturnList(matches.tail, returnList)
+        }
+        findEach_Tail_Iter(list, elements.tail, addMatchesToReturnList(find_Tail(list, elements.head), returnList))
+      }
+    }
+    reverse(findEach_Tail_Iter(list, elements, Nil))
+  }
+
+  def find(list: List[String], element: String): List[String] = {
+    if (list == Nil) Nil
+    else if (matchStringToElement(list.head, element)) list.head :: find(list.tail, element)
+    else find(list.tail, element)
+  }
+
+  def findEach(list: List[String], elements: List[String]): List[String] = {
+    def findEach_Iter(list: List[String], elements: List[String]): List[String] = {
+      if (elements == Nil) Nil
+      else {
+        def createListWithRepetitions(matches: List[String]): List[String] = {
+          if (matches == Nil) Nil
+          else matches.head :: createListWithRepetitions(matches.tail)
+        }
+        createListWithRepetitions(find(list, elements.head)) ::: findEach(list, elements.tail)
+      }
+    }
+    newListWithoutRepetition(findEach_Iter(list, elements), Nil)
+  }
+
+  @tailrec
+  final def newListWithoutRepetition(list: List[String], returnList: List[String]): List[String] = {
+    if (list == Nil) reverse(returnList)
+    else if (!contains(returnList, list.head)) newListWithoutRepetition(list.tail, list.head :: returnList)
+    else newListWithoutRepetition(list.tail, returnList)
   }
 
   //zadanie 5
@@ -69,5 +142,23 @@ class Functions {
       else reverseIter(list.tail, list.head :: rev)
     }
     reverseIter(list, Nil)
+  }
+
+  //own string length function
+  def stringLength(string: String): Int = {
+    @tailrec
+    def stringLengthIter(string: String, length: Int): Int = {
+      if (string == "") length
+      else stringLengthIter(string.tail, length + 1)
+    }
+    stringLengthIter(string, 0)
+  }
+
+  //own contains function
+  @tailrec
+  final def contains[A](list: List[A], element: A): Boolean = {
+    if (list == Nil) return false
+    else if (list.head == element) return true
+    else contains(list.tail, element)
   }
 }
