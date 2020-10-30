95d5d3833ff8322206490dd5af1a6dc56c7e9828
Kic4k <lukaskickic@gmail.com>
2020-10-30 18:44:57 +0100

    task 3 with test added

--- L3/l3.scala
+++ L3/l3.scala
@@ -1,8 +1,8 @@
 //2
 /*
 zlozonosc:
-pamieciowa:O(n), n-długosc podanej listy
-obliczeniowa:O(1),
+obliczeniowa:O(n), n-długosc podanej listy
+pamieciowa:O(1),
 */
 def listLenght[A](list:List[A]):Int = {
   def help(acc:Int,list:List[A]):Int = list match {
@@ -16,3 +16,24 @@ listLenght(List())
 listLenght(List(1))
 listLenght(List(1,2))
 listLenght(List('a','b','c'))
+
+//3
+/*
+zlozonosc:
+obliczeniowa:O(n), n-długosc dłuższej listy
+pamieciowa:O(1),
+*/
+def mergeLists[A](listA:List[A],listB:List[A]):List[A] = (listA,listB) match {
+  case (Nil,Nil) => Nil
+  case (listA,Nil) => listA
+  case (Nil,listB) => listB
+  case (headA::tailA,headB::tailB) => headA::headB::mergeLists(tailA,tailB)
+}
+//3 test
+mergeLists(List(1,3,5,7),List(2,4,6,8))
+mergeLists(List(1,3,5,7),List(2,4))
+mergeLists(List(1,3),List(2,4,6,8))
+mergeLists(List(1,3),List('a','b'))
+mergeLists(List(1,3),List())
+mergeLists(List(),List(1))
+mergeLists(List(),List())
