9ad2fbe60cea4f62632f4ee4a6cd6d52ae97c932
Klaudia Blazyczek <blazyczek.klaudia@gmail.com>
2020-11-02 15:38:25 +0100

    Implement exc 1 without tests

--- PP_Lab/src/main/scala/Lista4/Lista4.scala
+++ PP_Lab/src/main/scala/Lista4/Lista4.scala
@@ -1,5 +1,22 @@
 package Lista4
 
 object Lista4 extends App{
+  // Tree
+  sealed trait BT[+A]
+  case object Empty extends BT[Nothing]
+  case class Node[+A](elem:A, left:BT[A], right:BT[A]) extends BT[A]
 
+  // additional methods
+
+
+
+  // zadanie 1 (3pkt)
+  def createTree(amountOfLevels: Int, leftInterval: Int, rightInterval: Int): BT[Int] = {
+    val randomInt = scala.util.Random
+    def innerCreateTree(levels: Int): BT[Int] = {
+      if(levels == (-1)) Empty
+      else Node(randomInt.nextInt(rightInterval-leftInterval+1)+leftInterval, innerCreateTree(levels-1), innerCreateTree(levels-1))
+    }
+    innerCreateTree(amountOfLevels)
+  }
 }
--- PP_Lab/src/test/scala/Lista4/Lista4Test.scala
+++ PP_Lab/src/test/scala/Lista4/Lista4Test.scala
@@ -2,5 +2,10 @@ package Lista4
 import org.scalatest.FunSuite
 
 class Lista4Test extends FunSuite{
+  test("Lista 4 zadanie 1"){
+    //
+    // nie wiem jakie testy
+    //
+  }
 
 }
