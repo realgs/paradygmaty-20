09957a92196ecfda5c761c325eef38e41975d28d
Klaudia Blazyczek <blazyczek.klaudia@gmail.com>
2020-11-03 17:08:28 +0100

    Improve exc. 1

--- PP_Lab/src/main/scala/Lista4/Lista4.scala
+++ PP_Lab/src/main/scala/Lista4/Lista4.scala
@@ -6,12 +6,16 @@ object Lista4 extends App{
   case object Empty extends BT[Nothing]
   case class Node[+A](elem:A, left:BT[A], right:BT[A]) extends BT[A]
 
-  // additional methods
+  // Exceptions
+  class WrongValueOfArguments extends Exception
+
+  // Additional methods
 
 
 
   // zadanie 1 (3pkt)
   def createTree(amountOfLevels: Int, leftInterval: Int, rightInterval: Int): BT[Int] = {
+    if (amountOfLevels<0 || rightInterval<leftInterval) throw new WrongValueOfArguments
     val randomInt = scala.util.Random
     def innerCreateTree(levels: Int): BT[Int] = {
       if(levels == (-1)) Empty
