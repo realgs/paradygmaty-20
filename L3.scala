0ad24ec0e9b2b3f1bff4667bb989fd9a81e81399
Rafsonn <rafq321@gmail.com>
2020-11-07 23:06:39 +0100

    exercise 3 added

--- L3.scala
+++ L3.scala
@@ -14,6 +14,16 @@ object L3 extends App{
       else 1 + z2(list.tail)
     }
 
+  //zadanie 3
+  def z3 [A] (list1: List[A], list2: List[A]): List[A] =
+  {
+    (list1, list2) match
+      {
+      case (Nil, _) => list2
+      case (_ , Nil) => list1
+      case (head_1 :: tail_1, head_2 :: tail_2 ) => head_1 :: head_2 :: z3(tail_1, tail_2)
+    }
 
+  }
 
 }
