d331f1b5b5e28e8247b6aec98c45b597dbc980a5
Klaudia Blazyczek <blazyczek.klaudia@gmail.com>
2020-11-10 20:48:31 +0100

    Implement all excercises with tests

--- PP_Lab/src/main/scala/Lista4/Lista4Trees.scala
+++ PP_Lab/src/main/scala/Lista4/Lista4Trees.scala
@@ -2,7 +2,7 @@ package Lista4
 
 import scala.annotation.tailrec
 
-object Lista4Trees extends App{
+object Lista4Trees extends App {
   // Tree
   sealed trait BT[+A]
   case object Empty extends BT[Nothing]
@@ -15,43 +15,43 @@ object Lista4Trees extends App{
   class InternalException extends Exception
 
 
-  // Test Methods
-  // used for testing exc 1
+  // used to test exc 1
   def isTreeFull[A](tree: BT[A]): Boolean = {
     tree match {
       case Empty => true
       case Node(_, l, r) =>
-        def innerIsTreeFull(leftTree: BT[A], rightTree: BT[A]): Boolean ={
+        def innerIsTreeFull(leftTree: BT[A], rightTree: BT[A]): Boolean = {
           (leftTree, rightTree) match {
-            case(Node(_, l1, r1), Node(_, l2, r2)) => innerIsTreeFull(l1, r1) && innerIsTreeFull(l2, r2)
-            case(Empty, Empty) => true
-            case(_, _) => false
+            case (Node(_, l1, r1), Node(_, l2, r2)) => innerIsTreeFull(l1, r1) && innerIsTreeFull(l2, r2)
+            case (Empty, Empty) => true
+            case (_, _) => false
           }
-      }
+        }
         innerIsTreeFull(l, r)
     }
   }
 
-  // used for testing exc 1
+  // used to test exc 1
   def isTreeDepthRight[A](tree: BT[A], expectedLevel: Int): Boolean = {
     tree match {
       case Empty => false
       case Node(_, l, r) =>
-        def innerIsTreeDepthRight(leftTree: BT[A], rightTree: BT[A], level: Int): Boolean ={
+        def innerIsTreeDepthRight(leftTree: BT[A], rightTree: BT[A], level: Int): Boolean = {
           (leftTree, rightTree) match {
-            case(Empty, Empty) => expectedLevel == 0
-            case(Node(_, l1, r1), Node(_, l2, r2)) => innerIsTreeDepthRight(l1, r1, level-1) == innerIsTreeDepthRight(l2, r2, level-1)
-            case(_, _) => false
+            case (Empty, Empty) => expectedLevel == 0
+            case (Node(_, l1, r1), Node(_, l2, r2)) => innerIsTreeDepthRight(l1, r1, level-1) == innerIsTreeDepthRight(l2, r2, level-1)
+            case (_, _) => false
           }
         }
         innerIsTreeDepthRight(l, r, expectedLevel)
     }
   }
 
-  // used for printing tree
-  def inorder[A](bt:BT[A]):List[A] = bt match {
-    case Node(v,l,r) => inorder(l) ::: v :: inorder(r)
-    case Empty => Nil
+  // used to test trees
+  def inorder[A](tree:BT[A]):List[A] =
+    tree match {
+      case Node(v,l,r) => inorder(l) ::: v :: inorder(r)
+      case Empty => Nil
   }
 
   // used in exc 2 and 3
@@ -66,116 +66,130 @@ object Lista4Trees extends App{
           case (Node(_, left1, _), Node(_, left2, _)) => compareLevels(left1, left2) //skoro drzewa sa pelne to wystarczy ze sprawdze 1 sciezke
         }
       compareLevels(leftBT, rightBT)
-    }else false
+    }
+    else false
   }
 
   //////////////////////////////////////////////////zadania////////////////////////////////////////////////////////////////
 
   // zadanie 1 (3pkt)
   // dla amountOfLevels == 0 zwraca drzewo z samym korzeniem z wartoscia - potomkami korzenia sa Empty
-  // dla amountOfLevels < 0 rzuca blad
   def createTree(amountOfLevels: Int, leftInterval: Int, rightInterval: Int): BT[Int] = {
-    if (amountOfLevels < 0 || rightInterval < leftInterval) throw new WrongValueOfArguments
-    val randomInt = scala.util.Random
-    def innerCreateTree(levels: Int): BT[Int] = {
-      if(levels == (-1)) Empty
-      else Node(randomInt.nextInt(rightInterval-leftInterval+1)+leftInterval, innerCreateTree(levels-1), innerCreateTree(levels-1))
+    if (amountOfLevels < 0 || rightInterval < leftInterval)
+      throw new WrongValueOfArguments
+    else {
+      val randomInt = scala.util.Random
+      def innerCreateTree(levels: Int): BT[Int] = {
+        if(levels == (-1))
+          Empty
+        else
+          Node(randomInt.nextInt(rightInterval-leftInterval+1)+leftInterval, innerCreateTree(levels-1), innerCreateTree(levels-1))
+      }
+      innerCreateTree(amountOfLevels)
     }
-    innerCreateTree(amountOfLevels)
   }
 
 
   // zadanie 2 (3pkt)
-  def subtractTrees(leftTree: BT[Int], rightTree: BT[Int]):BT[Int] = {
-    if (!chceckIfMatch(leftTree, rightTree)) throw new WrongTreesLevel
-    else{
-      (leftTree, rightTree) match{
+  def subtractTrees(leftTree: BT[Int], rightTree: BT[Int]): BT[Int] = {
+    if (!chceckIfMatch(leftTree, rightTree))
+      throw new WrongTreesLevel
+    else {
+      (leftTree, rightTree) match {
         case (Node(value1, left1, right1), Node(value2, left2, right2)) => Node(value1 - value2, subtractTrees(left1, left2), subtractTrees(right1, right2))
         case (_, _) => Empty
       }
     }
   }
 
-  /*val tree1 = createTree(3, 1, 50)
-  println("First tree: " + inorder(tree1))
-  val tree2 = createTree(3, 1, 50)
-  println("Second tree: " + inorder(tree2))
-  val resultTree12 = subtractTrees(tree1, tree2)
-  println("Result tree: " + inorder(resultTree12))
-  val tree3 = createTree(2, 1, 50)
-  println("Third tree: " + inorder(tree3))*/
-  //val failTree = subtractTrees(tree1, tree3) ==>> throw new WrongTreesLevel
 
-
-  // zadanie 3 (wgłąb 1 pkt, wszerz 3 pkt)
-  // w glab DFS
-  def compareTreesDFS(leftTree: BT[Int], rightTree: BT[Int]): (BT[Int], BT[Int]) ={
-    if (!chceckIfMatch(leftTree, rightTree)) throw new WrongTreesLevel
-    else{
+  // zadanie 3 (wgłab 1 pkt, wszerz 3 pkt)
+  // wglab DFS
+  def compareTreesDFS(leftTree: BT[Int], rightTree: BT[Int]): (BT[Int], BT[Int]) = {
+    if (!chceckIfMatch(leftTree, rightTree))
+      throw new WrongTreesLevel
+    else {
       def innerCompare(leftSubTree: BT[Int], rightSubTree: BT[Int]): (BT[Int], BT[Int]) =
         (leftSubTree, rightSubTree) match {
-          case(Node(valL, Empty, Empty), Node(valR, Empty, Empty)) =>
-            if(valL == valR) (Empty, Empty)
-            else (Node(valL, Empty, Empty), Node(valR, Empty, Empty))
+          case (Node(valL, Empty, Empty), Node(valR, Empty, Empty)) =>
+            if(valL == valR)
+              (Empty, Empty)
+            else
+              (Node(valL, Empty, Empty), Node(valR, Empty, Empty))
 
           case (Node(valL, left1, right1), Node(valR, left2, right2)) =>
             val resultLeftSubTrees = innerCompare(left1, left2)
             val resultRightSubTrees = innerCompare(right1, right2)
-            if(valL != valR) (Node(valL, resultLeftSubTrees._1, resultRightSubTrees._1), Node(valR, resultLeftSubTrees._2, resultRightSubTrees._2))
-            else{
-              if(resultLeftSubTrees._1 == Empty && resultRightSubTrees._1 == Empty) (Empty, Empty)
-              else (Node(-1, resultLeftSubTrees._1, resultRightSubTrees._1), Node(-1, resultLeftSubTrees._2, resultRightSubTrees._2))
+            if(valL != valR)
+              (Node(valL, resultLeftSubTrees._1, resultRightSubTrees._1), Node(valR, resultLeftSubTrees._2, resultRightSubTrees._2))
+            else {
+              if(resultLeftSubTrees._1 == Empty && resultRightSubTrees._1 == Empty )
+                (Empty, Empty) // wystarczy sprawdzic dla 1 drzewa wyniki lewego i prawego poddrzewa
+              else
+                (Node(-1, resultLeftSubTrees._1, resultRightSubTrees._1), Node(-1, resultLeftSubTrees._2, resultRightSubTrees._2))
             }
-          case (_, _) =>   throw new InternalException
+          case (_, _) => throw new InternalException
         }
       innerCompare(leftTree, rightTree)
     }
   }
 
-
   //wszerz BFS
-  /*def compareTreesBFS(leftTree: BT[Int], rightTree: BT[Int]): (BT[Int], BT[Int]) ={
-    if (!chceckIfMatch(leftTree, rightTree)) throw new WrongTreesLevel
-    else{
-      def innerCompare(leftSubTree: List[BT[Int]], rightSubTree:List[BT[Int]]): (BT[Int], BT[Int]) =
-        (leftSubTree, rightSubTree) match {
-          case (Node(valL, Empty, Empty)::_, Node(valR, Empty, Empty)::_) =>
-            if(valL == valR) (Empty, Empty)
-            else (Node(valL, Empty, Empty), Node(valR, Empty, Empty))
+  def ifSubTreesEqualsBFS(left: BT[Int], right: BT[Int]) : Boolean = {
+    @tailrec
+    def innerEquals(leftTree: List[BT[Int]], rightTree: List[BT[Int]]): Boolean =
+      (leftTree, rightTree) match {
+        case (Nil, Nil) => true
+        case (Empty::tailL, Empty::tailR) => innerEquals(tailL, tailR)
+        case (Node(valL, left1, right1)::tailL, Node(valR, left2, right2)::tailR) =>
+          if(valL == valR)
+            innerEquals(tailL:::List(left1, right1), tailR:::List(left2, right2))
+          else
+            false
+        case(_, _) => false
+      }
+    innerEquals(List(left), List(right))
+  }
 
-          case (Node(valL, left1, right1)::tailL, Node(valR, left2, right2)::tailR) =>
-            val resultLeftSubTrees = innerCompare(tailL:::List(left1), tailR:::List(left2))
-            val resultRightSubTrees = innerCompare(tailL:::List(right1), tailR:::List(right2))
-            if(valL != valR) (Node(valL, resultLeftSubTrees._1, resultRightSubTrees._1), Node(valR, resultLeftSubTrees._2, resultRightSubTrees._2))
-            else{
-              if(resultLeftSubTrees._1 == Empty && resultRightSubTrees._1 == Empty) (Empty, Empty)
-              else (Node(-1, resultLeftSubTrees._1, resultRightSubTrees._1), Node(-1, resultLeftSubTrees._2, resultRightSubTrees._2))
-            }
-          case (_, _) =>   throw new InternalException
+  def compareTreesBFS(leftTree: BT[Int], rightTree: BT[Int]): (BT[Int], BT[Int]) = {
+    if (!chceckIfMatch(leftTree, rightTree))
+      throw new WrongTreesLevel
+    else {
+      def innerCompare(left: BT[Int], right: BT[Int]): (BT[Int], BT[Int]) = {
+        val Node(firstVal, firstLeftSubTree, firstRightSubTree) = left
+        val Node(secondVal, secondLeftSubTree, secondRightSubTree) = right
+
+       (ifSubTreesEqualsBFS(firstLeftSubTree, secondLeftSubTree), ifSubTreesEqualsBFS(firstRightSubTree, secondRightSubTree)) match {
+          // obydwa poddrzewa sa rowne
+          case(true, true) => if (firstVal == secondVal)
+                                (Empty, Empty)
+                              else
+                                (Node(firstVal, Empty, Empty), Node(secondVal, Empty, Empty))
+          // lewe poddrzewa sa rowne, prawe sie roznia
+          case(true, false) =>
+            val (leftSubTree, rightSubTree) = innerCompare(firstRightSubTree, secondRightSubTree)
+            if (firstVal == secondVal)
+              (Node(-1, Empty, leftSubTree), Node(-1, Empty, rightSubTree))
+            else
+              (Node(firstVal, Empty, leftSubTree), Node(secondVal, Empty, rightSubTree))
+          // lewe poddrzewa sa rozne, prawe rowne
+          case (false, true) =>
+            val (leftSubTree, rightSubTree) = innerCompare(firstLeftSubTree, secondLeftSubTree)
+            if (firstVal == secondVal)
+              (Node(-1, leftSubTree, Empty), Node(-1, rightSubTree, Empty))
+            else
+              (Node(firstVal, leftSubTree, Empty), Node(secondVal, rightSubTree, Empty))
+
+          // obydwa poddrzewa sa rozne
+          case (false, false) =>
+            val (leftSubTreeLeft, rightSubTreeLeft) = innerCompare(firstLeftSubTree, secondLeftSubTree)
+            val (leftSubTreeRight, rightSubTreeRight) = innerCompare(firstRightSubTree, secondRightSubTree)
+            if (firstVal == secondVal)
+              (Node(-1, leftSubTreeLeft, leftSubTreeRight), Node(-1, rightSubTreeLeft, rightSubTreeRight))
+            else
+              (Node(firstVal, leftSubTreeLeft, leftSubTreeRight), Node(secondVal, rightSubTreeLeft, rightSubTreeRight))
         }
-      innerCompare(List(leftTree), List(rightTree))
-    }
-  }*/
-
-  def compareTreesBFS(leftTree: BT[Int], rightTree: BT[Int]): (BT[Int], BT[Int]) ={
-    if (!chceckIfMatch(leftTree, rightTree)) throw new WrongTreesLevel
-    else{
-      def innerCompare(leftSubTree: BT[Int], rightSubTree:BT[Int]): (BT[Int], BT[Int]) =
-        (leftSubTree, rightSubTree) match {
-          case (Node(valL, Empty, Empty), Node(valR, Empty, Empty)) =>
-            if(valL == valR) (Empty, Empty)
-            else (Node(valL, Empty, Empty), Node(valR, Empty, Empty))
-
-          case (Node(valL, left1, right1), Node(valR, left2, right2)) =>
-            val resultLeftSubTrees = innerCompare(left1, left2)
-            val resultRightSubTrees = innerCompare(right1, right2)
-            if(valL != valR) (Node(valL, resultLeftSubTrees._1, resultRightSubTrees._1), Node(valR, resultLeftSubTrees._2, resultRightSubTrees._2))
-            else{
-              if(resultLeftSubTrees._1 == Empty && resultRightSubTrees._1 == Empty) (Empty, Empty)
-              else (Node(-1, resultLeftSubTrees._1, resultRightSubTrees._1), Node(-1, resultLeftSubTrees._2, resultRightSubTrees._2))
-            }
-          case (_, _) =>   throw new InternalException
-        }
+      }
       innerCompare(leftTree, rightTree)
     }
   }
--- PP_Lab/src/test/scala/Lista4/Lista4TreesTest.scala
+++ PP_Lab/src/test/scala/Lista4/Lista4TreesTest.scala
@@ -1,9 +1,10 @@
 package Lista4
-import Lista4.Lista4Trees.{Empty, Node, inorder}
+import Lista4.Lista4Trees.{Empty, Node, createTree, inorder, subtractTrees}
 import org.scalatest.FunSuite
 
-class Lista4TreesTest extends FunSuite{
-  test("Lista 4 zadanie 1"){
+class Lista4TreesTest extends FunSuite {
+
+  test("Lista 4 zadanie 1") {
     val tree1 = Lista4Trees.createTree(2, 1, 10)
     val tree2 = Lista4Trees.createTree(0, 1, 10)
     val tree3 = Lista4Trees.createTree(3, 1, 10)
@@ -20,7 +21,14 @@ class Lista4TreesTest extends FunSuite{
     assert(Lista4Trees.isTreeDepthRight(tree4, 4))
   }
 
-  test("Lista 4 zadanie 3 DFS"){
+  test("Lista 4 zadanie 2") {
+    val tree1 = createTree(3, 1, 50)
+    val tree2 = createTree(3, 1, 50)
+    val result = inorder(tree1).zip(inorder(tree2)).map(n => n._1 - n._2)
+    assert(inorder(subtractTrees(tree1, tree2)) == result)
+  }
+
+  test("Lista 4 zadanie 3 DFS") {
     val tree1 = Node(2, Node(3, Node(2, Empty, Empty), Node(4, Empty, Empty)), Node(3, Node(3, Empty, Empty), Node(2, Empty, Empty)))
     val tree2 =  Node(4, Node(2, Node(2, Empty, Empty), Node(4, Empty, Empty)), Node(3, Node(5, Empty, Empty), Node(5, Empty, Empty)))
     val result12 = Lista4Trees.compareTreesDFS(tree1, tree2)
@@ -34,7 +42,12 @@ class Lista4TreesTest extends FunSuite{
     assert(result34._2 == Node(-1, Node(1, Empty, Node(4, Empty, Empty)), Node(-1, Empty, Node(1, Empty, Empty))))
   }
 
-  test("Lista 4 zadanie 3 BFS"){
+  test("Lista 4 zadanie 3 BFS") {
+    assert(Lista4Trees.ifSubTreesEqualsBFS(Node(8, Node(10, Node(1, Empty, Empty), Node(2, Empty, Empty)), Node(7, Node(3, Empty, Empty), Node(15, Empty, Empty))), Node(8, Node(10, Node(1, Empty, Empty), Node(2, Empty, Empty)), Node(7, Node(3, Empty, Empty), Node(15, Empty, Empty)))))
+    assert(Lista4Trees.ifSubTreesEqualsBFS(Node(4, Node(2, Empty, Empty), Node(-1, Node(5, Empty, Empty), Node(5, Empty, Empty))), Node(4, Node(2, Empty, Empty), Node(-1, Node(5, Empty, Empty), Node(5, Empty, Empty)))))
+    assert(!Lista4Trees.ifSubTreesEqualsBFS(Node(2, Node(3, Node(2, Empty, Empty), Node(4, Empty, Empty)), Node(3, Node(3, Empty, Empty), Node(2, Empty, Empty))), Node(2, Node(3, Empty, Empty), Node(-1, Node(3, Empty, Empty), Node(2, Empty, Empty)))))
+
+
     val tree1 = Node(2, Node(3, Node(2, Empty, Empty), Node(4, Empty, Empty)), Node(3, Node(3, Empty, Empty), Node(2, Empty, Empty)))
     val tree2 =  Node(4, Node(2, Node(2, Empty, Empty), Node(4, Empty, Empty)), Node(3, Node(5, Empty, Empty), Node(5, Empty, Empty)))
     val result12 = Lista4Trees.compareTreesBFS(tree1, tree2)
