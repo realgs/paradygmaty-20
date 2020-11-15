bdbf26b06ed0d0bf6526b07ed4878cb0ccd151ea
Łukasz Łosieczka <lukasz.losieczka@gmail.com>
2020-11-15 20:18:28 +0100

    Function 3, 4, 5 with Tests added

--- L4.scala
+++ L4.scala
@@ -2,30 +2,93 @@ sealed trait BT[+A]
 case object Empty extends BT[Nothing]
 case class Node[+A](elem:A,left:BT[A],right:BT[A])extends BT[A]
 
+sealed trait nlist[+A]
+case class Koniec[+A]() extends nlist[A]
+case class Element[+A](elem: A,list: nlist[A]) extends nlist[A]
+
+sealed  trait llist[+A]
+case class LKoniec[+A]() extends llist[A]
+case class LElement[+A](elem: A,tail: ()=>llist[A]) extends llist[A]
+
 class L4 {
 
   //zadanie 1 (3 pkt)
-  def generateTree(n:Int, down:Int, up:Int):BT[Int] ={
+  def generateTree(n: Int, down: Int, up: Int): BT[Int] = {
     val r = scala.util.Random
+
     def helper(n: Int): BT[Int] =
       if (n == 0) Node(down + r.nextInt(up - down), Empty, Empty)
       else Node(down + r.nextInt(up - down), helper(n - 1), helper(n - 1))
 
-    if(down<0||up<0||down>up||n<=0) Empty
-    else helper(n-1)
+    if (down < 0 || up < 0 || down > up || n <= 0) Empty
+    else helper(n - 1)
   }
 
   //zadanie 2 (3 pkt)
-  def subOfTrees(tree1:BT[Int],tree2:BT[Int]):BT[Int] =
-    (tree1,tree2) match{
-      case (Node(v1,l1,r1),Node(v2,l2,r2)) => Node(v1-v2,subOfTrees(l1,l2),subOfTrees(r1,r2))
-      case (Empty,Empty) => Empty
-      case(_,_) => throw new Exception("Incorrect Tree")
+  def subOfTrees(tree1: BT[Int], tree2: BT[Int]): BT[Int] =
+    (tree1, tree2) match {
+      case (Node(v1, l1, r1), Node(v2, l2, r2)) => Node(v1 - v2, subOfTrees(l1, l2), subOfTrees(r1, r2))
+      case (Empty, Empty) => Empty
+      case (_, _) => throw new Exception("Incorrect Tree")
     }
 
   //zadanie 3 (4 pkt)
-  def sameSubTrees[A](tree1:BT[A],tree2:BT[A]):(BT[A],BT[A]) ={
+  //wgłąb
+  def sameElemDelDepth(tree1: BT[Int], tree2: BT[Int]): (BT[Int], BT[Int]) =
+    (tree1, tree2) match {
+      case (Node(v1, l1, r1), Node(v2, l2, r2)) =>
+        val (lt1, lt2) = sameElemDelDepth(l1, l2)
+        val (rt1, rt2) = sameElemDelDepth(r1, r2)
+        if (v1 == v2) {
+          if (lt1 == Empty && rt1 == Empty) (Empty, Empty)
+          else (Node(-1, lt1, rt1), Node(-1, lt2, rt2))
+        }
+        else (Node(v1, lt1, rt1), Node(v2, lt2, rt2))
+      case (Empty, Empty) => (Empty, Empty)
+      case (_, _) => throw new Exception("Incorrect Tree")
+    }
 
+  //wszerz
+  //def sameElemDelBreadth(tree1: BT[Int], tree2: BT[Int]): (BT[Int], BT[Int]) ={}
+
+  //zadanie 4 (5 pkt)
+  def eachNElement(lazyList: llist[Int], n: Int, m: Int): llist[Int] = {
+    def iteration(list: llist[Int], i: Int, j: Int): llist[Int] = {
+      if (i * n + j < m) {
+        if (j > 0)
+          list match {
+            case LElement(_, tail) =>
+              iteration(tail(), if (j + 1 == n) i + 1 else i, if (j + 1 == n) 0 else j + 1)
+            case LKoniec() => LKoniec()
+          }
+        else
+          list match {
+            case LElement(elem, tail) => LElement(elem, () =>
+              iteration(tail(), if (j + 1 == n) i + 1 else i, if (j + 1 == n) 0 else j + 1))
+            case LKoniec() => LKoniec()
+          }
+      }
+      else LKoniec()
+    }
+
+    if (n <= 0 || m < 0) throw new Exception("Wrong parameters")
+    else iteration(lazyList, 0, 0)
   }
 
+  //zadanie 5 (5 pkt)
+  def ldzialanie(lazyList1: llist[Int], lazyList2: llist[Int], sign: Char): llist[Int] = {
+    def operation(lazyList1: llist[Int], lazyList2: llist[Int],fun:Int=>Int=>Int):llist[Int] =
+      (lazyList1,lazyList2) match {
+        case (LKoniec(),_) => lazyList2
+        case (_,LKoniec()) => lazyList1
+        case (LElement(h1,t1),LElement(h2,t2)) => LElement(fun(h1)(h2),()=>operation(t1(),t2(),fun))
+      }
+    sign match {
+      case '+' => operation(lazyList1,lazyList2,(x:Int)=>(y:Int)=>x+y)
+      case '-' => operation(lazyList1,lazyList2,(x:Int)=>(y:Int)=>x-y)
+      case '*' => operation(lazyList1,lazyList2,(x:Int)=>(y:Int)=>x*y)
+      case '/' => operation(lazyList1,lazyList2,(x:Int)=>(y:Int)=>x/y)
+      case _ => throw new Exception("Wrong sign")
+    }
+  }
 }
