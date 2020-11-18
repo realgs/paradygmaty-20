7c6d417087720b90958b233dac99e1b60d825824
marcijay <jak.marcinko@gmail.com>
2020-11-19 00:02:30 +0100

    Added task 2 with tests

--- Lista4/src/main/scala/Trees.scala
+++ Lista4/src/main/scala/Trees.scala
@@ -9,8 +9,8 @@ object Trees {
 
   //Zad 1 (3 pkt)
   def generateTree(height: Int, lowerBoundary: Int, upperBoundary: Int): BT[Int] = {
-    if(height < 0 || lowerBoundary > upperBoundary || lowerBoundary < 0) throw new Exception("Illegal argument value!")
-    else if(height == 0) Node(Random.between(lowerBoundary, upperBoundary), Empty, Empty)
+    if(height < -1 || lowerBoundary > upperBoundary || lowerBoundary < 0) throw new Exception("Illegal argument value!")
+    else if(height == -1) Empty
     else Node(Random.between(lowerBoundary, upperBoundary), generateTree(height - 1, lowerBoundary, upperBoundary), generateTree(height - 1, lowerBoundary, upperBoundary))
   }
 
@@ -33,6 +33,16 @@ object Trees {
       case Node(_, subLeft, Empty) => if (subLeft == Empty) true else false
       case Node(_, Empty, subRight) => if (subRight == Empty) true else false
       case Node(_, subLeft, subRight) => isTreeFull(subLeft) && isTreeFull(subRight)
+    }
+  }
+
+  //Zad 2 (3 pkt)
+  def subtractInTrees (tree1: BT[Int], tree2: BT[Int]): BT[Int] = {
+    if(getHeight(tree1) != getHeight(tree2) || !isTreeFull(tree1) || !isTreeFull(tree2))
+      throw new Exception("Trees not full or equal in height!")
+    else (tree1, tree2) match {
+      case (Node(val1, left1, right1), Node(val2, left2, right2)) => Node(val1 - val2, subtractInTrees(left1, left2), subtractInTrees(right1, right2))
+      case _ => Empty
     }
   }
 
--- Lista4/src/test/scala/TreesTest.scala
+++ Lista4/src/test/scala/TreesTest.scala
@@ -16,8 +16,29 @@ class TreesTest extends FunSuite {
   test("createTree.height_1"){
     assert(Trees.generateTree(1, 1, 2) === Trees.Node(1, Trees.Node(1, Trees.Empty, Trees.Empty), Trees.Node(1, Trees.Empty, Trees.Empty)))
   }
-  test("createTree.height_-1"){
-    assertThrows[Exception](Trees.generateTree(-1,5,8))
+  test("createTree.height_-2"){
+    assertThrows[Exception](Trees.generateTree(-2,5,8))
+  }
+  test("createTree.EmptyTree"){
+    val tree = Trees.generateTree(-1, 10, 22)
+    assert(Trees.getHeight(Trees.Empty) === -1)
+    assert(Trees.isTreeFull(Trees.Empty))
+  }
+
+  //task 2 tests
+  test("subtractInTrees.height_2_val_random_1"){
+    assert(Trees.subtractInTrees(Trees.generateTree(2,1,2),Trees.generateTree(2,1,2) ) ===
+      Trees.Node(0, Trees.Node(0, Trees.Node(0, Trees.Empty, Trees.Empty), Trees.Node(0, Trees.Empty, Trees.Empty)), Trees.Node(0, Trees.Node(0, Trees.Empty, Trees.Empty), Trees.Node(0, Trees.Empty, Trees.Empty))))
+  }
+  test("subtractInTrees.height_1_val_given"){
+    assert(Trees.subtractInTrees(Trees.Node(5, Trees.Node(2, Trees.Empty, Trees.Empty), Trees.Node(6, Trees.Empty, Trees.Empty)), Trees.Node(2, Trees.Node(7, Trees.Empty, Trees.Empty), Trees.Node(6, Trees.Empty, Trees.Empty))) ===
+      Trees.Node(3, Trees.Node(-5, Trees.Empty, Trees.Empty), Trees.Node(0, Trees.Empty, Trees.Empty)))
+  }
+  test("subtractionInTrees.unequalHeight"){
+    assertThrows[Exception](Trees.subtractInTrees(Trees.generateTree(2, 1, 5), Trees.generateTree(3, 1, 4)))
+  }
+  test("subtractInTrees.emptyTrees"){
+    assert(Trees.subtractInTrees(Trees.generateTree(-1,1,2),Trees.generateTree(-1,1,2) ) === Trees.Empty)
   }
 
 }
