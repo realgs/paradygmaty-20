cda2e1f533771d9faf00d5eb0c02c4b9050422c9
bedni <w.bednarz2000@gmail.com>
2020-11-19 16:01:03 +0100

    Initial commit

---
+++ Lab4/src/Functions.scala
@@ -0,0 +1,13 @@
+object Functions {
+
+  // Zadanie 1 (3 pkt)
+
+  // Zadanie 2 (3 pkt)
+
+  // Zadanie 3 (4 pkt)
+
+  // Zadanie 4 (5 pkt)
+
+  // Zadanie 5 (5 pkt)
+
+}
---
+++ Lab4/src/Tests.scala
@@ -0,0 +1,19 @@
+import Functions._
+
+object Tests {
+
+  def main(args: Array[String]): Unit = {
+
+      // Zadanie 1
+
+      // Zadanie 2
+
+      // Zadanie 3
+
+      // Zadanie 4
+
+      // Zadanie 5
+
+  }
+
+}
