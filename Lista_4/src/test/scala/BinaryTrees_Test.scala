e3596da76b4915d70d97013352492d16d3eead8c
LeviSforza <3monika100@gmail.com>
2020-11-19 15:03:50 +0100

    Add a few corrections in tests

--- Lista_4/src/test/scala/BinaryTrees_Test.scala
+++ Lista_4/src/test/scala/BinaryTrees_Test.scala
@@ -2,72 +2,72 @@ import org.scalatest.funsuite.AnyFunSuite
 
 class BinaryTrees_Test extends AnyFunSuite {
 
-  val binaryTrees = new BinaryTrees
+  val BT = new BinaryTrees
 
   //zadanie 1
   test("Test depth; treeGenerator function") {
-    assert(binaryTrees.maxDepth(binaryTrees.treeGenerator(3, 10)) == 3)
+    assert(BT.maxDepth(BT.treeGenerator(3, 10)) == 3)
   }
 
   test("Test isFull; treeGenerator function") {
-    assert(binaryTrees.countLeafs(binaryTrees.treeGenerator(3, 10)) == 8)
+    assert(BT.countLeafs(BT.treeGenerator(3, 10)) == 8)
   }
 
   test("Test root; treesSubtraction function") {
-    val t1 = binaryTrees.treeGenerator(2, 10)
-    val t2 = binaryTrees.treeGenerator(2, 10)
-    val res = binaryTrees.treesSubtraction(t1, t2)
-    assert(binaryTrees.rootValue(res) == binaryTrees.rootValue(t1) - binaryTrees.rootValue(t2))
+    val t1 = BT.treeGenerator(2, 10)
+    val t2 = BT.treeGenerator(2, 10)
+    val res = BT.treesSubtraction(t1, t2)
+    assert(BT.rootValue(res) == BT.rootValue(t1) - BT.rootValue(t2))
   }
 
   test("Test full trees; treesSubtraction function") {
-    val t1 = binaryTrees.Node(10, binaryTrees.Leaf(20), binaryTrees.Leaf(30))
-    val t2 = binaryTrees.Node(1, binaryTrees.Leaf(2), binaryTrees.Leaf(3))
-    val res = binaryTrees.treesSubtraction(t1, t2)
-    assert(res == binaryTrees.Node(9, binaryTrees.Leaf(18), binaryTrees.Leaf(27)))
+    val t1 = BT.Node(10, BT.Leaf(20), BT.Leaf(30))
+    val t2 = BT.Node(1, BT.Leaf(2), BT.Leaf(3))
+    val res = BT.treesSubtraction(t1, t2)
+    assert(res == BT.Node(9, BT.Leaf(18), BT.Leaf(27)))
   }
 
   test("Test exception; treesSubtraction function") {
     assertThrows[IllegalArgumentException] {
-      val t1 = binaryTrees.treeGenerator(2, 10)
-      val t2 = binaryTrees.treeGenerator(3, 10)
-      binaryTrees.treesSubtraction(t1, t2)
+      val t1 = BT.treeGenerator(2, 10)
+      val t2 = BT.treeGenerator(3, 10)
+      BT.treesSubtraction(t1, t2)
     }
   }
 
   test("Test deleteCopiesDFS function") {
-    val t1 = binaryTrees.Node(1,binaryTrees.Node(5,binaryTrees.Leaf(2),binaryTrees.Leaf(8)),binaryTrees.Node(9,binaryTrees.Leaf(6),binaryTrees.Leaf(4)))
-    val t2 = binaryTrees.Node(1,binaryTrees.Node(5,binaryTrees.Leaf(7),binaryTrees.Leaf(9)),binaryTrees.Node(9,binaryTrees.Leaf(6),binaryTrees.Leaf(4)))
+    val t1 = BT.Node(1, BT.Node(5, BT.Leaf(2), BT.Leaf(8)), BT.Node(9, BT.Leaf(6), BT.Leaf(4)))
+    val t2 = BT.Node(1, BT.Node(5, BT.Leaf(7), BT.Leaf(9)), BT.Node(9, BT.Leaf(6), BT.Leaf(4)))
 
-    val r1 = binaryTrees.deleteCopiesDFS(t1, t2)._1
-    val r2 = binaryTrees.deleteCopiesDFS(t1, t2)._2
-    assert(r1 == binaryTrees.Node(-1,binaryTrees.Node(-1,binaryTrees.Leaf(2),binaryTrees.Leaf(8)),binaryTrees.Empty))
-    assert(r2 == binaryTrees.Node(-1,binaryTrees.Node(-1,binaryTrees.Leaf(7),binaryTrees.Leaf(9)),binaryTrees.Empty))
+    val r1 = BT.deleteCopiesDFS(t1, t2)._1
+    val r2 = BT.deleteCopiesDFS(t1, t2)._2
+    assert(r1 == BT.Node(-1, BT.Node(-1, BT.Leaf(2), BT.Leaf(8)), BT.Empty))
+    assert(r2 == BT.Node(-1, BT.Node(-1, BT.Leaf(7), BT.Leaf(9)), BT.Empty))
   }
 
   test("Test exception; deleteCopiesDFS function") {
     assertThrows[IllegalArgumentException] {
-      val t1 = binaryTrees.treeGenerator(2, 10)
-      val t2 = binaryTrees.treeGenerator(3, 10)
-      binaryTrees.deleteCopiesDFS(t1, t2)
+      val t1 = BT.treeGenerator(2, 10)
+      val t2 = BT.treeGenerator(3, 10)
+      BT.deleteCopiesDFS(t1, t2)
     }
   }
 
   test("Test deleteCopiesBFS function") {
-    val t1 = binaryTrees.Node(1,binaryTrees.Node(5,binaryTrees.Leaf(2),binaryTrees.Leaf(8)),binaryTrees.Node(9,binaryTrees.Leaf(6),binaryTrees.Leaf(4)))
-    val t2 = binaryTrees.Node(1,binaryTrees.Node(5,binaryTrees.Leaf(7),binaryTrees.Leaf(9)),binaryTrees.Node(9,binaryTrees.Leaf(6),binaryTrees.Leaf(4)))
+    val t1 = BT.Node(1, BT.Node(5, BT.Leaf(2), BT.Leaf(8)), BT.Node(9, BT.Leaf(6), BT.Leaf(4)))
+    val t2 = BT.Node(1, BT.Node(5, BT.Leaf(7), BT.Leaf(9)), BT.Node(9, BT.Leaf(6), BT.Leaf(4)))
 
-    val r1 = binaryTrees.deleteCopiesBFS(t1, t2)._1
-    val r2 = binaryTrees.deleteCopiesBFS(t1, t2)._2
-    assert(r1 == binaryTrees.Node(-1,binaryTrees.Node(-1,binaryTrees.Leaf(2),binaryTrees.Leaf(8)),binaryTrees.Empty))
-    assert(r2 == binaryTrees.Node(-1,binaryTrees.Node(-1,binaryTrees.Leaf(7),binaryTrees.Leaf(9)),binaryTrees.Empty))
+    val r1 = BT.deleteCopiesBFS(t1, t2)._1
+    val r2 = BT.deleteCopiesBFS(t1, t2)._2
+    assert(r1 == BT.Node(-1, BT.Node(-1, BT.Leaf(2), BT.Leaf(8)), BT.Empty))
+    assert(r2 == BT.Node(-1, BT.Node(-1, BT.Leaf(7), BT.Leaf(9)), BT.Empty))
   }
 
   test("Test exception; deleteCopiesBFS function") {
     assertThrows[IllegalArgumentException] {
-      val t1 = binaryTrees.treeGenerator(2, 10)
-      val t2 = binaryTrees.treeGenerator(3, 10)
-      binaryTrees.deleteCopiesBFS(t1, t2)
+      val t1 = BT.treeGenerator(2, 10)
+      val t2 = BT.treeGenerator(3, 10)
+      BT.deleteCopiesBFS(t1, t2)
     }
   }
 
--- Lista_4/src/test/scala/LazyLists_Test.scala
+++ Lista_4/src/test/scala/LazyLists_Test.scala
@@ -2,57 +2,57 @@ import org.scalatest.funsuite.AnyFunSuite
 
 class LazyLists_Test extends AnyFunSuite {
 
-  val lazyLists = new LazyLists
+  val LL = new LazyLists
 
   //zadanie 1
   test("Test for ints; eachNElement function") {
     val lxs = LazyList(5, 6, 3, 2, 1)
-    assert(lazyLists.eachNElement(lxs, 2, 3) == LazyList(5, 3))
-    assert(lazyLists.eachNElement(lxs, 2, 4) == LazyList(5, 3))
-    assert(lazyLists.eachNElement(lxs, 2, 5) == LazyList(5, 3, 1))
+    assert(LL.eachNElement(lxs, 2, 3) == LazyList(5, 3))
+    assert(LL.eachNElement(lxs, 2, 4) == LazyList(5, 3))
+    assert(LL.eachNElement(lxs, 2, 5) == LazyList(5, 3, 1))
   }
 
   test("Test for strings; eachNElement function") {
     val lxs = LazyList("a", "b", "c", "d")
-    assert(lazyLists.eachNElement(lxs, 3, 7) == LazyList("a", "d"))
-    assert(lazyLists.eachNElement(lxs, 1, 4) == LazyList("a", "b", "c", "d"))
+    assert(LL.eachNElement(lxs, 3, 7) == LazyList("a", "d"))
+    assert(LL.eachNElement(lxs, 1, 4) == LazyList("a", "b", "c", "d"))
   }
 
   test("Test empty list; eachNElement function") {
-    assert(lazyLists.eachNElement(LazyList(), 2, 3) == LazyList())
+    assert(LL.eachNElement(LazyList(), 2, 3) == LazyList())
   }
 
   test("Test empty list; doOperation function") {
-    assert(lazyLists.doOperation(LazyList(), LazyList(1, 2, 3), "add") == LazyList())
-    assert(lazyLists.doOperation(LazyList(1, 2, 3), LazyList(), "add") == LazyList())
+    assert(LL.doOperation(LazyList(), LazyList(1, 2, 3), "add") == LazyList())
+    assert(LL.doOperation(LazyList(1, 2, 3), LazyList(), "add") == LazyList())
   }
 
   test("Test operations; doOperation function") {
     val la = LazyList(10, 8, 6)
     val lb = LazyList(2, 4, 6, 2, 2, 2)
-    assert(lazyLists.doOperation(la, lb, "add") == LazyList(12, 12, 12))
-    assert(lazyLists.doOperation(la, lb, "subtract") == LazyList(8, 4, 0))
-    assert(lazyLists.doOperation(la, lb, "multiply") == LazyList(20, 32, 36))
-    assert(lazyLists.doOperation(la, lb, "divide") == LazyList(5, 2, 1))
+    assert(LL.doOperation(la, lb, "add") == LazyList(12, 12, 12))
+    assert(LL.doOperation(la, lb, "subtract") == LazyList(8, 4, 0))
+    assert(LL.doOperation(la, lb, "multiply") == LazyList(20, 32, 36))
+    assert(LL.doOperation(la, lb, "divide") == LazyList(5, 2, 1))
   }
 
   test("Test empty list; doOperationF function") {
-    assert(lazyLists.doOperationF(LazyList(), LazyList(1.4, 2.5, 3.6), "add") == LazyList())
-    assert(lazyLists.doOperationF(LazyList(1.4, 2.5, 3.6), LazyList(), "add") == LazyList())
+    assert(LL.doOperationF(LazyList(), LazyList(1.4, 2.5, 3.6), "add") == LazyList())
+    assert(LL.doOperationF(LazyList(1.4, 2.5, 3.6), LazyList(), "add") == LazyList())
   }
 
   test("Test operations; doOperationF function") {
     val la = LazyList(10.6, 8.4, 20.2)
     val lb = LazyList(2.0, 4.0, 2.0, 2.0, 2.0, 2.0)
-    assert(lazyLists.doOperationF(la, lb, "add") == LazyList(12.6, 12.4, 22.2))
-    assert(lazyLists.doOperationF(la, lb, "subtract") == LazyList(8.6, 4.4, 18.2))
-    assert(lazyLists.doOperationF(la, lb, "multiply") == LazyList(21.2, 33.6, 40.4))
-    assert(lazyLists.doOperationF(la, lb, "divide") == LazyList(5.3, 2.1, 10.1))
+    assert(LL.doOperationF(la, lb, "add") == LazyList(12.6, 12.4, 22.2))
+    assert(LL.doOperationF(la, lb, "subtract") == LazyList(8.6, 4.4, 18.2))
+    assert(LL.doOperationF(la, lb, "multiply") == LazyList(21.2, 33.6, 40.4))
+    assert(LL.doOperationF(la, lb, "divide") == LazyList(5.3, 2.1, 10.1))
   }
 
   test("Test exception; doOperation function") {
     assertThrows[IllegalArgumentException] {
-      assert(lazyLists.doOperation(LazyList(), LazyList(1, 2, 3), "something") == LazyList())
+      assert(LL.doOperation(LazyList(), LazyList(1, 2, 3), "something") == LazyList())
     }
   }
 
