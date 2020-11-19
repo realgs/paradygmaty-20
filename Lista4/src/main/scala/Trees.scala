8f8bf1e546da62097d2e8b178e1787d697a38e6d
marcijay <jak.marcinko@gmail.com>
2020-11-19 13:40:54 +0100

    Added task 3a with tests

--- Lista4/src/main/scala/Trees.scala
+++ Lista4/src/main/scala/Trees.scala
@@ -8,23 +8,18 @@ object Trees {
   case class Node[+A](elem:A, left:BT[A], right:BT[A]) extends BT[A]
 
   //Zad 1 (3 pkt)
-  def generateTree(height: Int, lowerBoundary: Int, upperBoundary: Int): BT[Int] = {
-    if(height < -1 || lowerBoundary > upperBoundary || lowerBoundary < 0) throw new Exception("Illegal argument value!")
-    else if(height == -1) Empty
-    else Node(Random.between(lowerBoundary, upperBoundary), generateTree(height - 1, lowerBoundary, upperBoundary), generateTree(height - 1, lowerBoundary, upperBoundary))
+  def generateTree (height: Int, lowerBoundary: Int, upperBoundary: Int): BT[Int] = {
+    if(height < 0 || lowerBoundary > upperBoundary || lowerBoundary < 0) throw new Exception("Illegal argument value!")
+    else if(height == 0) Empty
+    else Node(Random.between(lowerBoundary, upperBoundary + 1), generateTree(height - 1, lowerBoundary, upperBoundary), generateTree(height - 1, lowerBoundary, upperBoundary))
   }
 
   //funkcje potrzebne do sprawdzenia drzewa
-  def getHeight[A](tree: BT[A]): Int = {
-    def height(inTree: BT[A]): Int = {
-      inTree match {
-        case Empty => 0
-        case Node(_, Empty, Empty) => 0
-        case Node(_, subLeft, subRight) => 1 + Math.max(height(subLeft), height(subRight))
-      }
+  def getHeight[A] (tree: BT[A]): Int = {
+    tree match {
+      case Empty => 0
+      case Node(_, subLeft, subRight) => 1 + Math.max(getHeight(subLeft), getHeight(subRight))
     }
-    if (tree == Empty) -1
-    else height(tree)
   }
 
   def isTreeFull[A] (tree: BT[A]): Boolean = {
@@ -43,6 +38,21 @@ object Trees {
     else (tree1, tree2) match {
       case (Node(val1, left1, right1), Node(val2, left2, right2)) => Node(val1 - val2, subtractInTrees(left1, left2), subtractInTrees(right1, right2))
       case _ => Empty
+    }
+  }
+
+  //Zad 3 (1 pkt dfs, 3 pkt bfs)
+  def removeSameValuesDFS (tree1: BT[Int], tree2: BT[Int]): (BT[Int], BT[Int]) = {
+    if(getHeight(tree1) != getHeight(tree2) || !isTreeFull(tree1) || !isTreeFull(tree2))
+      throw new Exception("Trees not full or equal in height!")
+    else (tree1, tree2) match {
+      case (Empty, Empty) => (Empty, Empty)
+      case (Node(val1, left1, right1), Node(val2, left2, right2)) =>
+        val (subLeft1, subLeft2) = removeSameValuesDFS(left1, left2)
+        val (subRight1, subRight2) = removeSameValuesDFS(right1, right2)
+        if(val1 == val2 && subLeft1 == Empty && subRight1 == Empty) (Empty, Empty)
+        else if(val1 == val2) (Node(-1, subLeft1, subRight1), Node(-1, subLeft2, subRight2))
+        else (Node(val1, subLeft1, subRight1), Node(val2, subLeft2, subRight2))
     }
   }
 
--- Lista4/src/test/scala/TreesTest.scala
+++ Lista4/src/test/scala/TreesTest.scala
@@ -13,21 +13,21 @@ class TreesTest extends FunSuite {
     assert(Trees.getHeight(tree) === 5)
     assert(Trees.isTreeFull(tree))
   }
-  test("createTree.height_1"){
-    assert(Trees.generateTree(1, 1, 2) === Trees.Node(1, Trees.Node(1, Trees.Empty, Trees.Empty), Trees.Node(1, Trees.Empty, Trees.Empty)))
+  test("createTree.height_2"){
+    assert(Trees.generateTree(2, 1, 1) === Trees.Node(1, Trees.Node(1, Trees.Empty, Trees.Empty), Trees.Node(1, Trees.Empty, Trees.Empty)))
   }
   test("createTree.height_-2"){
     assertThrows[Exception](Trees.generateTree(-2,5,8))
   }
   test("createTree.EmptyTree"){
-    val tree = Trees.generateTree(-1, 10, 22)
-    assert(Trees.getHeight(Trees.Empty) === -1)
+    val tree = Trees.generateTree(0, 10, 22)
+    assert(Trees.getHeight(Trees.Empty) === 0)
     assert(Trees.isTreeFull(Trees.Empty))
   }
 
   //task 2 tests
-  test("subtractInTrees.height_2_val_random_1"){
-    assert(Trees.subtractInTrees(Trees.generateTree(2,1,2),Trees.generateTree(2,1,2) ) ===
+  test("subtractInTrees.height_3_val_random_1"){
+    assert(Trees.subtractInTrees(Trees.generateTree(3,1,1),Trees.generateTree(3,1,1) ) ===
       Trees.Node(0, Trees.Node(0, Trees.Node(0, Trees.Empty, Trees.Empty), Trees.Node(0, Trees.Empty, Trees.Empty)), Trees.Node(0, Trees.Node(0, Trees.Empty, Trees.Empty), Trees.Node(0, Trees.Empty, Trees.Empty))))
   }
   test("subtractInTrees.height_1_val_given"){
@@ -38,7 +38,36 @@ class TreesTest extends FunSuite {
     assertThrows[Exception](Trees.subtractInTrees(Trees.generateTree(2, 1, 5), Trees.generateTree(3, 1, 4)))
   }
   test("subtractInTrees.emptyTrees"){
-    assert(Trees.subtractInTrees(Trees.generateTree(-1,1,2),Trees.generateTree(-1,1,2) ) === Trees.Empty)
+    assert(Trees.subtractInTrees(Trees.generateTree(0,1,2),Trees.generateTree(0,1,2) ) === Trees.Empty)
   }
+  test("subtractInTrees.height&fullTest"){
+    val tree = Trees.subtractInTrees(Trees.generateTree(5, 6, 20), Trees.generateTree(5, 20, 60))
+    assert(Trees.getHeight(tree) === 5)
+    assert(Trees.isTreeFull(tree))
+  }
+
+  //task 3 DFS tests
+  test("removeSameValuesDFS.1"){
+    val tree1 = Trees.Node(1, Trees.Node(2, Trees.Node(4, Trees.Empty, Trees.Empty), Trees.Node(5, Trees.Empty, Trees.Empty)), Trees.Node(3, Trees.Node(6, Trees.Empty, Trees.Empty), Trees.Node(7, Trees.Empty, Trees.Empty)))
+    val tree2 = Trees.Node(1, Trees.Node(3, Trees.Node(4, Trees.Empty, Trees.Empty), Trees.Node(6, Trees.Empty, Trees.Empty)), Trees.Node(3, Trees.Node(6, Trees.Empty, Trees.Empty), Trees.Node(7, Trees.Empty, Trees.Empty)))
+    val (result1, result2) = Trees.removeSameValuesDFS(tree1, tree2)
+    assert(result1 == Trees.Node(-1, Trees.Node(2, Trees.Empty, Trees.Node(5, Trees.Empty, Trees.Empty)), Trees.Empty))
+    assert(result2 == Trees.Node(-1, Trees.Node(3, Trees.Empty, Trees.Node(6, Trees.Empty, Trees.Empty)), Trees.Empty))
+  }
+  test("removeSameValuesDFS.2"){
+    val tree1 = Trees.Node(1, Trees.Node(1, Trees.Empty, Trees.Empty), Trees.Node(1, Trees.Empty, Trees.Empty))
+    val tree2 = Trees.Node(1, Trees.Node(1, Trees.Empty, Trees.Empty), Trees.Node(1, Trees.Empty, Trees.Empty))
+    val (result1, result2) = Trees.removeSameValuesDFS(tree1, tree2)
+    assert(result1 == Trees.Empty)
+    assert(result2 == Trees.Empty)
+  }
+  test("removeSameValuesDFS.emptyTrees"){
+    assert(Trees.removeSameValuesDFS(Trees.Empty, Trees.Empty) === (Trees.Empty, Trees.Empty))
+  }
+  test("removeSameValuesDFS.unequalTrees"){
+    assertThrows[Exception](Trees.removeSameValuesDFS(Trees.generateTree(3, 5, 8), Trees.generateTree(2, 6, 9)))
+  }
+
+  //task 3 BFS tests
 
 }
