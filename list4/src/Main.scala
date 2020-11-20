2834e043925f225d8b60ab406f2f108bceed49b6
wietrzynska7 <wietrzynska7@gmail.com>
2020-11-20 12:41:38 +0100

    Added tests

---
+++ list4/src/Main.scala
@@ -0,0 +1,14 @@
+import lazyListsTest.{zad4Test, zad5Test}
+import treesTest.{zad1Test, zad2Test, zad3Test}
+
+object Main {
+  def main(args: Array[String]): Unit = {
+    zad1Test()
+    zad2Test()
+    zad3Test()
+    zad4Test()
+    zad5Test()
+  }
+
+}
+
