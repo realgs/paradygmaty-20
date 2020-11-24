0df870b9718b750782e635189b5f1ba64f3bf367
Klaudia Blazyczek <blazyczek.klaudia@gmail.com>
2020-11-24 21:54:29 +0100

    Implement exc 5

--- PP_Lab/src/main/scala/Lista5/Lista5.scala
+++ PP_Lab/src/main/scala/Lista5/Lista5.scala
@@ -1,5 +1,37 @@
 package Lista5
 
+import scala.annotation.tailrec
+import scala.collection.immutable.Queue
+
 object Lista5 {
+
+  def duplicate[A](elementCollection: Queue[A], repetitionsCollection: Queue[Int]): Queue[A] = {
+    @tailrec
+    def innerDuplicate(elements: Queue[A], repetitions: Queue[Int], retCollection: Queue[A], currentCounter: Int): Queue[A] = {
+      if (currentCounter > 0 ){
+        innerDuplicate(elements, repetitions, retCollection.enqueue(elements.front), currentCounter-1)
+      }
+      else {
+        val repetitionDequeue = repetitions.dequeue
+        val elementsDequeue = elements.dequeue
+        if (repetitionDequeue._2.isEmpty || elementsDequeue._2.isEmpty){
+          retCollection
+        }
+        else{
+          innerDuplicate(elementsDequeue._2, repetitionDequeue._2, retCollection, repetitionDequeue._2.front)
+        }
+      }
+    }
+    if(elementCollection.isEmpty || repetitionsCollection.isEmpty) {
+      Queue()
+    }
+    else{
+      innerDuplicate(elementCollection, repetitionsCollection, Queue(), repetitionsCollection.front)
+    }
+  }
+
+
+
+
 
 }
--- PP_Lab/src/test/scala/Lista5/Lista5Test.scala
+++ PP_Lab/src/test/scala/Lista5/Lista5Test.scala
@@ -2,6 +2,15 @@ package Lista5
 
 import org.scalatest.FunSuite
 
+import scala.collection.immutable.Queue
+
 class Lista5Test extends FunSuite{
+  test("Lista 5 zadanie 1") {
+    assert(Lista5.duplicate(Queue(1,2,3), Queue(0,3,1,4)) == Queue(2,2,2,3))
+    assert(Lista5.duplicate(Queue(1,2,3), Queue(1,4)) == Queue(1,2,2,2,2))
+    assert(Lista5.duplicate(Queue('a','l','a'), Queue(1,2,-1)) == Queue('a','l','l'))
+    assert(Lista5.duplicate(Queue(), Queue(0,3,1,4)) == Queue())
+    assert(Lista5.duplicate(Queue(1,2,3), Queue()) == Queue())
+  }
 
 }
