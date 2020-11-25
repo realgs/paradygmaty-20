6c5e103858413acc9b06f30c454563d0208fe4ee
Klaudia Blazyczek <blazyczek.klaudia@gmail.com>
2020-11-25 13:34:51 +0100

    Implement exc 2 and its tests

--- PP_Lab/src/main/scala/Lista5/Lista5.scala
+++ PP_Lab/src/main/scala/Lista5/Lista5.scala
@@ -2,34 +2,62 @@ package Lista5
 
 import scala.annotation.tailrec
 import scala.collection.immutable.Queue
+import scala.collection.immutable.HashSet
 
 object Lista5 {
-
+  //zadanie 1 (2.5 pkt)
+  //wg. dokumentacji: enqueue, dequeue, front - O(1)
   def duplicate[A](elementCollection: Queue[A], repetitionsCollection: Queue[Int]): Queue[A] = {
     @tailrec
     def innerDuplicate(elements: Queue[A], repetitions: Queue[Int], retCollection: Queue[A], currentCounter: Int): Queue[A] = {
-      if (currentCounter > 0 ){
+      if (currentCounter > 0 )
         innerDuplicate(elements, repetitions, retCollection.enqueue(elements.front), currentCounter-1)
-      }
       else {
         val repetitionDequeue = repetitions.dequeue
         val elementsDequeue = elements.dequeue
-        if (repetitionDequeue._2.isEmpty || elementsDequeue._2.isEmpty){
+        if (repetitionDequeue._2.isEmpty || elementsDequeue._2.isEmpty)
           retCollection
-        }
-        else{
+        else
           innerDuplicate(elementsDequeue._2, repetitionDequeue._2, retCollection, repetitionDequeue._2.front)
-        }
       }
     }
-    if(elementCollection.isEmpty || repetitionsCollection.isEmpty) {
+
+    if(elementCollection.isEmpty || repetitionsCollection.isEmpty)
       Queue()
-    }
-    else{
+    else
       innerDuplicate(elementCollection, repetitionsCollection, Queue(), repetitionsCollection.front)
+  }
+
+
+  //zadanie 2 (2.5 pkt)
+  //wg. dokumentacji: enqueue, dequeue, front : O(1). Dla hashSet contains,+ : O(1)
+  def duplicateWithoutRepeating[A](elementCollection: Queue[A], repetitionsCollection: Queue[Int]): Queue[A] = {
+    if(elementCollection.isEmpty || repetitionsCollection.isEmpty)
+      Queue()
+    else {
+      @tailrec
+      //usuwanie powtorzen z kolejki elementow oraz odpowiadajace im wartosci powtorzen z kolejki ilosci powtorzen
+      def innerPrepare(elements: Queue[A], repetitions: Queue[Int], newElements: Queue[A], newRepetition: Queue[Int], checkSet: HashSet[A]): (Queue[A], Queue[Int]) = {
+        if (elements.isEmpty || repetitions.isEmpty)
+          (newElements, newRepetition)
+        else {
+          if (checkSet.contains(elements.front))
+            innerPrepare(elements.dequeue._2, repetitions.dequeue._2, newElements, newRepetition, checkSet)
+          else {
+            val repetitionDequeue = repetitions.dequeue
+            val elementsDequeue = elements.dequeue
+            innerPrepare(elementsDequeue._2, repetitionDequeue._2, newElements.enqueue(elementsDequeue._1), newRepetition.enqueue(repetitionDequeue._1), checkSet+elementsDequeue._1)
+          }
+        }
+      }
+      val processedQueues = innerPrepare(elementCollection, repetitionsCollection, Queue(), Queue(), HashSet())
+      duplicate(processedQueues._1, processedQueues._2)
     }
   }
 
+  //zadanie 3 (5 pkt)
+  //zadanie 4 (5 pkt)
+  //zadanie 5 (5 pkt)
 
 
 
--- PP_Lab/src/test/scala/Lista5/Lista5Test.scala
+++ PP_Lab/src/test/scala/Lista5/Lista5Test.scala
@@ -13,4 +13,14 @@ class Lista5Test extends FunSuite{
     assert(Lista5.duplicate(Queue(1,2,3), Queue()) == Queue())
   }
 
+  test("Lista 5 zadanie 2") {
+    assert(Lista5.duplicateWithoutRepeating(Queue(1,2,3,1,2,4,3), Queue(0,3,1,4,5,2,4)) == Queue(2,2,2,3,4,4))
+    assert(Lista5.duplicateWithoutRepeating(Queue(1,2,1,2,1,2), Queue(4,-2)) == Queue(1,1,1,1))
+    assert(Lista5.duplicateWithoutRepeating(Queue(), Queue(0,3,1,4,5,2,4)) == Queue())
+    assert(Lista5.duplicateWithoutRepeating(Queue(1,2,3,1,2,4,3), Queue()) == Queue())
+    assert(Lista5.duplicateWithoutRepeating(Queue('a','l','a'), Queue(0,3,1)) == Queue('l','l','l'))
+  }
+
+  
+
 }
