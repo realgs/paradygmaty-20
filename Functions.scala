b656a6269d5b8dc5f555fa4c8fb86d3100e3b408
JUSTY <maluszynskaj@gmail.com>
2020-11-28 19:13:43 +0100

    add ex 1,2 with simple tests

--- Functions.scala
+++ Functions.scala
@@ -1,3 +1,51 @@
+import scala.collection.immutable.Queue
+
 class Functions {
+  //ex 1 (2.5 pkt)
+  def duplicate[A](elements: Queue[A], repetitions: Queue[Int]): Queue[A] = {
+    @scala.annotation.tailrec
+    def duplicateInner[A](elements: Queue[A], element: A, repetitions: Queue[Int], counter: Int, resultQueue: Queue[A]): Queue[A] = {
+      if (counter > 0)
+        duplicateInner(elements, element, repetitions, counter - 1, resultQueue.enqueue(element))
+      else if (elements.isEmpty || repetitions.isEmpty) {
+        resultQueue
+      }
+      else {
+        val (nextElement, nextElements) = elements.dequeue
+        val (nextCounter, nextRepetitions) = repetitions.dequeue
+        duplicateInner(nextElements, nextElement, nextRepetitions, nextCounter, resultQueue)
+      }
+    }
+
+    if (elements.isEmpty || repetitions.isEmpty)
+      Queue()
+    else {
+      val (nextElement, nextElements) = elements.dequeue
+      val (nextCounter, nextRepetitions) = repetitions.dequeue
+      duplicateInner(nextElements, nextElement, nextRepetitions, nextCounter, Queue())
+    }
+  }
+
+  //ex 2 (2.5 pkt)
+  def duplicateWithNoReps[A](elements: Set[A], repetitions: Queue[Int]): Queue[A] = {
+    def duplicateInnerWithNoReps[A](elements: Set[A], element: A, repetitions: Queue[Int], counter: Int, resultQueue: Queue[A]): Queue[A] = {
+      if (counter > 0)
+        duplicateInnerWithNoReps(elements, element, repetitions, counter - 1, resultQueue.enqueue(element))
+      else if (elements.isEmpty || repetitions.isEmpty) {
+        resultQueue
+      }
+      else {
+        val (nextCounter, nextRepetitions) = repetitions.dequeue
+        duplicateInnerWithNoReps(elements.tail, elements.head, nextRepetitions, nextCounter, resultQueue)
+      }
+    }
+
+    if (elements.isEmpty || repetitions.isEmpty)
+      Queue()
+    else {
+      val (nextCounter, nextRepetitions) = repetitions.dequeue
+      duplicateInnerWithNoReps(elements.tail, elements.head, nextRepetitions, nextCounter, Queue())
+    }
+  }
 
 }
--- Tests.scala
+++ Tests.scala
@@ -1,3 +1,23 @@
-class Tests {
+import org.junit.jupiter.api.Test
 
+import scala.collection.immutable.Queue
+
+class Tests {
+  val testFunction = new Functions
+
+  @Test
+  def testFunction1 = {
+    assert(testFunction.duplicate(Queue(1, 2, 3), Queue(0, 3, 1, 4)) == Queue(2, 2, 2, 3))
+    assert(testFunction.duplicate(Queue(), Queue(1,2,3)) == Queue())
+    assert(testFunction.duplicate(Queue('b', 'a','b'), Queue(0, 0, 3)) == Queue('b', 'b', 'b'))
+  }
+
+  @Test
+  def testFunction2 = {
+    assert(testFunction.duplicateWithNoReps(Set(1, 2, 3), Queue(0, 3, 1, 4)) == Queue(2, 2, 2, 3))
+    assert(testFunction.duplicateWithNoReps(Set(), Queue(1,2,3)) == Queue())
+    assert(testFunction.duplicateWithNoReps(Set('b', 'a','c'), Queue(0, 0, 3)) == Queue('c', 'c', 'c'))
+    assert(testFunction.duplicateWithNoReps(Set(1,1,2,3), Queue(3,3,3,3)) == Queue(1,1,1,2,2,2,3,3,3))
+    assert(testFunction.duplicateWithNoReps(Set('a', 'n', 'n', 'a'), Queue(1,2,3,4)) == Queue('a', 'n', 'n'))
+  }
 }
