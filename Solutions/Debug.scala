fd8f53733d6280a5e320d57a5dd2ada2eec3bb58
JUSTY <maluszynskaj@gmail.com>
2020-12-01 20:05:45 +0100

    add tests to ex 4,5

--- Solutions/Debug.scala
+++ Solutions/Debug.scala
@@ -15,4 +15,18 @@ trait Debug {
     }
   }
 
+  def getName: String = getClass.getSimpleName
+
+  def getVars: Map[String, (Class[_], AnyRef)] = {
+    val fields = getClass.getDeclaredFields
+    var map: Map[String, (Class[_], AnyRef)] = Map()
+    var i = 0
+    while(i < fields.length){
+      fields(i).setAccessible(true)
+      map = map + (fields(i).getName -> (fields(i).getType, fields(i).get(this)))
+      i += 1
+    }
+    map
+  }
+
 }
--- Tests/Tests.scala
+++ Tests/Tests.scala
@@ -1,7 +1,8 @@
-package Tests
+package lab5
+
+import java.lang.reflect.AnnotatedElement
 
 import ClassesForTest.{Car, Point, Rectangle}
-import Solutions.Functions
 import org.junit.jupiter.api.Test
 
 import scala.collection.immutable.Queue
@@ -9,26 +10,25 @@ import scala.collection.immutable.Queue
 class Tests {
   val testFunction: Functions = new Functions
 
-
   @Test
   def testFunction1 = {
     assert(testFunction.duplicate(Queue(1, 2, 3), Queue(0, 3, 1, 4)) == Queue(2, 2, 2, 3))
-    assert(testFunction.duplicate(Queue(), Queue(1,2,3)) == Queue())
-    assert(testFunction.duplicate(Queue('b', 'a','b'), Queue(0, 0, 3)) == Queue('b', 'b', 'b'))
+    assert(testFunction.duplicate(Queue(), Queue(1, 2, 3)) == Queue())
+    assert(testFunction.duplicate(Queue('b', 'a', 'b'), Queue(0, 0, 3)) == Queue('b', 'b', 'b'))
   }
 
   @Test
   def testFunction2 = {
     assert(testFunction.duplicateWithNoReps(Set(1, 2, 3), Queue(0, 3, 1, 4)) == Queue(2, 2, 2, 3))
-    assert(testFunction.duplicateWithNoReps(Set(), Queue(1,2,3)) == Queue())
-    assert(testFunction.duplicateWithNoReps(Set('b', 'a','c'), Queue(0, 0, 3)) == Queue('c', 'c', 'c'))
-    assert(testFunction.duplicateWithNoReps(Set(1,1,2,3), Queue(3,3,3,3)) == Queue(1,1,1,2,2,2,3,3,3))
-    assert(testFunction.duplicateWithNoReps(Set('a', 'n', 'n', 'a'), Queue(1,2,3,4)) == Queue('a', 'n', 'n'))
+    assert(testFunction.duplicateWithNoReps(Set(), Queue(1, 2, 3)) == Queue())
+    assert(testFunction.duplicateWithNoReps(Set('b', 'a', 'c'), Queue(0, 0, 3)) == Queue('c', 'c', 'c'))
+    assert(testFunction.duplicateWithNoReps(Set(1, 1, 2, 3), Queue(3, 3, 3, 3)) == Queue(1, 1, 1, 2, 2, 2, 3, 3, 3))
+    assert(testFunction.duplicateWithNoReps(Set('a', 'n', 'n', 'a'), Queue(1, 2, 3, 4)) == Queue('a', 'n', 'n'))
   }
 
   @Test
   def testFunction3 = {
-    val p: Point = new Point(3,4)
+    val p: Point = new Point(3, 4)
     p.debugName
 
     val c: Car = new Car("Mercedes", "black")
@@ -40,7 +40,7 @@ class Tests {
 
   @Test
   def testFunction4 = {
-    val p: Point = new Point(3,4)
+    val p: Point = new Point(3, 4)
     p.debugVars()
 
     val c: Car = new Car("Mercedes", "black")
@@ -49,4 +49,27 @@ class Tests {
     val r: Rectangle = new Rectangle(5.5, 10.5)
     r.debugVars()
   }
+
+  @Test
+  def testFunction5 = {
+    //testing getName
+    val p: Point = new Point(3, 4)
+    assert(p.getName == "Point")
+
+    val c: Car = new Car("Mercedes", "black")
+    assert(c.getName == "Car")
+
+    val r: Rectangle = new Rectangle(5.5, 10.5)
+    assert(r.getName == "Rectangle")
+
+    //testing getVars (by printing)
+    printMap(p.getVars)
+    printMap(c.getVars)
+    printMap(r.getVars)
+  }
+
+  def printMap(m: Map[String, (Class[_], AnyRef)]) =
+    m.foreach {
+      case (key, (valType, value)) => println(s"Var $key => $valType, $value")
+    }
 }
