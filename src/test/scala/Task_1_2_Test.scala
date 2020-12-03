444dc393d335084e3f55541efe3276e00564a8b2
sofia-malyshok <soniamalish17@gmail.com>
2020-12-03 12:41:17 +0100

    Tests fixes

--- src/test/scala/Task_1_2_Test.scala
+++ src/test/scala/Task_1_2_Test.scala
@@ -1,7 +1,7 @@
 import org.scalatest.funsuite.AnyFunSuite
 
 class Task_1_2_Test extends AnyFunSuite {
-  test("zadanie1.test") {
+  test("task1.test") {
     assert(Task_1_2.repeat(List(2, 4, 3, 1), List(1, 2, 3)) == List(2, 4, 4, 3, 3, 3))
     assert(Task_1_2.repeat(List("ala", "ma", "kota"), List(0, 2, 1, 3, 9, 4)) == List("ma", "ma","kota"))
     assert(Task_1_2.repeat(List('+', '-', '+', '=', '/','?', '*'), List(2, 0, 4, 1)) == List('+', '+', '+', '+', '+', '+', '='))
@@ -9,7 +9,7 @@ class Task_1_2_Test extends AnyFunSuite {
     assert(Task_1_2.repeat(List(20, 40, 34, 1), List()) == Nil)
   }
 
-  test("zadanie2.test") {
+  test("task2.test") {
     assert(Task_1_2.repeateWithoutDublicates(List(2,4,3,1), List(1,2,3)) == List(2, 4, 4, 3, 3, 3))
     assert(Task_1_2.repeateWithoutDublicates(List("ala", "ma", "kota"), List(0, 2, 1, 3, 9, 4)) == List("ma", "ma", "kota"))
     assert(Task_1_2.repeateWithoutDublicates(List(), List(1, 2, 3)) == Nil)
--- src/test/scala/Task_3_4_5_Test.scala
+++ src/test/scala/Task_3_4_5_Test.scala
@@ -1,17 +1,17 @@
 import org.scalatest.funsuite.AnyFunSuite
 
 class Task_3_4_5_Test extends AnyFunSuite {
-  test("zadanie3.test") {
+  test("task3.test") {
     val p: Point = new Point(3, 4)
     p.debugName()
   }
 
-  test("zadanie4.test") {
+  test("task4.test") {
     val p: Point = new Point(3, 4)
     p.debugVars()
   }
 
-  test("zadanie5.test") {
+  test("task5.test") {
     var p: Point = new Point(3, 4)
     assert(p.getNameOfClass() == "Class: Point")
     assert(p.getListWithVarsInfo() == List("Var: x => int,3", "Var: y => int,4", "Var: a => java.lang.String,test"))
