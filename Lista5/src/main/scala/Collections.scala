7f278e1057a0cf9a22b174fca43201b78925931b
marcijay <jak.marcinko@gmail.com>
2020-12-03 13:15:32 +0100

    Added task 2 (using set) with tests

--- Lista5/src/main/scala/Collections.scala
+++ Lista5/src/main/scala/Collections.scala
@@ -25,4 +25,26 @@ object Collections {
       tailDuplicate(collectionOfElements, collectionOfRepetitionNumbers, Queue(), collectionOfRepetitionNumbers.front)
   }
 
+  //Zad 2 (2,5 ptk)
+  //Wszystkie powtarzające się elementy w kolekcji wejściowej są ignorowane, ale ma to wpływ na podane liczby powtórzeń dla konkretnych elementów
+  def duplicateNoEntryDuplicates[A](collectionOfElements: Set[A], collectionOfRepetitionNumbers: Queue[Int]): Queue[A] = {
+    @tailrec
+    def tailDuplicate(elements: Set[A], repetitions: Queue[Int], resultCollection: Queue[A], repCounter: Int): Queue[A] = {
+      if (repCounter > 0 )
+        tailDuplicate(elements, repetitions, resultCollection.enqueue(elements.head), repCounter-1)
+      else {
+        val newElements = elements.tail
+        val (_, newRepetitions) = repetitions.dequeue
+        if (newElements.isEmpty || newRepetitions.isEmpty)
+          resultCollection
+        else
+          tailDuplicate(newElements, newRepetitions, resultCollection, newRepetitions.front)
+      }
+    }
+
+    if(collectionOfElements.isEmpty || collectionOfRepetitionNumbers.isEmpty)
+      Queue()
+    else
+      tailDuplicate(collectionOfElements, collectionOfRepetitionNumbers, Queue(), collectionOfRepetitionNumbers.front)
+  }
 }
--- Lista5/src/test/scala/CollectionsTest.scala
+++ Lista5/src/test/scala/CollectionsTest.scala
@@ -25,7 +25,39 @@ class CollectionsTest extends FunSuite{
   test("duplicate.negativeRepetitions") {
     assert(Collections.duplicate(Queue(1, 2, 3), Queue(3, -2, 1)) == Queue(1,1,1,3))
   }
+  test("duplicate.duplicateElements") {
+    assert(Collections.duplicate(Queue(1, 1, 3), Queue(3, 2, 1)) == Queue(1,1,1,1,1,3))
+  }
   test("duplicate.equalSizes_String") {
     assert(Collections.duplicate(Queue("Ala", "ma", "kota"), Queue(3, 2, 1)) == Queue("Ala","Ala","Ala","ma","ma","kota"))
+  }
+
+  //Zad 2 tests
+  test("duplicateNoEntryDuplicates.bothEmpty") {
+    assert(Collections.duplicateNoEntryDuplicates(Set(), Queue()) == Queue())
+  }
+  test("duplicateNoEntryDuplicates.repetitionsEmpty") {
+    assert(Collections.duplicateNoEntryDuplicates(Set(1, 2, 3), Queue()) == Queue())
+  }
+  test("duplicateNoEntryDuplicates.elementsEmpty") {
+    assert(Collections.duplicateNoEntryDuplicates(Set(), Queue(1, 2, 3)) == Queue())
+  }
+  test("duplicateNoEntryDuplicates.equalSizes") {
+    assert(Collections.duplicateNoEntryDuplicates(Set(1, 2, 3), Queue(3, 2, 1)) == Queue(1,1,1,2,2,3))
+  }
+  test("duplicateNoEntryDuplicates.lessRepetitions") {
+    assert(Collections.duplicateNoEntryDuplicates(Set(1, 2, 3), Queue(3, 2)) == Queue(1,1,1,2,2))
+  }
+  test("duplicateNoEntryDuplicates.moreRepetitions") {
+    assert(Collections.duplicateNoEntryDuplicates(Set(1, 2, 3), Queue(3, 2, 1, 4)) == Queue(1,1,1,2,2,3))
+  }
+  test("duplicateNoEntryDuplicates.negativeRepetitions") {
+    assert(Collections.duplicateNoEntryDuplicates(Set(1, 2, 3), Queue(3, -2, 1)) == Queue(1,1,1,3))
+  }
+  test("duplicateNoEntryDuplicates.duplicateElements") {
+    assert(Collections.duplicateNoEntryDuplicates(Set(1, 1, 3), Queue(3, 2, 1)) == Queue(1,1,1,3,3))
+  }
+  test("duplicateNoEntryDuplicates.equalSizes_String") {
+    assert(Collections.duplicateNoEntryDuplicates(Set("Ala", "ma", "kota"), Queue(3, 2, 1)) == Queue("Ala","Ala","Ala","ma","ma","kota"))
   }
 }
