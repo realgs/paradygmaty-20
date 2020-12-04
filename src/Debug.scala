535718883261d786f1da0fed94384f0c9604d88a
MrQubek <winiarz.paw@gmail.com>
2020-12-04 15:47:25 +0100

    replace function debugGetVarsList(): Array[Array[Any]] with debugGetVarsArray(): Array[Array[Any]]
    refactor tests

--- src/Debug.scala
+++ src/Debug.scala
@@ -11,11 +11,11 @@ trait Debug {
   // 5 pkt
   private[this] def getVals[A](f: Field): String = {
     f.setAccessible(true)
-    "Var: " + f.getName+" => "+ f.getType + ", " + f.get(this)
+    "Var: " + f.getName + " => " + f.getType + ", " + f.get(this)
   }
 
   def debugVars(): Unit = {
-    for( i <- 0 to this.getClass.getDeclaredFields.length-1){
+    for (i <- 0 until this.getClass.getDeclaredFields.length) {
       println(getVals(this.getClass.getDeclaredFields.array(i)))
     }
   }
@@ -24,25 +24,27 @@ trait Debug {
   // 5 pkt
   def debugGetClassName(): String = this.getClass.getName
 
-  private[this] def getFieldName(f: Field):String = f.getName
-  private[this] def getFieldType(f: Field):String = f.getType.toString
-  private[this] def getFieldVal(f: Field):String = {
+  private[this] def getFieldName(f: Field): String = f.getName
+
+  private[this] def getFieldType(f: Field): Class[_] = f.getType
+
+  private[this] def getFieldVal(f: Field): Any = {
     f.setAccessible(true)
-    val tmp=f.get(this).toString
+    val tmp = f.get(this)
     f.setAccessible(false)
     tmp
   }
-  private[this] def getFiledParamsAsList(f: Field):List[String] =
-    List(getFieldName(f), getFieldType(f), getFieldVal(f))
 
-  //return value: List of Lists of String. Every List of Strings describe one variable and consist 3 elements in order:
-  // name of field, type of field and value of field
-  def debugGetVarsList(): List[List[String]] = {
-    @tailrec
-    def helper(array: Array[Field],acc:List[List[String]], beginIndex:Int, endIndex:Int):List[List[String]] = {
-      if(beginIndex>=endIndex) helper(array, getFiledParamsAsList(array(beginIndex)) +:acc,beginIndex-1,endIndex)
-      else acc
+  private[this] def getFieldParamsAsArray(f: Field): Array[Any] =
+    Array(getFieldName(f), getFieldType(f), getFieldVal(f))
+
+  //return value: Array of Array of Any. Every array inside array describes one variable and consist 3 elements in order:
+  // name of field (String), type of field (Class[_]) and value of field (Any)
+  def debugGetVarsArray(): Array[Array[Any]] = {
+    val arr = new Array[Array[Any]](this.getClass.getDeclaredFields.array.length)
+    for (i <- 0 until this.getClass.getDeclaredFields.array.length) {
+      arr(i) = getFieldParamsAsArray(this.getClass.getDeclaredFields.array(i))
     }
-    helper(this.getClass.getDeclaredFields,Nil,this.getClass.getDeclaredFields.length-1,0)
+    arr
   }
 }
--- src/TestsForLista5.scala
+++ src/TestsForLista5.scala
@@ -1,10 +1,9 @@
 
 import scala.collection.mutable
-
 import Lista5.duplicate
 import Lista5.duplicate2
 
-object TestsForLista5 extends App{
+object TestsForLista5 extends App {
 
   var testDequeue1 = new mutable.ArrayDeque[Int]()
   testDequeue1.append(1)
@@ -12,10 +11,10 @@ object TestsForLista5 extends App{
   testDequeue1.append(3)
 
   println("Tests for zad 1")
-  println(duplicate(testDequeue1,List(-1,0,2)))
-  println(duplicate(testDequeue1,List(1,2,3)))
-  println(duplicate(testDequeue1,List(1,2,3,4,5)))
-  println(duplicate(testDequeue1,List(1,2)))
+  println(duplicate(testDequeue1, List(-1, 0, 2)) == mutable.ArrayDeque(3, 3))
+  println(duplicate(testDequeue1, List(1, 2, 3)) == mutable.ArrayDeque(1, 2, 2, 3, 3, 3))
+  println(duplicate(testDequeue1, List(1, 2, 3, 4, 5)) == mutable.ArrayDeque(1, 2, 2, 3, 3, 3))
+  println(duplicate(testDequeue1, List(1, 2)) == mutable.ArrayDeque(1, 2, 2))
 
   println("\nTests for zad 2")
   var testDequeue2 = new mutable.ArrayDeque[Char]()
@@ -24,13 +23,14 @@ object TestsForLista5 extends App{
   testDequeue2.append('a')
 
   try {
-    println(duplicate2(testDequeue1, List(1, 2, 3)))
-  }catch{
+    println(duplicate2(testDequeue1, List(1, 2, 3)) == mutable.ArrayDeque(1, 2, 2, 3, 3, 3))
+  } catch {
     case e: Exception => println("Duplicate exception captured")
   }
+
   try {
-    println(duplicate2(testDequeue2, List(1, 2, 3)))
-  }catch{
+    println(duplicate2(testDequeue2, List(1, 2, 3)) == mutable.ArrayDeque('a', 'l', 'l', 'a', 'a', 'a'))
+  } catch {
     case e: Exception => println("Duplicate exception captured")
   }
 
@@ -44,13 +44,19 @@ object TestsForLista5 extends App{
 
   println("\nTests for zad 4")
   testDebug1.debugVars()
+
+  println("")
   testDebug2.debugVars()
 
   println("\nTests for zad 5")
-  println(testDebug1.debugGetClassName())
-  println(testDebug1.debugGetVarsList())
-  println(testDebug2.debugGetClassName())
-  println(testDebug2.debugGetVarsList())
+  println(testDebug1.debugGetClassName()=="TestClass1")
+  println(testDebug1.debugGetVarsArray()(0)(0))
+  println(testDebug1.debugGetVarsArray()(0)(1))
+  println(testDebug1.debugGetVarsArray()(0)(2))
 
+  println("\n" + (testDebug2.debugGetClassName()=="TestClass2"))
+  println(testDebug2.debugGetVarsArray()(0)(0))
+  println(testDebug2.debugGetVarsArray()(0)(1))
+  println(testDebug2.debugGetVarsArray()(0)(2))
 
 }
