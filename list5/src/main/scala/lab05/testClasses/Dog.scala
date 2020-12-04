3b06d811e2541111bc41fb28160a769950f8c40e
wietrzynska7 <wietrzynska7@gmail.com>
2020-12-04 18:03:10 +0100

    added test classes

---
+++ list5/src/main/scala/lab05/testClasses/Dog.scala
@@ -0,0 +1,9 @@
+package lab05.testClasses
+
+import lab05.Debug
+
+class Dog(dogName: String, dogAge: Int) extends Debug {
+  var name: String = dogName
+  var age: Int = dogAge
+  var vaccinated  = false
+}
---
+++ list5/src/main/scala/lab05/testClasses/Point.scala
@@ -0,0 +1,8 @@
+package lab05.testClasses
+import lab05.Debug
+
+class Point(xv: Int, yv: Int) extends Debug {
+  var x: Int = xv
+  var y: Int = yv
+  var a: String = "test"
+}
