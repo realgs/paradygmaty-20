001e3e5a11f035944f0f2c9002e9b86b0f6b3411
wietrzynska7 <wietrzynska7@gmail.com>
2020-12-04 17:59:45 +0100

    added Debug trait

--- list5/src/main/scala/lab05/Debug.scala
+++ list5/src/main/scala/lab05/Debug.scala
@@ -1,5 +1,28 @@
 package lab05
 
-class Debug {
+import java.lang.reflect.AnnotatedType
 
+//zadanie 3 (5pkt)
+trait Debug {
+  def debugName(): Unit = println("Class: " + getClass.getSimpleName)
+
+  //zadanie 4 (5pkt)
+  def debugVars(): Unit = {
+    for (field <- getClass.getDeclaredFields) {
+      field.setAccessible(true)
+      println("Var: " + field.getName + "=>" + field.getAnnotatedType + ", " + field.get(this))
+      field.setAccessible(false)
+    }
+  }
+
+  //zadanie 5 (5pkt)
+  def debugStringName: String = getClass.getSimpleName
+
+  def debugVarsArray: Array[(String, AnnotatedType, AnyRef)] = getClass.getDeclaredFields.map { field =>
+    field.setAccessible(true)
+    val varsArray = (field.getName, field.getAnnotatedType, field.get(this))
+    field.setAccessible(false)
+    varsArray
+  }
 }
+
