874da6154a91ddbb8b1083ea307d7e196d9a7e63
bedni <w.bednarz2000@gmail.com>
2020-12-06 20:33:29 +0100

    Initial commit

---
+++ Lab5/src/Tasks.scala
@@ -0,0 +1,20 @@
+object Tasks {
+
+  // Task 1. (2.5 pkt)
+
+  // Task 2. (2.5 pkt)
+
+  // Tasks 3, 4, 5
+  trait Debug {
+
+    // Task 3. (5 pkt)
+
+
+    // Task 4. (5 pkt)
+
+
+    // Task 5. (5 pkt)
+
+  }
+
+}
---
+++ Lab5/src/Tests.scala
@@ -0,0 +1,16 @@
+object Tests {
+
+  def main(args: Array[String]): Unit = {
+
+    // Task 1. tests
+
+    // Task 2. tests
+
+    // Task 3. tests
+
+    // Task 4. tests
+
+    // Task 5. tests
+
+  }
+}
