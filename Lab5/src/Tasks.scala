4ee3b8acf2e29ef56c04bb2224329c065365fc01
bedni <w.bednarz2000@gmail.com>
2020-12-06 21:48:08 +0100

    Tasks and tests implementation

--- Lab5/src/Tasks.scala
+++ Lab5/src/Tasks.scala
@@ -1,20 +1,82 @@
+import java.lang.reflect.AnnotatedElement
+
+import scala.annotation.tailrec
+import scala.collection.immutable.Queue
+
 object Tasks {
 
   // Task 1. (2.5 pkt)
+  def duplicate[A](collection: Queue[A], numbersOfDuplications: Queue[Int]): Queue[A] = {
+    @tailrec
+    def duplicateInner[B](collection: Queue[B], numbersOfDuplications: Queue[Int], elementToDuplicate: B, duplicationCounter: Int, result: Queue[B]): Queue[B] =
+      // don't throw Exception if negative number
+      if(duplicationCounter > 0) duplicateInner(collection, numbersOfDuplications, elementToDuplicate, duplicationCounter - 1, result.enqueue(elementToDuplicate))
+      else if(collection.isEmpty || numbersOfDuplications.isEmpty) result
+      else {
+        val (first, newCollection) = collection.dequeue
+        val (firstDuplicationNumber, newNumbersOfDuplications) = numbersOfDuplications.dequeue
+
+        duplicateInner(newCollection, newNumbersOfDuplications, first, firstDuplicationNumber, result)
+      }
+
+    if(collection.isEmpty || numbersOfDuplications.isEmpty) Queue()
+    else {
+      val (first, newCollection) = collection.dequeue
+      val (firstDuplicationNumber, newNumbersOfDuplications) = numbersOfDuplications.dequeue
+
+      duplicateInner(newCollection, newNumbersOfDuplications, first, firstDuplicationNumber, Queue())
+    }
+  }
 
   // Task 2. (2.5 pkt)
+  def duplicateWithoutRepetitions[A](collection: Set[A], numbersOfDuplications: Queue[Int]): Queue[A] = {
+    @tailrec
+    def duplicateInner[B](collection: Set[B], numbersOfDuplications: Queue[Int], elementToDuplicate: B, duplicationCounter: Int, result: Queue[B]): Queue[B] =
+    // don't throw Exception if negative number
+      if(duplicationCounter > 0) duplicateInner(collection, numbersOfDuplications, elementToDuplicate, duplicationCounter - 1, result.enqueue(elementToDuplicate))
+      else if(collection.isEmpty || numbersOfDuplications.isEmpty) result
+      else {
+        val (firstDuplicationNumber, newNumbersOfDuplications) = numbersOfDuplications.dequeue
+
+        duplicateInner(collection.tail, newNumbersOfDuplications, collection.head, firstDuplicationNumber, result)
+      }
+
+    if(collection.isEmpty || numbersOfDuplications.isEmpty) Queue()
+    else {
+
+      val (firstDuplicationNumber, newNumbersOfDuplications) = numbersOfDuplications.dequeue
+
+      duplicateInner(collection.tail, newNumbersOfDuplications, collection.head, firstDuplicationNumber, Queue.empty)
+    }
+  }
 
   // Tasks 3, 4, 5
   trait Debug {
 
     // Task 3. (5 pkt)
-
+    def debugName(): Unit =
+      println(s"Class: ${getClass.getSimpleName}")
 
     // Task 4. (5 pkt)
-
+    def debugVars(): Unit =
+      getClass.getDeclaredFields.foreach(field => {
+        field.setAccessible(true)
+        println(s"Var: ${field.getName} => ${field.getAnnotatedType}, ${field.get(this)}")
+      })
 
     // Task 5. (5 pkt)
+    def getClassName: String = getClass.getSimpleName
 
+    def getClassFields: Map[String, (AnnotatedElement, AnyRef)] = {
+
+      var fields: Map[String, (AnnotatedElement, AnyRef)] = Map()
+
+      getClass.getDeclaredFields.foreach(field => {
+        field.setAccessible(true)
+        fields = fields + (field.getName -> (field.getType, field.get(this)))
+      })
+
+      fields
+    }
   }
-
 }
--- Lab5/src/Tests.scala
+++ Lab5/src/Tests.scala
@@ -1,16 +1,37 @@
+import Tasks._
+import scala.collection.immutable.Queue
+
 object Tests {
 
   def main(args: Array[String]): Unit = {
 
     // Task 1. tests
+    println(duplicate(Queue(), Queue()) == Queue())
+    println(duplicate(Queue(1), Queue(4)) == Queue(1, 1, 1, 1))
+    println(duplicate(Queue(1), Queue(4, 2)) == Queue(1, 1, 1, 1))
+    println(duplicate(Queue(), Queue(4, 2)) == Queue())
+    println(duplicate(Queue(3, 6), Queue()) == Queue())
+    println(duplicate(Queue(3, 6, -321, 3213), Queue(5, 7, 0, 2)) == Queue(3, 3, 3, 3, 3, 6, 6, 6, 6, 6, 6, 6, 3213, 3213))
 
     // Task 2. tests
+    println(duplicateWithoutRepetitions(Set(), Queue()) == Queue())
+    println(duplicateWithoutRepetitions(Set(1), Queue(4)) == Queue(1, 1, 1, 1))
+    println(duplicateWithoutRepetitions(Set(1), Queue(4, 2)) == Queue(1, 1, 1, 1))
+    println(duplicateWithoutRepetitions(Set(), Queue(4, 2)) == Queue())
+    println(duplicateWithoutRepetitions(Set(3, 6), Queue()) == Queue())
+    println(duplicateWithoutRepetitions(Set(3, 6, -321, 3213), Queue(5, 7, 0, 2)) == Queue(3, 3, 3, 3, 3, 6, 6, 6, 6, 6, 6, 6, 3213, 3213))
+    println(duplicateWithoutRepetitions(Set(1, 1, 1), Queue(2, 3, 4)) == Queue(1, 1))
+    println(duplicateWithoutRepetitions(Set(1, 1, 2, 1, 4, 2), Queue(2, 3, 4, 5, 6, 7)) == Queue(1, 1, 2, 2, 2, 4, 4, 4, 4))
 
     // Task 3. tests
+    val p = new Point(3, 4)
+    p.debugName()
 
     // Task 4. tests
+    p.debugVars()
 
     // Task 5. tests
-
+    println(p.getClassName)
+    println(p.getClassFields)
   }
 }
