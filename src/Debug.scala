3da9db8563bf9e58ad0a74fbd50edd3b36825c84
Maciej Kopiński <mackopin@gmail.com>
2020-12-09 23:11:24 +0100

    Exercise 4

--- src/Debug.scala
+++ src/Debug.scala
@@ -2,13 +2,24 @@ trait Debug {
 
   //Zadanie 3
   def debugName() : Unit = {
-    println("Class: " + getClass())
+    println("Class: " + getClass().getName())
   }
 
   def debugVars() : Unit = {
-
+    val fields = getClass().getDeclaredFields()
+    println()
+    for( i <- 0 until fields.length)
+    {
+      val field = fields(i).toString().split(" ")
+      fields(i).setAccessible(true)
+      println("Var: " + field(2).substring(getClass().getName().length()+1) + " => " + field(1) + ", " + fields(i).get(this))
+    }
   }
 
-  //Zadanie 5.1
+//  Var: x => int, 3
+//  Var: y => int, 4
+//  Var: a => java.lang.String, test
+
+  //Zadanie 5
 
 }
--- src/Tests.scala
+++ src/Tests.scala
@@ -4,6 +4,7 @@ object Tests{
   def main(args: Array[String]): Unit={
     val point = new Point(1, 2)
     point.debugName()
+    point.debugVars()
   }
-  
+
 }
