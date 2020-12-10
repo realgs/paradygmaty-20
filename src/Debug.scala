9500d2e098a542503fa16f13f01474b4b125b065
Maciej Kopiński <mackopin@gmail.com>
2020-12-10 23:31:40 +0100

    Exercise 5

--- src/Debug.scala
+++ src/Debug.scala
@@ -1,3 +1,5 @@
+import java.lang.reflect.AnnotatedElement
+
 trait Debug {
 
   //Zadanie 3
@@ -7,7 +9,6 @@ trait Debug {
 
   def debugVars() : Unit = {
     val fields = getClass().getDeclaredFields()
-    println()
     for( i <- 0 until fields.length)
     {
       val field = fields(i).toString().split(" ")
@@ -21,5 +22,22 @@ trait Debug {
 //  Var: a => java.lang.String, test
 
   //Zadanie 5
+  def returnDebugName() : String={
+    "Class: " + getClass().getName()
+  }
+
+  def returnDebugVars() : List[(String, Class[_], Any)]={
+    var l=List[(String, Class[_], Any)]();
+    val fields = getClass().getDeclaredFields()
+    for( i <- 0 until fields.length) {
+    {
+      val field = fields(i).toString().split(" ")
+      fields(i).setAccessible(true)
+      println(fields(i).get(this).getClass())
+      l=((field(2).substring(getClass().getName().length()+1)).toString ,fields(i).get(this).getClass(), fields(i).get(this))::l
+    }
+    }
+    l.reverse
+  }
 
 }
--- src/Tests.scala
+++ src/Tests.scala
@@ -5,6 +5,8 @@ object Tests{
     val point = new Point(1, 2)
     point.debugName()
     point.debugVars()
+    println(point.returnDebugName().toString())
+    println(point.returnDebugVars().toString())
   }
 
 }
