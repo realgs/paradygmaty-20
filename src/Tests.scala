1950d31b4d0e83f768770d655237f0988d232c22
Maciej Kopiński <mackopin@gmail.com>
2020-12-11 12:59:39 +0100

    Addded tests

--- src/Tests.scala
+++ src/Tests.scala
@@ -5,8 +5,18 @@ object Tests{
     val point = new Point(1, 2)
     point.debugName()
     point.debugVars()
-    println(point.returnDebugName().toString())
-    println(point.returnDebugVars().toString())
+    println(point.returnDebugName())
+    println(point.returnDebugVars())
+    val point2 = new Point(-1, 5)
+    point2.debugName()
+    point2.debugVars()
+    println(point2.returnDebugName().toString())
+    println(point2.returnDebugVars().toString())
+    val date = new Date(11, 12, 2020)
+    date.debugName()
+    date.debugVars()
+    println(date.returnDebugName())
+    println(date.returnDebugVars())
   }
 
 }
