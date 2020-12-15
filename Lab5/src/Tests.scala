2fc5d8785aee4d81e40b8ad4b4f00e3c7b115b14
bedni <w.bednarz2000@gmail.com>
2020-12-15 19:36:20 +0100

    Modified tests to assertions

--- Lab5/src/Tests.scala
+++ Lab5/src/Tests.scala
@@ -6,25 +6,24 @@ object Tests {
   def main(args: Array[String]): Unit = {
 
     // Task 1. tests
-    println(duplicate(Queue(), Queue()) == Queue())
-    println(duplicate(Queue(1), Queue(4)) == Queue(1, 1, 1, 1))
-    println(duplicate(Queue(1), Queue(4, 2)) == Queue(1, 1, 1, 1))
-    println(duplicate(Queue(), Queue(4, 2)) == Queue())
-    println(duplicate(Queue(3, 6), Queue()) == Queue())
-    println(duplicate(Queue(3, 6, -321, 3213), Queue(5, 7, 0, 2)) == Queue(3, 3, 3, 3, 3, 6, 6, 6, 6, 6, 6, 6, 3213, 3213))
+    assert(duplicate(Queue(), Queue()) == Queue())
+    assert(duplicate(Queue(1), Queue(4)) == Queue(1, 1, 1, 1))
+    assert(duplicate(Queue(1), Queue(4, 2)) == Queue(1, 1, 1, 1))
+    assert(duplicate(Queue(), Queue(4, 2)) == Queue())
+    assert(duplicate(Queue(3, 6), Queue()) == Queue())
+    assert(duplicate(Queue(3, 6, -321, 3213), Queue(5, 7, 0, 2)) == Queue(3, 3, 3, 3, 3, 6, 6, 6, 6, 6, 6, 6, 3213, 3213))
 
     // Task 2. tests
     //TODO use LinkedHashSet
-    println(duplicateWithoutRepetitions(Set(), Queue()) == Queue())
-    println(duplicateWithoutRepetitions(Set(1), Queue(4)) == Queue(1, 1, 1, 1))
-    println(duplicateWithoutRepetitions(Set(1), Queue(4, 2)) == Queue(1, 1, 1, 1))
-    println(duplicateWithoutRepetitions(Set(), Queue(4, 2)) == Queue())
-    println(duplicateWithoutRepetitions(Set(3, 6), Queue()) == Queue())
-    println(duplicateWithoutRepetitions(Set(3, 6, -321, 3213), Queue(5, 7, 0, 2)) == Queue(3, 3, 3, 3, 3, 6, 6, 6, 6, 6, 6, 6, 3213, 3213))
-    println(duplicateWithoutRepetitions(Set(1, 1, 1), Queue(2, 3, 4)) == Queue(1, 1))
-    println(duplicateWithoutRepetitions(Set(1, 1, 2, 1, 4, 2), Queue(2, 3, 4, 5, 6, 7)) == Queue(1, 1, 2, 2, 2, 4, 4, 4, 4))
+    assert(duplicateWithoutRepetitions(Set(), Queue()) == Queue())
+    assert(duplicateWithoutRepetitions(Set(1), Queue(4)) == Queue(1, 1, 1, 1))
+    assert(duplicateWithoutRepetitions(Set(1), Queue(4, 2)) == Queue(1, 1, 1, 1))
+    assert(duplicateWithoutRepetitions(Set(), Queue(4, 2)) == Queue())
+    assert(duplicateWithoutRepetitions(Set(3, 6), Queue()) == Queue())
+    assert(duplicateWithoutRepetitions(Set(3, 6, -321, 3213), Queue(5, 7, 0, 2)) == Queue(3, 3, 3, 3, 3, 6, 6, 6, 6, 6, 6, 6, 3213, 3213))
+    assert(duplicateWithoutRepetitions(Set(1, 1, 1), Queue(2, 3, 4)) == Queue(1, 1))
+    assert(duplicateWithoutRepetitions(Set(1, 1, 2, 1, 4, 2), Queue(2, 3, 4, 5, 6, 7)) == Queue(1, 1, 2, 2, 2, 4, 4, 4, 4))
 
-    // TODO use assertions
     // Task 3. tests
     val p = new Point(3, 4)
     p.debugName()
@@ -36,9 +35,9 @@ object Tests {
     tc.debugVars()
 
     // Task 5. tests
-    println(p.getClassName)
-    println(p.getClassFields)
-    println(tc.getClassName)
-    println(tc.getClassFields)
+    assert(p.getClassName == "Point")
+    assert(p.getClassFields == Map("x" -> (classOf[Int], 3), "y" -> (classOf[Int], 4), "a" -> (classOf[String], "test")))
+    assert(tc.getClassName == "TestClass")
+    assert(tc.getClassFields == Map("a" -> (classOf[Int], 1), "b" -> (classOf[Double], -5.0), "c" -> (classOf[String], "DEFAULT TEST STRING"), "d" -> (classOf[String], "TEST")))
   }
 }
