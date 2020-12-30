319e047f01d1e05d3a94247b908b6738a51ea2b2
Jakub Kochanski <jakub1134@o2.pl>
2020-12-30 13:17:12 +0100

    Merge sort

---
+++ lista6/src/SequentialProgramming.scala
@@ -0,0 +1,47 @@
+
+object SequentialProgramming {
+ def mergeSort(arrayToSort: Array[Int]):Unit = {
+    def merge(startIndex: Int,middleIndex: Int,endIndex: Int):Unit = {
+      val tempArray = arrayToSort.slice(startIndex,endIndex + 1)
+
+      var firstIndex = 0
+      var secondIndex = middleIndex + 1 - startIndex
+      var currentIndex = startIndex
+
+      while(firstIndex + startIndex <= middleIndex && secondIndex + startIndex <= endIndex) {
+        if(tempArray(firstIndex) < tempArray(secondIndex)) {
+          arrayToSort(currentIndex) = tempArray(firstIndex)
+          firstIndex = firstIndex + 1
+        }
+        else {
+          arrayToSort(currentIndex) = tempArray(secondIndex)
+          secondIndex = secondIndex + 1
+        }
+        currentIndex = currentIndex + 1
+      }
+
+      while(firstIndex + startIndex <= middleIndex) {
+        arrayToSort(currentIndex) = tempArray(firstIndex)
+        firstIndex = firstIndex + 1
+        currentIndex = currentIndex + 1
+      }
+
+      while(secondIndex + startIndex <= endIndex) {
+        arrayToSort(currentIndex) = tempArray(secondIndex)
+        currentIndex = currentIndex + 1
+        secondIndex = secondIndex + 1
+      }
+
+    }
+
+    def sort(startIndex: Int,endIndex: Int):Unit = {
+      if(startIndex < endIndex) {
+        sort(startIndex, (startIndex + endIndex) / 2)
+        sort((startIndex + endIndex) / 2 + 1, endIndex)
+        merge(startIndex,(startIndex + endIndex) / 2,endIndex)
+      }
+    }
+
+    sort(0,arrayToSort.length - 1)
+ }
+}
