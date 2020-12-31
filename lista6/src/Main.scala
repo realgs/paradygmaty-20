006fe8fd2ef8be8f2211fce86a8589b0702d1f65
Jakub Kochanski <jakub1134@o2.pl>
2020-12-31 10:05:44 +0100

    Longest common prefix

--- lista6/src/Main.scala
+++ lista6/src/Main.scala
@@ -1,7 +1,8 @@
-import Tests.{matrixProductTest, mergeSortTest}
+import Tests.{longestCommonPrefixTest, matrixProductTest, mergeSortTest}
 object Main extends App {
   /* Przetwarzanie równoległe pozwala osiągnąć spory zysk czasowy,ale tylko w sytuacji gdy zadanie do wykonania równoległego jest odpowiedniej wielkości.
   Dla niewielkiej ilości danych przetwarzanie równoległe może spowolnić obliczenia. */
   mergeSortTest()
   matrixProductTest()
+  longestCommonPrefixTest()
 }
