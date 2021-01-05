87d8e36830d43c0f87981fd733a11fc084fa8200
arko0x <arkadiusz88084@gmail.com>
2021-01-05 18:33:21 +0100

    add array sum

---
+++ src/ArraySum.scala
@@ -0,0 +1,43 @@
+import Parallel._
+import org.scalameter._
+
+
+import scala.util.Random
+
+object ArraySum {
+  private def sum(array: Array[Int], startIndex: Int, endIndex: Int): Int = {
+    var sum: Int = 0
+    for (i <- Range(startIndex, endIndex)) sum = sum + array(i)
+    sum
+  }
+
+  def calculateSum(array: Array[Int]): Int = {
+    if (array.isEmpty) 0
+    else sum(array, 0, array.length - 1)
+  }
+
+  def parCalculateSum(array: Array[Int]): Int = {
+    if (array.isEmpty) 0
+    else {
+      val result = parallel(sum(array, 0, array.length / 2), sum(array, array.length / 2 + 1, array.length - 1))
+      result._1 + result._2
+    }
+  }
+
+  def main(args: Array[String]): Unit = {
+    val array = Array.fill(100000000)(new Random().nextInt(10))
+
+    Thread.sleep(500)
+
+    val time1 = measure {
+      parCalculateSum(array)
+    }
+
+    val time2 = measure {
+      calculateSum(array)
+    }
+
+    println(time1)
+    println(time2)
+  }
+}
