38e599d5111737a4cd7161cc4fd1da9eadbe303e
arko0x <arkadiusz88084@gmail.com>
2021-01-05 18:42:01 +0100

    fix array sum

--- src/ArraySum.scala
+++ src/ArraySum.scala
@@ -13,13 +13,13 @@ object ArraySum {
 
   def calculateSum(array: Array[Int]): Int = {
     if (array.isEmpty) 0
-    else sum(array, 0, array.length - 1)
+    else sum(array, 0, array.length)
   }
 
   def parCalculateSum(array: Array[Int]): Int = {
     if (array.isEmpty) 0
     else {
-      val result = parallel(sum(array, 0, array.length / 2), sum(array, array.length / 2 + 1, array.length - 1))
+      val result = parallel(sum(array, 0, array.length / 2), sum(array, array.length / 2, array.length))
       result._1 + result._2
     }
   }
