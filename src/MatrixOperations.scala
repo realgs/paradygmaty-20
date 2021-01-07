78ddc997ff00ce4e845735e66d8db01e7fc614b9
MrQubek <winiarz.paw@gmail.com>
2021-01-07 22:03:23 +0100

    finalize MatrixOperations and tests for them

--- src/MatrixOperations.scala
+++ src/MatrixOperations.scala
@@ -3,6 +3,15 @@ import scala.collection.parallel.CollectionConverters._
 
 object MatrixOperations {
 
+  def displayMat(mat: Array[Array[Int]]): Unit = {
+    for (i <- mat.indices) {
+      for (j <- mat(0).indices) {
+        print(" " + mat(i)(j))
+      }
+      println()
+    }
+  }
+
   def isEqual[A](matrixA: Array[Array[Int]], matrixB: Array[Array[Int]]): Boolean = {
     assert(matrixA.length > 0 && matrixB.length > 0 && matrixA.length == matrixB.length && matrixA(0).length == matrixB(0).length)
     for (i <- matrixA.indices)
@@ -15,8 +24,8 @@ object MatrixOperations {
   def isEqualParallel[A](matrixA: Array[Array[Int]], matrixB: Array[Array[Int]]): Boolean = {
     assert(matrixA.length > 0 && matrixB.length > 0 && matrixA.length == matrixB.length && matrixA(0).length == matrixB(0).length)
     for (k <- (0 to 1).par) {
-      for (i <- (k * matrixA.length / 2 until (matrixA.length - matrixA.length / 2) * k + matrixA.length / 2)) {
-        for (j <- 0 until matrixA(0).length) {
+      for (i <- k * matrixA.length / 2 until (matrixA.length - matrixA.length / 2) * k + matrixA.length / 2) {
+        for (j <- matrixA(0).indices) {
           if (matrixA(i)(j) != matrixB(i)(j)) {
             false
           }
@@ -25,4 +34,41 @@ object MatrixOperations {
     }
     true
   }
+
+  def multiply[A](matrixA: Array[Array[Int]], matrixB: Array[Array[Int]]): Array[Array[Int]] = {
+    assert(matrixA.length > 0 && matrixB.length > 0 &&
+      matrixA(0).length > 0 && matrixB(0).length > 0 &&
+      matrixA.length == matrixB.length &&
+      matrixA(0).length == matrixB.length)
+
+    var retMatrix = Array.fill(matrixA.length,matrixB(0).length)(0)
+
+    for (i <- retMatrix.indices) {
+      for (j <- retMatrix(0).indices) {
+        for (r <- matrixB(0).indices) {
+          retMatrix(i)(j) += matrixA(i)(r) * matrixB(r)(j)
+        }
+      }
+    }
+    retMatrix
+  }
+
+  def multiplyPar[A](matrixA: Array[Array[Int]], matrixB: Array[Array[Int]]): Array[Array[Int]] = {
+    assert(matrixA.length > 0 && matrixB.length > 0 &&
+      matrixA(0).length > 0 && matrixB(0).length > 0 &&
+      matrixA.length == matrixB.length &&
+      matrixA(0).length == matrixB.length)
+
+    var retMatrix = Array.fill(matrixA.length,matrixB(0).length)(0)
+
+    for (i <- retMatrix.indices.par) {
+      for (j <- retMatrix(0).indices) {
+        for (r <- matrixB(0).indices) {
+          retMatrix(i)(j) += matrixA(i)(r) * matrixB(r)(j)
+        }
+      }
+    }
+    retMatrix
+  }
+
 }
--- src/tests.scala
+++ src/tests.scala
@@ -1,3 +1,52 @@
-object tests {
 
+
+import scala.util.Random
+
+import MatrixOperations._
+
+object Tests extends App {
+
+  println("Comparison of sequential and parallel multiplication of matrixes.")
+
+  var xMatrixDimension = 1000
+  var yMatrixDimension = 1000
+  var commonMatrixDimension = 1000
+
+  println("Matrix A dimensions: "+ xMatrixDimension+" x " + commonMatrixDimension)
+  println("Matrix B dimensions: "+ commonMatrixDimension+" x " + yMatrixDimension)
+  println("Matrixes A and B are both filled with random integer values.")
+
+  var matA = Array.fill(xMatrixDimension, commonMatrixDimension)(Random.nextInt())
+  var matB = Array.fill(commonMatrixDimension, yMatrixDimension)(Random.nextInt())
+
+  /*displayMat(matA)
+  println()
+  displayMat(matB)
+  println()
+
+  println(isEqual(matA,matB))
+  println()
+
+  displayMat( multiply(matA, matB))
+  println()
+
+  displayMat( multiplyPar(matA, matB))
+  println()*/
+
+  var timeSeq:Long = 0
+  var timePar:Long = 0
+
+  val time = System.currentTimeMillis()
+  multiply(matA, matB)
+  timeSeq = System.currentTimeMillis() - time
+  println("Time of sequential multiplying: " + timeSeq+ " milliseconds")
+
+  val time2 = System.currentTimeMillis()
+  multiplyPar(matA, matB)
+  timePar = System.currentTimeMillis() - time2
+  println("Time of parallel multiplying: " + timePar+ " milliseconds")
+
+  println("Confirmation of equality of results: "+isEqual(multiply(matA, matB), multiplyPar(matA, matB)))
+
+  println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
 }
