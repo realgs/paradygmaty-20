87d5a9316d2d08b31de417bd0d8469fcfacd5bc3
Witold Brzozowski <brzozowskyy@gmail.com>
2021-01-07 13:20:51 +0100

    Add separate thread for input to client

--- L07/src/main/scala/client/Client.scala
+++ L07/src/main/scala/client/Client.scala
@@ -7,6 +7,8 @@ object Client {
   private var port = 8888
   private var socket: Socket = _
   private var connected = false
+  private var waitingForInput = false
+  private var eot = false
 
   def main(args: Array[String]): Unit = {
     if (!args.isEmpty) {
@@ -27,33 +29,49 @@ object Client {
     val out = new DataOutputStream(socket.getOutputStream)
     val in = new DataInputStream(socket.getInputStream)
 
+    val inputThread = new Thread(() => {
+      try {
+        while (connected) {
+          val input = scala.io.StdIn.readInt()
+          if (connected && waitingForInput) {
+            out.writeInt(input)
+            waitingForInput = false
+          }
+        }
+      } catch {
+        case e: IOException => e.printStackTrace()
+      }
+    })
+    inputThread.start()
+
     try {
       while (connected) {
-        var eot = false
-        var waitingForInput = false
-
         while (!eot) {
           val messageType = in.readByte()
           messageType match {
-            case 0 => connected = false
+            case 0 =>
+              connected = false
+              waitingForInput = false
+              println("Disconnected")
+              println("Input any number to quit")
             case 1 =>
               val messageCount = in.readInt()
-              for (_ <- Range(0, messageCount)) {
-                println(in.readUTF())
-              }
+              Range(0, messageCount).foreach(_ => println(in.readUTF()))
             case 2 => waitingForInput = true
+            case 3 => waitingForInput = false
             case 127 => eot = true
           }
         }
 
-        if (connected && waitingForInput) {
-          val pit = scala.io.StdIn.readInt()
-          out.writeInt(pit)
-          waitingForInput = false
-        }
+        Thread.sleep(500)
+        eot = false
       }
     } catch {
       case e: IOException => e.printStackTrace()
+    } finally {
+      out.close()
+      in.close()
+      inputThread.interrupt()
     }
   }
 }
