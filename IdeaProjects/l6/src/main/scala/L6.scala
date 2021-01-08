b503da986159187fbdfff3e765d265ceb55d1e7c
Maciej Kopiński <mackopin@gmail.com>
2021-01-09 00:09:18 +0100

    Tests if BinarySearch works

--- IdeaProjects/l6/src/main/scala/L6.scala
+++ IdeaProjects/l6/src/main/scala/L6.scala
@@ -1,7 +1,17 @@
+import scala.annotation.tailrec
+
 object L6 {
 
+  def measureTime[T](fun: =>T): Long={
+    val startTime=System.nanoTime()
+    val result=fun
+    val endTime=System.nanoTime()
+    (endTime-startTime)
+  }
 
   def main(args: Array[String])={
-    
+    System.out.println(BinarySearch.contains(Array(1, 2, 3, 4, 5, 6, -1, 22, 33, 41, 1233), -1))
+    System.out.println(BinarySearch.contains(Array(1, 2, 3, 4, 5, 6, -11, 22, 33, 41, 1233), -1))
+    System.out.println(BinarySearch.contains(Array(), -1))
   }
 }
