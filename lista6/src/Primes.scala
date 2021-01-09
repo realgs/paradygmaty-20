9b78980a5638f2e3769d67c0b893ac8840b491cb
Milena <milenamazur00@gmail.com>
2021-01-09 19:44:52 +0100

    primes and tree imp added

---
+++ lista6/src/Primes.scala
@@ -0,0 +1,36 @@
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.duration.DurationInt
+import scala.concurrent.{Await, Future}
+
+object Primes {
+
+  def generatePrimes(n: Int): List[Int] = {
+    if (n < 1) throw new IllegalArgumentException("cannot generate primes lower than 1")
+    else generatePrimesFromTo(1, n)
+  }
+
+  def generatePrimesParallel(n: Int): List[Int] = {
+    if (n < 1) throw new IllegalArgumentException("cannot generate primes lower than 1")
+    else {
+      val p1 = Future {generatePrimesFromTo(1, n / 2)}
+      val p2 = Future {generatePrimesFromTo((n / 2) + 1, n)}
+      Await.result(p1, 100.seconds) ::: Await.result(p2, 100.seconds)
+    }
+  }
+
+  private def generatePrimesFromTo(start: Int, end: Int): List[Int] = {
+    if (start > end) Nil
+    else if (isPrime(start)) start :: generatePrimesFromTo(start + 1, end)
+    else generatePrimesFromTo(start + 1, end)
+  }
+
+  private def isPrime(number: Int): Boolean = {
+    if (number < 2) return false
+    else {
+      for (x <- 2 until number) {
+        if (number % x == 0) return false
+      }
+      return true
+    }
+  }
+}
---
+++ lista6/src/Tree.scala
@@ -0,0 +1,53 @@
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.duration.DurationInt
+import scala.concurrent.{Await, Future}
+
+object Tree {
+
+  sealed trait BT[+A]
+  case object Empty extends BT[Nothing]
+  case class Node[+A](elem: A, left: BT[A], right: BT[A]) extends BT[A]
+
+  def areTreesTheSameParallel[A](tree1: BT[A], tree2: BT[A]): Boolean = {
+    (tree1, tree2) match {
+      case (Empty, Empty) => true
+      case (Node(value1, left1, right1), Node(value2, left2, right2)) =>
+        if (value1 == value2) {
+          val b1 = Future {areTreesTheSame(right1, right2)}
+          val b2 = Future {areTreesTheSame(left1, left2)}
+          Await.result(b1, 100.seconds) && Await.result(b2, 100.seconds)
+        }
+        else false
+      case (_, _) => false
+    }
+  }
+
+  def areTreesTheSame[A](tree1: BT[A], tree2: BT[A]): Boolean = {
+    (tree1, tree2) match {
+      case (Empty, Empty) => true
+      case (Node(value1, left1, right1), Node(value2, left2, right2)) =>
+        if (value1 == value2) areTreesTheSame(right1, right2) && areTreesTheSame(left1, left2)
+        else false
+      case (_, _) => false
+    }
+  }
+
+  def generateTree(depth: Int, minNumber: Int, maxNumber: Int): BT[Int] = {
+    if (depth < 0) throw new Exception("depth must be positive")
+    if (depth == 0) Empty
+    else {
+      def helper(actualDepth: Int): BT[Int] = {
+        if (actualDepth == depth) Empty
+        else Node(scala.util.Random.between(minNumber, maxNumber), helper(actualDepth + 1), helper(actualDepth + 1))
+      }
+      helper(0)
+    }
+  }
+
+  def copyTree[A](tree: BT[A]): BT[A] = {
+    tree match {
+      case Node(value, left, right) => Node(value, copyTree(left), copyTree(right))
+      case Empty => Empty
+    }
+  }
+}
