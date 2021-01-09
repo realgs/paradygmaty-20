db02135b18cf0e00f583d740a3f8ffb46ac9b6c5
bedni <w.bednarz2000@gmail.com>
2021-01-09 20:48:00 +0100

    Quicksort implementation

--- Lab6/src/Tests.scala
+++ Lab6/src/Tests.scala
@@ -9,24 +9,39 @@ object Tests {
     quickSortTimeTest()
   }
 
-  def allFunctionsTests(): Unit = ???
+  def allFunctionsTests(): Unit = {
+
+    assert(quickSortSequential(Array(4, 3, 5, -123, 0, 1, 3, 3, 1000, -5, 3)) sameElements Array(-123, -5, 0, 1, 3, 3, 3, 3, 4, 5, 1000))
+    assert(quickSortSequential(Array(1, 1, 1, 1, 1)) sameElements Array(1, 1, 1, 1, 1))
+    assert(quickSortSequential(Array(100, 99, 98, 97, 97)) sameElements Array(97, 97, 98, 99, 100))
+    assert(quickSortSequential(Array()) sameElements Array.emptyIntArray)
+
+    assert(quickSortParallel(Array(4, 3, 5, -123, 0, 1, 3, 3, 1000, -5, 3)) sameElements Array(-123, -5, 0, 1, 3, 3, 3, 3, 4, 5, 1000))
+    assert(quickSortParallel(Array(1, 1, 1, 1, 1)) sameElements Array(1, 1, 1, 1, 1))
+    assert(quickSortParallel(Array(100, 99, 98, 97, 97)) sameElements Array(97, 97, 98, 99, 100))
+    assert(quickSortParallel(Array()) sameElements Array.emptyIntArray)
+
+  }
 
   def quickSortTimeTest(): Unit = {
 
     println("QUICKSORT TEST\n")
 
-    quickSortTimeTest(10)
-    quickSortTimeTest(20)
-    quickSortTimeTest(30)
+    quickSortTimeTest(1000)
+    quickSortTimeTest(10000)
+    quickSortTimeTest(100000)
+    quickSortTimeTest(1000000)
   }
 
   def quickSortTimeTest(n: Int): Unit = {
 
     val testArray = generateRandomIntArray(n, -1000, 5000)
 
-    println(testArray.mkString("Array(", ", ", ")"))
-    calculateTime("Quicksort sequential", quickSortSequential(testArray))
+    println(s"n = $n")
+    calculateTime("Quicksort sequential", quickSortSequential(testArray.clone()))
     calculateTime("Quicksort parallel", quickSortParallel(testArray.clone()))
+
+    println()
   }
 
 }
--- Lab6/src/Utils.scala
+++ Lab6/src/Utils.scala
@@ -1,7 +1,16 @@
+import scala.util.Random
+
 object Utils {
 
-  def generateRandomIntArray(size: Int, min: Int, max: Int): Array[Int] = ???
+  def generateRandomIntArray(size: Int, min: Int, max: Int): Array[Int] =
+    Array.fill(size)(Random.between(min, max))
 
-  def calculateTime[T](operationName: String, block: => T): T = ???
+  def calculateTime[T](operationName: String, block: => T): T = {
+    val t0 = System.nanoTime()
+    val result = block
+    val t1 = System.nanoTime()
+    printf(s"$operationName time:\t %10d ns%n", t1 - t0)
+    result
+  }
 
 }
