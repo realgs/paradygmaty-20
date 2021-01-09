a08127f32be3332bffa6f28ee037fad0b1a00a37
Maciej Kopiński <mackopin@gmail.com>
2021-01-09 17:55:52 +0100

    Tests

--- IdeaProjects/l6/src/main/scala/L6.scala
+++ IdeaProjects/l6/src/main/scala/L6.scala
@@ -2,16 +2,32 @@ import scala.annotation.tailrec
 
 object L6 {
 
-  def measureTime[T](fun: =>T): Long={
-    val startTime=System.nanoTime()
-    val result=fun
-    val endTime=System.nanoTime()
-    (endTime-startTime)
+  def measureTime[T](fun: =>T): Unit={
+    val startTime=System.currentTimeMillis()
+    fun
+    val endTime=System.currentTimeMillis()
+    System.out.println((endTime-startTime)+"ms")
+  }
+
+  def showArray(a: Array[Int]): Unit={
+    var i=0;
+    while(i<a.size){
+      System.out.print(a(i)+" ")
+      i+=1
+    }
   }
 
   def main(args: Array[String])={
-    System.out.println(BinarySearch.contains(Array(1, 2, 3, 4, 5, 6, -1, 22, 33, 41, 1233), -1))
-    System.out.println(BinarySearch.contains(Array(1, 2, 3, 4, 5, 6, -11, 22, 33, 41, 1233), -1))
-    System.out.println(BinarySearch.contains(Array(), -1))
+    var i=1000
+    while(i<=1000000){
+      System.out.println(i+"-elements:")
+      var a=Array.fill(i){scala.util.Random.nextInt(i)}
+      measureTime(QuickSort.quickSort(a))
+      a=Array.fill(i){scala.util.Random.nextInt(i)}
+      measureTime(QuickSortParallel.quickSortParallel(a))
+      measureTime(BinarySearch.contains(a, -1))
+      measureTime(BinarySearchParallel.containsParallel(a, -1))
+      i*=10
+    }
   }
 }
