ad96a16345d3189e4f6b9f14242727965d1c0d87
Maciej Kopiński <mackopin@gmail.com>
2021-01-09 18:02:28 +0100

    repaired repository

--- IdeaProjects/l5/src/Date.scala
+++
@@ -1,5 +0,0 @@
-class Date(d: Int, m: Int, y: Int) extends Debug {
-  var day = d
-  var month = m
-  var year = y
-}
--- IdeaProjects/l5/src/Point.scala
+++
@@ -1,8 +0,0 @@
-
-class Point(xv: Int, yv: Int) extends Debug {
-  var x: Int = xv
-  var y: Int = yv
-  var a: String = "test"
-
-}
-
