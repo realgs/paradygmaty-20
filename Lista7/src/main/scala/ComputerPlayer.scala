4793121864909f367c4cf6723497e0f0f3153930
marcijay <jak.marcinko@gmail.com>
2021-01-22 15:15:32 +0100

    Added human player recive implementation and temoprary random computer behaviour

--- Lista7/src/main/scala/ComputerPlayer.scala
+++ Lista7/src/main/scala/ComputerPlayer.scala
@@ -1,5 +1,17 @@
+import HumanPlayer.ChoosePit
+import Server.Move
 import akka.actor.Actor
 
+import scala.util.Random
+
 class ComputerPlayer(val number: Byte) extends Actor{
-  override def receive: Receive = ???
+  override def receive: Receive = {
+    case ChoosePit(board: Board) =>
+      var choice = -1
+      while(!board.isChosenPitCorrect(choice)){
+        choice = Random.between(0, 13)
+      }
+      println(s"Computer player's choice: $choice")
+      sender() ! Move(choice)
+  }
 }
--- Lista7/src/main/scala/HumanPlayer.scala
+++ Lista7/src/main/scala/HumanPlayer.scala
@@ -1,8 +1,17 @@
+import HumanPlayer.ChoosePit
+import Server.Move
 import akka.actor.Actor
 
 class HumanPlayer(val number: Byte) extends  Actor{
-  override def receive: Receive = ???
-
+  override def receive: Receive = {
+    case ChoosePit(board: Board) =>
+      var choice = -1
+      while(!board.isChosenPitCorrect(choice)){
+        println(s"Enter number from ${board.getActivePlayerNumber * 7} to ${board.getActivePlayerNumber * 7 + 5}")
+        choice = scala.io.StdIn.readInt()
+      }
+      sender() ! Move(choice)
+  }
 }
 
 object HumanPlayer{
