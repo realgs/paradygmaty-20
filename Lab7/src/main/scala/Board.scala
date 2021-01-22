86d7f8622f3a44ba020228ba849a079aa2b4503f
bedni <w.bednarz2000@gmail.com>
2021-01-22 02:09:17 +0100

    Board implementation

--- Lab7/src/main/scala/Board.scala
+++ Lab7/src/main/scala/Board.scala
@@ -1,3 +1,59 @@
 class Board {
 
+  private val startStonesNumber = 6
+  private val board: Array[Int] = createBoard()
+  var ifPlayerOneMove = true
+  var ifOneAgainMove = false
+
+  def makeMove(fieldNumber: Int): Boolean = {
+
+    if(fieldNumber < 1 || fieldNumber > 6 || board(fieldNumber + (if(ifPlayerOneMove) -1 else 6)) == 0) false
+    else {
+
+      var index = fieldNumber + (if(ifPlayerOneMove) -1 else 6)
+      var stones = board(index)
+
+      board(index) = 0
+
+      while (stones > 0) {
+
+        index = (index + 1) % 14
+
+        if (index != (if(ifPlayerOneMove) 13 else 6)) {
+
+          board(index) = board(index) + 1
+          stones -= 1
+        }
+      }
+
+      if(index == (if(ifPlayerOneMove) 6 else 13)) ifOneAgainMove = true
+      else if(board(index) == 1 && index >= (if(ifPlayerOneMove) 0 else 7) && index <= (if(ifPlayerOneMove) 5 else 12)) {
+
+        board(if(ifPlayerOneMove) 6 else 13) += board(12 - index) + 1
+        board(12 - index) = 0
+        board(index) = 0
+        ifPlayerOneMove = !ifPlayerOneMove
+
+      } else ifPlayerOneMove = !ifPlayerOneMove
+
+      true
+    }
+  }
+
+  private def createBoard(): Array[Int] = {
+
+    val board = Array.fill(14)(startStonesNumber)
+    board(6) = 0
+    board(13) = 0
+
+    board
+  }
+
+  def print(): Unit = {
+
+    printf("%n    | %2d | %2d | %2d | %2d | %2d | %2d |%n", board(12), board(11), board(10), board(9), board(8), board(7))
+    printf(" %2d |-----------------------------| %2d", board(13), board(6))
+    printf("%n    | %2d | %2d | %2d | %2d | %2d | %2d |%n", board(0), board(1), board(2), board(3), board(4), board(5))
+  }
+
 }
