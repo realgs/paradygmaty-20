fbf5708795c1edbbee350535b0c3298b77311d42
Milena <milenamazur00@gmail.com>
2021-01-24 19:29:59 +0100

    30 second time limit added

--- lista7/src/Player.scala
+++ lista7/src/Player.scala
@@ -1,9 +1,39 @@
+import java.util.concurrent.TimeoutException
+
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.duration.{DurationInt, FiniteDuration}
+import scala.concurrent.{Await, Future}
 import scala.io.StdIn.readInt
 
-class Player() extends Engine {
+class Player(exceedTime: Int = 30) extends Engine {
 
   override def move(player: Int): Int = {
+    exceed(exceedTime.seconds, player)
+  }
+
+  def exceed(exceedTime: FiniteDuration, player: Int): Int = {
     print("Player" + (player + 1) + " choose pit: ")
-    readInt()
+    var timedOut = false
+
+    val f1 = Future {
+      while (System.in.available() == 0) {
+        if (timedOut) throw new TimeoutException()
+      }
+      val pos = readInt()
+      require(pos < 7 && pos > 0)
+      pos
     }
+
+
+    try Await.result(f1, exceedTime)
+    catch {
+      case _: TimeoutException =>
+        println("Sorry, the time is up")
+        timedOut = true
+        0
+      case _: IllegalArgumentException =>
+        println("The value must be an integer between 1 and 6")
+        -1
+    }
+  }
 }
