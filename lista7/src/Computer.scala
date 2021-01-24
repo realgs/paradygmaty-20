70b507de76880fb3534bb565b5d4db1fa8204c1d
Milena <milenamazur00@gmail.com>
2021-01-24 19:28:18 +0100

    computer strategy added

--- lista7/src/Computer.scala
+++ lista7/src/Computer.scala
@@ -1,7 +1,21 @@
-import scala.util.Random
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.duration.DurationInt
+import scala.concurrent.{Await, Future}
 
-class Computer() extends Engine {
+class Computer(simulator: Simulator = new Simulator()) extends Engine {
   override def move(player: Int): Int = {
-    Random.nextInt(6) + 1
+
+    val result = {
+      for (i <- 0 until 6)
+        yield Future(simulator.simulate(i, player))
+    }
+    var returnValue = (Int.MinValue, -1)
+    for (i <- 0 until 6) {
+      val x = Await.result(result(i), 200.seconds)
+      if (x > returnValue._1) returnValue = (x, i)
+    }
+    returnValue._2 + 1
+    //returning + 1 because I assume that player choose pit from 1 to 6, and as computer and player
+    //are both the engine, than both has to return values between 1 and 6
   }
 }
--- lista7/src/Player.scala
+++ lista7/src/Player.scala
@@ -3,7 +3,7 @@ import scala.io.StdIn.readInt
 class Player() extends Engine {
 
   override def move(player: Int): Int = {
-    println("Player" + player + " choose pit: ")
+    print("Player" + (player + 1) + " choose pit: ")
     readInt()
     }
 }
