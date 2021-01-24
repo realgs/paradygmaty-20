2b7490ea69588b0e239661fd1584546f2ee98033
Maciej Kopiński <mackopin@gmail.com>
2021-01-24 17:13:57 +0100

    Created testing procedure

--- IdeaProjects/L7/src/main/scala/Main.scala
+++ IdeaProjects/L7/src/main/scala/Main.scala
@@ -1,3 +1,14 @@
-object Main extends App {
+import akka.actor.{ActorSystem, Props}
 
+object Main extends App{
+
+  val board = new Board
+  board.showBoard()
+
+  val mySystem=ActorSystem("mySystem")
+  val player1=mySystem.actorOf(Props(new BotPlayer("player1")))
+  val player2=mySystem.actorOf(Props(new BotPlayer("player2")))
+  val server=mySystem.actorOf(Props(new Server(player1, player2)))
+  server ! Start
+  mySystem.terminate()
 }
